99718ba9e6b9a64f91609eb9c70e9f5a49e47f4a
Taylor Brent <taylor.w.brent@gmail.com>
2021-07-02 10:29:59 -0400

    remove unneccessary require

--- contracts/SlowMintingERC20.sol
+++ contracts/SlowMintingERC20.sol
@@ -108,7 +108,6 @@ abstract contract SlowMintingERC20 is ISlowMintingERC20, RelayERC20 {
 
     /// ==== Internal ====
     function _startMinting(address account, uint256 amount) internal override {
-        require(_msgSender() == address(this), "ERC20: mint is only callable by self");
         require(account != address(0), "ERC20: mint to the zero address");
         require(amount > 0, "cannot mint 0");
 
