d87ec884dc19fd7d7ddd2eb2ff42a71aa2a641ab
Julian R <56316686+julianmrodri@users.noreply.github.com>
2021-07-09 10:35:31 -0300

    enable gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
+require("hardhat-gas-reporter");
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
 module.exports = {
   solidity: "0.8.4",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 2000
+    }
+  }
 };
