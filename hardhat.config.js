a956714dd684480b59665d029f1c30443d3ee119
Taylor Brent <taylor.w.brent@gmail.com>
2021-07-12 14:12:42 -0400

    fix optimizer hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,11 +7,13 @@ require("hardhat-gas-reporter");
  */
 
 module.exports = {
-  solidity: "0.8.4",
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 2000
-    }
+  solidity: {
+      version: "0.8.4",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 2000
+        }
+      }
   }
 };
