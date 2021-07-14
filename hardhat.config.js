cc5ffddc880eea2e7efcbc8bad9d19db00a89563
Julian R <56316686+julianmrodri@users.noreply.github.com>
2021-07-13 22:58:21 -0300

    update config and yarn files

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,12 +7,14 @@ require("hardhat-gas-reporter");
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-module.exports = {
-  solidity: "0.8.4",
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 2000
-    }
+ module.exports = {
+  solidity: {
+      version: "0.8.4",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 2000
+        }
+      }
   }
 };
