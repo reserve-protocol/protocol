b4dac6be80d2e999c570f5a4050b79b21eb1e2c0
Julian R <56316686+julianmrodri@users.noreply.github.com>
2021-07-15 18:47:55 -0300

    tx calculator tests in RToken

--- contracts/test/RTokenMock.sol
+++ contracts/test/RTokenMock.sol
@@ -6,16 +6,24 @@ import "../RToken.sol";
 
 contract RTokenMock is RToken {
 
+    function mint(address recipient, uint256 amount) external {
+        _mint(recipient, amount);
+    }
+    
     function maxSupply() external view returns(uint256) {
         return config.maxSupply;
     }
-    
+
     function issuanceRate() external view returns(uint256) {
         return config.issuanceRate;
     }
 
     function circuitBreaker() external view returns(address) {
         return address(config.circuitBreaker);
+    }      
+
+    function txFeeCalculator() external view returns(address) {
+        return address(config.txFeeCalculator);
     }      
 
     function startMinting(address account, uint256 amount) public {
