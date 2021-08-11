f51620c638fe539e446bfd7aaed9a56f5a2b7dfc
Julian R <56316686+julianmrodri@users.noreply.github.com>
2021-08-11 00:43:03 -0300

    add aux functions and constants

--- common/constants.js
+++ common/constants.js
@@ -5,11 +5,14 @@ const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 const ONE_ETH= BigNumber.from("1000000000000000000");
 
 const MAX_UINT256 = BigNumber.from(2).pow(256).sub(1);
+
 const SCALE_FACTOR = 1e18;
+const BN_SCALE_FACTOR = BigNumber.from(SCALE_FACTOR.toString());
 
 module.exports = {
     ZERO_ADDRESS,
     MAX_UINT256,
     SCALE_FACTOR,
+    BN_SCALE_FACTOR,
     ONE_ETH
 };
