3f64ad86f44e90393202919de10cf0a2c8dec3ff
Julian R <56316686+julianmrodri@users.noreply.github.com>
2021-10-14 12:36:51 -0300

    get block timestamp

--- test/utils/time.ts
+++ test/utils/time.ts
@@ -4,3 +4,8 @@ export const advanceTime = async (seconds: number | string) => {
   await ethers.provider.send('evm_increaseTime', [parseInt(seconds.toString())])
   await ethers.provider.send('evm_mine', [])
 }
+
+export const getLatestBlockTimestamp = async (): Promise<number> => {
+  const latestBlock = await ethers.provider.getBlock('latest')
+  return latestBlock.timestamp
+}
