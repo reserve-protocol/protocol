5828c929cb93e6960cdbdd3c9d8738541f526e9b
Julian R <56316686+julianmrodri@users.noreply.github.com>
2021-10-14 12:37:03 -0300

    FaucetP0 and tests

---
+++ contracts/proto0/FaucetP0.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: BlueOak-1.0.0
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "./interfaces/IFaucet.sol";
+
+contract FaucetP0 is IFaucet {
+    using SafeERC20 for IERC20;
+
+    address public immutable beneficiary;
+    IERC20 public immutable token;
+
+    struct Handout {
+        uint256 amount;
+        uint256 start; // Start timestamp
+        uint256 duration; // Duration in seconds
+        uint256 released; // Amount already released
+    }
+
+    Handout[] public handouts;
+
+    //uint256 public handoutIndex; // Potential optimization
+
+    constructor(address beneficiary_, address token_) {
+        require(beneficiary_ != address(0), "Beneficiary is zero address");
+        require(token_ != address(0), "Token is zero address");
+
+        beneficiary = beneficiary_;
+        token = IERC20(token_);
+    }
+
+    function handout(uint256 amount, uint256 timePeriod) external override {
+        require(amount > 0, "Cannot handout zero");
+
+        token.safeTransferFrom(msg.sender, address(this), amount);
+
+        // Register handout
+        handouts.push(Handout(amount, block.timestamp, timePeriod, 0));
+    }
+
+    function drip() external override {
+        uint256 releasable = _processHandouts(block.timestamp);
+        SafeERC20.safeTransfer(IERC20(token), beneficiary, releasable);
+    }
+
+    function _processHandouts(uint256 timestamp) internal returns (uint256) {
+        uint256 releasable = 0;
+        for (
+            uint256 index = 0; /*handoutIndex*/
+            index < handouts.length;
+            index++
+        ) {
+            Handout storage currHandout = handouts[index];
+
+            // Check if there are still funds to be released
+            if (currHandout.released < currHandout.amount) {
+                uint256 vestedAmount = _vestedAmount(currHandout, timestamp);
+
+                // Release amount
+                releasable += vestedAmount - currHandout.released;
+
+                // Update released
+                currHandout.released = vestedAmount;
+
+                // Note: Potential optimization by cleaning up Handout once all consumed
+            }
+
+            // Note:  Potential optimization by increasing handoutIndex
+        }
+
+        return releasable;
+    }
+
+    function _vestedAmount(Handout memory _handout, uint256 timestamp) internal pure returns (uint256) {
+        if (timestamp < _handout.start) {
+            return 0;
+        } else if (timestamp > _handout.start + _handout.duration) {
+            return _handout.amount;
+        } else {
+            return (_handout.amount * (timestamp - _handout.start)) / _handout.duration;
+        }
+    }
+}
---
+++ test/proto0/FaucetP0.test.ts
@@ -0,0 +1,431 @@
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+import { BigNumber, ContractFactory, Contract } from 'ethers'
+import { bn } from '../../common/numbers'
+import { advanceTime, getLatestBlockTimestamp } from '../utils/time'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { ERC20Mock } from '../../typechain/ERC20Mock'
+import { RTokenMockP0 } from '../../typechain/RTokenMockP0'
+import { FaucetP0 } from '../../typechain/FaucetP0'
+import { ZERO_ADDRESS } from '../../common/constants'
+import { subtask } from 'hardhat/config'
+
+interface IHandoutInfo {
+  amount: BigNumber
+  start: number
+  duration: number
+  released: BigNumber
+}
+
+describe('FaucetP0 contract', () => {
+  let owner: SignerWithAddress
+  let addr1: SignerWithAddress
+  let addr2: SignerWithAddress
+  let beneficiary: Contract | SignerWithAddress
+
+  let ERC20: ContractFactory
+  let FaucetFactory: ContractFactory
+  let RToken: ContractFactory
+  let faucet: FaucetP0
+  let rToken: RTokenMockP0
+  let rsr: ERC20Mock
+
+  let initialBal: BigNumber
+
+  const expectHandoutInfo = async (index: number, hdnOutInfo: Partial<IHandoutInfo>) => {
+    const { amount, start, duration, released } = await faucet.handouts(index)
+
+    expect(amount).to.equal(hdnOutInfo.amount)
+    expect(start).to.equal(hdnOutInfo.start)
+    expect(duration).to.equal(hdnOutInfo?.duration)
+    expect(released).to.equal(hdnOutInfo.released)
+  }
+
+  beforeEach(async () => {
+    ;[owner, addr1, addr2, beneficiary] = await ethers.getSigners()
+
+    // Deploy RSR and RToken
+    ERC20 = await ethers.getContractFactory('ERC20Mock')
+    rsr = <ERC20Mock>await ERC20.deploy('Reserve Rights', 'RSR')
+
+    RToken = await ethers.getContractFactory('RTokenMockP0')
+    rToken = <RTokenMockP0>await RToken.deploy('RToken', 'RTKN', rsr.address)
+
+    // Mint and set balances
+    initialBal = bn(100e18)
+    await rToken.connect(owner).mint(addr1.address, initialBal)
+    await rToken.connect(owner).mint(addr2.address, initialBal)
+
+    // Deploy Faucet
+    FaucetFactory = await ethers.getContractFactory('FaucetP0')
+    faucet = <FaucetP0>await FaucetFactory.deploy(beneficiary.address, rToken.address)
+  })
+
+  describe('Deployment', () => {
+    it('Deployment should setup Faucet correctly', async () => {
+      expect(await faucet.beneficiary()).to.equal(beneficiary.address)
+      expect(await faucet.token()).to.equal(rToken.address)
+    })
+
+    it('Deployment does not accept empty beneficiary and token', async () => {
+      await expect(FaucetFactory.deploy(ZERO_ADDRESS, rToken.address)).to.be.revertedWith('Beneficiary is zero address')
+      await expect(FaucetFactory.deploy(beneficiary.address, ZERO_ADDRESS)).to.be.revertedWith('Token is zero address')
+    })
+  })
+
+  describe('Handouts', () => {
+    const timePeriod: number = 60 * 60 * 24 // 1 day
+
+    it('Should not allow handouts with zero amount', async () => {
+      const zero: BigNumber = bn(0)
+
+      await expect(faucet.connect(addr1).handout(zero, timePeriod)).to.be.revertedWith('Cannot handout zero')
+    })
+
+    it('Should revert when not providing approval for tokens', async () => {
+      const hndAmt: BigNumber = bn(10e18)
+
+      await expect(faucet.connect(addr1).handout(hndAmt, timePeriod)).to.be.revertedWith(
+        'ERC20: transfer amount exceeds allowance'
+      )
+    })
+
+    it('Should revert when not having enough RTokens for handout', async () => {
+      const hndAmt: BigNumber = bn(20000e18)
+
+      // Approval
+      await rToken.connect(addr1).approve(faucet.address, hndAmt)
+
+      await expect(faucet.connect(addr1).handout(hndAmt, timePeriod)).to.be.revertedWith(
+        'ERC20: transfer amount exceeds balance'
+      )
+    })
+
+    it('Should allow handouts correctly', async () => {
+      const hndAmt: BigNumber = bn(10e18)
+
+      // Approval
+      await rToken.connect(addr1).approve(faucet.address, hndAmt)
+
+      // Handout
+      await faucet.connect(addr1).handout(hndAmt, timePeriod)
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(hndAmt)
+
+      // Check handout registered
+      expectHandoutInfo(0, {
+        amount: hndAmt,
+        start: await getLatestBlockTimestamp(),
+        duration: timePeriod,
+        released: bn(0),
+      })
+    })
+
+    it('Should allow multiple handouts', async () => {
+      const hndAmt1: BigNumber = bn(10e18)
+      const hndAmt2: BigNumber = bn(50e18)
+
+      // Approval
+      await rToken.connect(addr1).approve(faucet.address, hndAmt1)
+      await rToken.connect(addr2).approve(faucet.address, hndAmt2)
+
+      // Handout
+      await faucet.connect(addr1).handout(hndAmt1, timePeriod)
+
+      // Check handout registered
+      expectHandoutInfo(0, {
+        amount: hndAmt1,
+        start: await getLatestBlockTimestamp(),
+        duration: timePeriod,
+        released: bn(0),
+      })
+
+      // Additional Handout
+      await faucet.connect(addr2).handout(hndAmt2, timePeriod)
+
+      // Check handout registered
+      expectHandoutInfo(1, {
+        amount: hndAmt2,
+        start: await getLatestBlockTimestamp(),
+        duration: timePeriod,
+        released: bn(0),
+      })
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt1))
+      expect(await rToken.balanceOf(addr2.address)).to.equal(initialBal.sub(hndAmt2))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(hndAmt1.add(hndAmt2))
+    })
+  })
+
+  describe('Drip', () => {
+    it('Should allow drips all funds if period is zero', async () => {
+      const hndAmt: BigNumber = bn(2e18)
+
+      // Approval
+      await rToken.connect(addr1).approve(faucet.address, hndAmt)
+
+      // Handout
+      await faucet.connect(addr1).handout(hndAmt, 0)
+
+      const hndTimestamp = await getLatestBlockTimestamp()
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(hndAmt)
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(0)
+
+      // Drip
+      await faucet.connect(addr1).drip()
+
+      // Check drip registered
+      expectHandoutInfo(0, {
+        amount: hndAmt,
+        start: hndTimestamp,
+        duration: 0,
+        released: hndAmt,
+      })
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(0)
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(hndAmt)
+    })
+
+    it('Should allow drips - full amount if period is complete', async () => {
+      const hndAmt: BigNumber = bn(10e18)
+      const timePeriod: number = 60 * 60 * 24 // 1 day
+
+      // Approval
+      await rToken.connect(addr1).approve(faucet.address, hndAmt)
+
+      // Handout
+      await faucet.connect(addr1).handout(hndAmt, timePeriod)
+
+      const hndTimestamp = await getLatestBlockTimestamp()
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(hndAmt)
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(0)
+
+      // Advance to the end to withdraw full amount
+      advanceTime(timePeriod + 1)
+
+      // Drip
+      await faucet.connect(addr1).drip()
+
+      // Check drip registered
+      expectHandoutInfo(0, {
+        amount: hndAmt,
+        start: hndTimestamp,
+        duration: timePeriod,
+        released: hndAmt,
+      })
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(0)
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(hndAmt)
+    })
+
+    it('Should not return more funds once all was released', async () => {
+      const hndAmt: BigNumber = bn(10e18)
+      const timePeriod: number = 60 * 60 * 24 // 1 day
+
+      // Approval
+      await rToken.connect(addr1).approve(faucet.address, hndAmt)
+
+      // Handout
+      await faucet.connect(addr1).handout(hndAmt, timePeriod)
+
+      const hndTimestamp = await getLatestBlockTimestamp()
+
+      // Advance to the end to withdraw full amount
+      advanceTime(timePeriod + 1)
+
+      // Drip
+      await faucet.connect(addr1).drip()
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(0)
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(hndAmt)
+
+      // Try to drip again
+      await faucet.connect(addr1).drip()
+
+      // Check drip not modified
+      expectHandoutInfo(0, {
+        amount: hndAmt,
+        start: hndTimestamp,
+        duration: timePeriod,
+        released: hndAmt,
+      })
+
+      // No changes in balances
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(0)
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(hndAmt)
+    })
+
+    it('Should allow drips - two equal parts', async () => {
+      const hndAmt: BigNumber = bn(10e18)
+      const timePeriod: number = 60 * 60 * 24 // 1 day
+
+      // Approval
+      await rToken.connect(addr1).approve(faucet.address, hndAmt)
+
+      // Handout
+      await faucet.connect(addr1).handout(hndAmt, timePeriod)
+
+      const hndTimestamp = await getLatestBlockTimestamp()
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(hndAmt)
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(0)
+
+      // Advance to the middle of period
+      advanceTime(timePeriod / 2 - 1)
+
+      // Drip
+      await faucet.connect(addr1).drip()
+
+      // Check drip registered
+      expectHandoutInfo(0, {
+        amount: hndAmt,
+        start: hndTimestamp,
+        duration: timePeriod,
+        released: hndAmt.div(2),
+      })
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(hndAmt.div(2))
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(hndAmt.div(2))
+
+      // Advance to the end
+      advanceTime(timePeriod / 2)
+
+      // Drip with any account
+      await faucet.connect(addr2).drip()
+
+      // Check drip registered
+      expectHandoutInfo(0, {
+        amount: hndAmt,
+        start: hndTimestamp,
+        duration: timePeriod,
+        released: hndAmt,
+      })
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(0)
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(hndAmt)
+    })
+
+    it('Should allow drips - for multiple handouts', async () => {
+      const hndAmt: BigNumber = bn(10e18)
+      const timePeriod: number = 60 * 60 * 24 // 1 day
+
+      // Approval
+      await rToken.connect(addr1).approve(faucet.address, hndAmt)
+
+      // Handout
+      await faucet.connect(addr1).handout(hndAmt, timePeriod)
+
+      const hndTimestamp = await getLatestBlockTimestamp()
+
+      const hndAmt2: BigNumber = bn(20e18)
+      const timePeriod2: number = 60 * 60 // 1 hour
+
+      // Approval
+      await rToken.connect(addr1).approve(faucet.address, hndAmt2)
+
+      // Handout
+      await faucet.connect(addr1).handout(hndAmt2, timePeriod2)
+
+      const hndTimestamp1 = await getLatestBlockTimestamp()
+
+      // Advance to the middle of period
+      advanceTime(timePeriod / 2 - 3) // already 2 additional blocks processed
+
+      // Drip with any account
+      await faucet.connect(addr2).drip()
+
+      // Check drip registered in both handouts
+      expectHandoutInfo(0, {
+        amount: hndAmt,
+        start: hndTimestamp,
+        duration: timePeriod,
+        released: hndAmt.div(2),
+      })
+
+      // Handout 2
+      expectHandoutInfo(1, {
+        amount: hndAmt2,
+        start: hndTimestamp1,
+        duration: timePeriod2,
+        released: hndAmt2,
+      })
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt).sub(hndAmt2))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(hndAmt.div(2))
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(hndAmt2.add(hndAmt.div(2)))
+
+      // Advance to the 75% of largest period
+      advanceTime(timePeriod / 4 - 1)
+
+      // Drip with any account
+      await faucet.connect(addr2).drip()
+
+      // Check drip updated in handout
+      expectHandoutInfo(0, {
+        amount: hndAmt,
+        start: hndTimestamp,
+        duration: timePeriod,
+        released: hndAmt.div(2).add(hndAmt.div(4)),
+      })
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt).sub(hndAmt2))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(hndAmt.div(4))
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(hndAmt2.add(hndAmt.div(2)).add(hndAmt.div(4)))
+
+      // Approval
+      await rToken.connect(addr2).approve(faucet.address, hndAmt)
+
+      // Handout
+      await faucet.connect(addr2).handout(hndAmt, timePeriod / 2)
+
+      const hndTimestamp2 = await getLatestBlockTimestamp()
+
+      // Check new handout registered
+      expectHandoutInfo(2, {
+        amount: hndAmt,
+        start: hndTimestamp2,
+        duration: timePeriod / 2,
+        released: bn(0),
+      })
+
+      // Advance to the end of largest period, shoeuld process half of the last handout
+      advanceTime(timePeriod / 4 - 1)
+
+      // Drip with any account
+      await faucet.connect(addr2).drip()
+
+      // Check drip updated
+
+      expectHandoutInfo(0, {
+        amount: hndAmt,
+        start: hndTimestamp,
+        duration: timePeriod,
+        released: hndAmt,
+      })
+
+      expectHandoutInfo(2, {
+        amount: hndAmt,
+        start: hndTimestamp2,
+        duration: timePeriod / 2,
+        released: hndAmt.div(2),
+      })
+
+      expect(await rToken.balanceOf(addr1.address)).to.equal(initialBal.sub(hndAmt).sub(hndAmt2))
+      expect(await rToken.balanceOf(addr2.address)).to.equal(initialBal.sub(hndAmt))
+      expect(await rToken.balanceOf(faucet.address)).to.equal(hndAmt.div(2))
+      expect(await rToken.balanceOf(beneficiary.address)).to.equal(hndAmt2.add(hndAmt).add(hndAmt.div(2)))
+    })
+  })
+})
