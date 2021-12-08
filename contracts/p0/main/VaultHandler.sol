00e315c5078e103370b5e4af257494e8231b690e
Matt Elder <fiddlemath@gmail.com>
2021-12-07 21:04:04 -0800

    Some simplifying changes to VaultHandler

--- contracts/p0/main/VaultHandler.sol
+++ contracts/p0/main/VaultHandler.sol
@@ -31,7 +31,7 @@ contract VaultHandlerP0 is Ownable, Mixin, SettingsHandlerP0, RevenueDistributor
     // Fully capitalized: #RTokens <= #BUs / b
 
     Fix internal _historicalBasketDilution; // the product of all historical basket dilutions
-    Fix internal _prevBasketRate; // redemption value of the basket in fiatcoins last update
+    Fix internal _prevBasketRate; // {USD/qBU} redemption value of the basket in fiatcoins last update
 
     IVault[] public override vaults;
 
@@ -82,31 +82,15 @@ contract VaultHandlerP0 is Ownable, Mixin, SettingsHandlerP0, RevenueDistributor
 
     /// {qRTok} -> {qBU}
     function toBUs(uint256 amount) public view override returns (uint256) {
-        if (rToken().totalSupply() == 0) {
-            return amount;
-        }
-
-        // (_meltingFactor() / _basketDilutionFactor()) * amtBUs
         return _baseFactor().mulu(amount).toRoundUint();
     }
 
     /// {qBU} -> {qRTok}
-    // solhint-disable-next-line func-param-name-mixedcase
     function fromBUs(uint256 amtBUs) public view override returns (uint256) {
-        if (rToken().totalSupply() == 0) {
-            return amtBUs;
-        }
-
-        // (_basketDilutionFactor() / _meltingFactor()) * amount
-        return toFix(amtBUs).div(_baseFactor()).toRoundUint();
+        return divFix(amtBUs,_baseFactor()).toRoundUint();
     }
 
     // ==== Internal ====
-
-    /// @return {qRTok/qBU} The base factor
-    function _baseFactor() internal view returns (Fix) {
-        return _meltingFactor().div(_basketDilutionFactor());
-    }
 
     function _switchVault(IVault vault_) internal {
         beforeUpdate();
@@ -115,14 +99,27 @@ contract VaultHandlerP0 is Ownable, Mixin, SettingsHandlerP0, RevenueDistributor
         beforeUpdate();
     }
 
-    /// @return {none) Denominator of the base factor
+    /// @return {qRTok/qBU} The conversion rate from BUs to RTokens,
+    /// 1.0 if the total rtoken supply is 0
+    /// Else, (melting factor) / (basket dilution factor)
+    function _baseFactor() internal view returns (Fix) {
+        return rToken().totalSupply() == 0 ? FIX_ONE : _meltingFactor().div(_basketDilutionFactor());
+    }
+
+    /// @return {qBU/qRTok) the basket dilution factor
+
+    /* As the basketRate increases, the basketDilutionFactor increases at a proportional rate.
+     * for two times t0 < t1 when the rTokenCut() doesn't change, we have:
+     * (basketDiluationFactor at t1) - (basketDilutionFactor at t0) = rTokenCut() * ((basketRate at t1) - (basketRate at t0))
+     */
     function _basketDilutionFactor() internal view returns (Fix) {
+        // {USD/qBU}
         Fix currentRate = vault().basketRate();
 
         // Assumption: Defi redemption rates are monotonically increasing
+        // {USD/qBU}
         Fix delta = currentRate.minus(_prevBasketRate);
 
-        // here, in order to deal with changes to the rTokenCut coming from RevenueDistributor.
         // r = p2 / (p1 + (p2-p1) * (rTokenCut))
         Fix r = currentRate.div(_prevBasketRate.plus(delta.mul(rTokenCut())));
         Fix dilutionFactor = _historicalBasketDilution.mul(r);
@@ -132,14 +129,9 @@ contract VaultHandlerP0 is Ownable, Mixin, SettingsHandlerP0, RevenueDistributor
 
     /// @return {none} Numerator of the base factor
     function _meltingFactor() internal view returns (Fix) {
-        Fix totalSupply = toFix(rToken().totalSupply()); // {qRTok}
-        Fix totalBurnt = toFix(rToken().totalMelted()); // {qRTok}
-        if (totalSupply.eq(FIX_ZERO)) {
-            return FIX_ONE;
-        }
-
-        // (totalSupply + totalBurnt) / totalSupply
-        return totalSupply.plus(totalBurnt).div(totalSupply);
+        Fix supply = toFix(rToken().totalSupply()); // {qRTok}
+        Fix melted = toFix(rToken().totalMelted()); // {qRTok}
+        return supply.eq(FIX_ZERO) ? FIX_ONE : supply.plus(melted).div(supply);
     }
 
     /// Redeems up to `amtBUs` basket units from all past vaults.
