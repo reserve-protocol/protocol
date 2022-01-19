72e4545370ce04066e5fa63ad886d57fcc38484a
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-01-19 10:08:35 -0300

    fix deployer tests

--- test/p0/DeployerP0.test.ts
+++ test/p0/DeployerP0.test.ts
@@ -5,15 +5,14 @@ import { ethers, waffle } from 'hardhat'
 
 import { ZERO_ADDRESS } from '../../common/constants'
 import { bn } from '../../common/numbers'
-import { AAVEAssetP0 } from '../../typechain/AAVEAssetP0'
-import { AaveLendingPoolMockP0 } from '../../typechain/AaveLendingPoolMockP0'
-import { COMPAssetP0 } from '../../typechain/COMPAssetP0'
-import { ComptrollerMockP0 } from '../../typechain/ComptrollerMockP0'
+import { AaveOracle } from '../../typechain/AaveOracle'
+import { AssetP0 } from '../../typechain/AssetP0'
+import { CompoundOracle } from '../../typechain/CompoundOracle'
 import { DeployerP0 } from '../../typechain/DeployerP0'
 import { ERC20Mock } from '../../typechain/ERC20Mock'
 import { FurnaceP0 } from '../../typechain/FurnaceP0'
 import { MainP0 } from '../../typechain/MainP0'
-import { RSRAssetP0 } from '../../typechain/RSRAssetP0'
+import { MarketMock } from '../../typechain/MarketMock'
 import { RTokenAssetP0 } from '../../typechain/RTokenAssetP0'
 import { RTokenP0 } from '../../typechain/RTokenP0'
 import { StRSRP0 } from '../../typechain/StRSRP0'
@@ -34,13 +33,18 @@ describe('DeployerP0 contract', () => {
 
   // RSR
   let rsr: ERC20Mock
-  let rsrAsset: RSRAssetP0
+  let rsrAsset: AssetP0
 
   // AAVE and Compound
-  let compAsset: COMPAssetP0
-  let compoundMock: ComptrollerMockP0
-  let aaveAsset: AAVEAssetP0
-  let aaveMock: AaveLendingPoolMockP0
+  let compToken: ERC20Mock
+  let compAsset: AssetP0
+  let compoundOracle: CompoundOracle
+  let aaveToken: ERC20Mock
+  let aaveAsset: AssetP0
+  let aaveOracle: AaveOracle
+
+  // Market
+  let market: MarketMock
 
   // Config values
   let config: IConfig
@@ -48,6 +52,7 @@ describe('DeployerP0 contract', () => {
 
   // Contracts to retrieve after deploy
   let rToken: RTokenP0
+  let rTokenAsset: RTokenAssetP0
   let stRSR: StRSRP0
   let furnace: FurnaceP0
   let main: MainP0
@@ -67,10 +72,12 @@ describe('DeployerP0 contract', () => {
     ;({
       rsr,
       rsrAsset,
+      compToken,
+      aaveToken,
       compAsset,
       aaveAsset,
-      compoundMock,
-      aaveMock,
+      compoundOracle,
+      aaveOracle,
       collateral,
       vault,
       config,
@@ -78,27 +85,65 @@ describe('DeployerP0 contract', () => {
       deployer,
       main,
       rToken,
+      rTokenAsset,
       furnace,
       stRSR,
+      market,
     } = await loadFixture(defaultFixture))
   })
 
   describe('Deployment', () => {
-    it('Should deploy contracts', async () => {
-      // Contracts deployed
-      expect(main.address).not.to.equal(ZERO_ADDRESS)
-      expect(rToken.address).not.to.equal(ZERO_ADDRESS)
-      expect(furnace.address).not.to.equal(ZERO_ADDRESS)
-      expect(stRSR.address).not.to.equal(ZERO_ADDRESS)
+    it('Should setup values correctly', async () => {
+      expect(await deployer.rsr()).to.equal(rsr.address)
+      expect(await deployer.comp()).to.equal(compToken.address)
+      expect(await deployer.aave()).to.equal(aaveToken.address)
+      expect(await deployer.market()).to.equal(market.address)
+      expect(await deployer.compoundOracle()).to.equal(compoundOracle.address)
+      expect(await deployer.aaveOracle()).to.equal(aaveOracle.address)
+    })
+
+    it('Should deploy required contracts', async () => {
+      expect(main.address).to.not.equal(ZERO_ADDRESS)
+      expect(rsrAsset.address).to.not.equal(ZERO_ADDRESS)
+      expect(compAsset.address).to.not.equal(ZERO_ADDRESS)
+      expect(aaveAsset.address).to.not.equal(ZERO_ADDRESS)
+      expect(rToken.address).to.not.equal(ZERO_ADDRESS)
+      expect(rTokenAsset.address).to.not.equal(ZERO_ADDRESS)
+      expect(furnace.address).to.not.equal(ZERO_ADDRESS)
+      expect(stRSR.address).to.not.equal(ZERO_ADDRESS)
+      expect(vault.address).to.not.equal(ZERO_ADDRESS)
+    })
+
+    it('Should register deployment', async () => {
+      expect(await deployer.deployments(0)).to.equal(main.address)
     })
 
     it('Should setup Main correctly', async () => {
+      expect(await main.vault()).to.equal(vault.address)
       expect(await main.rsr()).to.equal(rsr.address)
-      expect((await main.oracle())[0]).to.equal(compoundMock.address)
-      const rTokenAsset = <RTokenAssetP0>(
-        await ethers.getContractAt('RTokenAssetP0', await main.rTokenAsset())
-      )
+
+      expect(await main.rsrAsset()).to.equal(rsrAsset.address)
+      expect(await rsrAsset.erc20()).to.equal(rsr.address)
+
+      expect(await main.compAsset()).to.equal(compAsset.address)
+      expect(await compAsset.erc20()).to.equal(compToken.address)
+
+      expect(await main.aaveAsset()).to.equal(aaveAsset.address)
+      expect(await aaveAsset.erc20()).to.equal(aaveToken.address)
+
+      expect(await main.rTokenAsset()).to.equal(rTokenAsset.address)
       expect(await rTokenAsset.erc20()).to.equal(rToken.address)
+
+      expect(await main.stRSR()).to.equal(stRSR.address)
+      expect(await main.revenueFurnace()).to.equal(furnace.address)
+
+      // Check assets
+      const allAssets = await main.allAssets()
+      expect(allAssets[0]).to.equal(rTokenAsset.address)
+      expect(allAssets[1]).to.equal(rsrAsset.address)
+      expect(allAssets[2]).to.equal(compAsset.address)
+      expect(allAssets[3]).to.equal(aaveAsset.address)
+      expect(allAssets.slice(4)).to.eql(collateral.map((c) => c.address))
     })
 
     it('Should setup RToken correctly', async () => {
@@ -119,24 +164,6 @@ describe('DeployerP0 contract', () => {
       expect(await stRSR.symbol()).to.equal('stRTKNRSR')
       expect(await stRSR.decimals()).to.equal(18)
       expect(await stRSR.totalSupply()).to.equal(0)
-    })
-
-    it('Should revert if Vault has unapproved collateral', async () => {
-      const approvedCollateral: string[] = [collateral[0].address]
-
-      await expect(
-        deployer.deploy(
-          'RTKN RToken',
-          'RTKN',
-          owner.address,
-          vault.address,
-          config,
-          dist,
-          compoundMock.address,
-          aaveMock.address,
-          approvedCollateral
-        )
-      ).to.be.revertedWith('UnapprovedCollateral()')
     })
   })
 })
