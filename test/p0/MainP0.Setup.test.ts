22c4ba014bd16fa9c2ce2c7cc662e520e3f156e3
JulianR <julianmrodri@gmail.com>
2022-01-25 11:21:32 -0300

    config tests for main

--- test/p0/MainP0.Setup.test.ts
+++ test/p0/MainP0.Setup.test.ts
@@ -24,7 +24,7 @@ import { RTokenAssetP0 } from '../../typechain/RTokenAssetP0'
 import { StaticATokenMock } from '../../typechain/StaticATokenMock'
 import { StRSRP0 } from '../../typechain/StRSRP0'
 import { USDCMock } from '../../typechain/USDCMock'
-import { advanceTime } from '../utils/time'
+import { advanceTime, getLatestBlockTimestamp } from '../utils/time'
 import { Collateral, defaultFixture, IConfig, IRevenueShare } from './utils/fixtures'
 
 const createFixtureLoader = waffle.createFixtureLoader
@@ -157,7 +157,35 @@ describe('MainP0 contract', () => {
       expect(await main.owner()).to.equal(owner.address)
       expect(await main.pauser()).to.equal(owner.address)
 
-      // Assets
+      // Assets and other components
+      expect(await main.rsrAsset()).to.equal(rsrAsset.address)
+      expect(await main.compAsset()).to.equal(compAsset.address)
+      expect(await main.aaveAsset()).to.equal(aaveAsset.address)
+      expect(await main.rTokenAsset()).to.equal(rTokenAsset.address)
+      expect(await main.stRSR()).to.equal(stRSR.address)
+      expect(await main.revenueFurnace()).to.equal(furnace.address)
+      expect(await main.market()).to.equal(market.address)
+
+      // Configuration
+      expect(await main.rsrCut()).to.equal(fp('0.6'))
+      expect(await main.rTokenCut()).to.equal(fp('0.4'))
+      expect(await main.rewardStart()).to.equal(config.rewardStart)
+      expect(await main.rewardPeriod()).to.equal(config.rewardPeriod)
+      expect(await main.auctionPeriod()).to.equal(config.auctionPeriod)
+      expect(await main.stRSRWithdrawalDelay()).to.equal(config.stRSRWithdrawalDelay)
+      expect(await main.defaultDelay()).to.equal(config.defaultDelay)
+      expect(await main.maxTradeSlippage()).to.equal(config.maxTradeSlippage)
+      expect(await main.maxAuctionSize()).to.equal(config.maxAuctionSize)
+      expect(await main.minRecapitalizationAuctionSize()).to.equal(
+        config.minRecapitalizationAuctionSize
+      )
+      expect(await main.minRevenueAuctionSize()).to.equal(config.minRevenueAuctionSize)
+      expect(await main.migrationChunk()).to.equal(config.migrationChunk)
+      expect(await main.issuanceRate()).to.equal(config.issuanceRate)
+      expect(await main.defaultThreshold()).to.equal(config.defaultThreshold)
+    })
+
+    it('Should register Assets correctly', async () => {
       // RSR
       expect(await main.rsrAsset()).to.equal(rsrAsset.address)
       expect(await rsrAsset.erc20()).to.equal(rsr.address)
@@ -183,35 +211,10 @@ describe('MainP0 contract', () => {
       expect(allAssets[2]).to.equal(compAsset.address)
       expect(allAssets[3]).to.equal(aaveAsset.address)
       expect(allAssets.slice(4)).to.eql(collateral.map((c) => c.address))
+    })
 
-      // Other components
-      expect(await main.stRSR()).to.equal(stRSR.address)
-      expect(await main.revenueFurnace()).to.equal(furnace.address)
-
-      expect(await main.rsrCut()).to.equal(fp('0.6'))
-      expect(await main.rTokenCut()).to.equal(fp('0.4'))
-      expect(await main.rewardStart()).to.equal(config.rewardStart)
-      expect(await main.rewardPeriod()).to.equal(config.rewardPeriod)
-      expect(await main.auctionPeriod()).to.equal(config.auctionPeriod)
-      expect(await main.stRSRWithdrawalDelay()).to.equal(config.stRSRWithdrawalDelay)
-      expect(await main.defaultDelay()).to.equal(config.defaultDelay)
-      expect(await main.maxTradeSlippage()).to.equal(config.maxTradeSlippage)
-      expect(await main.maxAuctionSize()).to.equal(config.maxAuctionSize)
-      expect(await main.minRecapitalizationAuctionSize()).to.equal(
-        config.minRecapitalizationAuctionSize
-      )
-      expect(await main.minRevenueAuctionSize()).to.equal(config.minRevenueAuctionSize)
-      expect(await main.migrationChunk()).to.equal(config.migrationChunk)
-      expect(await main.issuanceRate()).to.equal(config.issuanceRate)
-      expect(await main.defaultThreshold()).to.equal(config.defaultThreshold)
-
-      expect(await rTokenAsset.erc20()).to.equal(rToken.address)
-      expect(await main.rsrAsset()).to.equal(rsrAsset.address)
-      expect(await main.compAsset()).to.equal(compAsset.address)
-      expect(await main.aaveAsset()).to.equal(aaveAsset.address)
-      expect(await main.market()).to.equal(market.address)
-      expect(await main.rToken()).to.equal(rToken.address)
-      expect(await main.rsr()).to.equal(rsr.address)
+    it('Should register Basket correctly', async () => {
+      // Basket
       expect(await main.fullyCapitalized()).to.equal(true)
       expect((await main.backingTokens()).length).to.equal(4)
     })
@@ -292,8 +295,262 @@ describe('MainP0 contract', () => {
     })
   })
 
-  describe('Configuration/Mood', () => {
-    // TODO: Check that owner can set settings
+  describe.only('Configuration/State', () => {
+    it('Should allow to update rewardStart if Owner', async () => {
+      const newValue: BigNumber = bn(await getLatestBlockTimestamp())
+
+      // Check existing value
+      expect(await main.rewardStart()).to.equal(config.rewardStart)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setRewardStart(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.rewardStart()).to.equal(config.rewardStart)
+
+      // Update with owner
+      await main.connect(owner).setRewardStart(newValue)
+
+      // Check value was updated
+      expect(await main.rewardStart()).to.equal(newValue)
+    })
+
+    it.skip('Should allow to update rewardPeriod if Owner', async () => {
+      const newValue: BigNumber = bn('360')
+
+      // Check existing value
+      expect(await main.rewardPeriod()).to.equal(config.rewardPeriod)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setRewardPeriod(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.rewardPeriod()).to.equal(config.rewardPeriod)
+
+      // Update with owner
+      await main.connect(owner).setRewardPeriod(newValue)
+
+      // Check value was updated
+      expect(await main.rewardPeriod()).to.equal(newValue)
+    })
+
+    it('Should allow to update auctionPeriod if Owner', async () => {
+      const newValue: BigNumber = bn('360')
+
+      // Check existing value
+      expect(await main.auctionPeriod()).to.equal(config.auctionPeriod)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setAuctionPeriod(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.auctionPeriod()).to.equal(config.auctionPeriod)
+
+      // Update with owner
+      await main.connect(owner).setAuctionPeriod(newValue)
+
+      // Check value was updated
+      expect(await main.auctionPeriod()).to.equal(newValue)
+    })
+
+    it('Should allow to update stRSRWithdrawalDelay if Owner', async () => {
+      const newValue: BigNumber = bn('360')
+
+      // Check existing value
+      expect(await main.stRSRWithdrawalDelay()).to.equal(config.stRSRWithdrawalDelay)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setStRSRWithdrawalDelay(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.stRSRWithdrawalDelay()).to.equal(config.stRSRWithdrawalDelay)
+
+      // Update with owner
+      await main.connect(owner).setStRSRWithdrawalDelay(newValue)
+
+      // Check value was updated
+      expect(await main.stRSRWithdrawalDelay()).to.equal(newValue)
+    })
+
+    it('Should allow to update defaultDelay if Owner', async () => {
+      const newValue: BigNumber = bn('360')
+
+      // Check existing value
+      expect(await main.defaultDelay()).to.equal(config.defaultDelay)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setDefaultDelay(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.defaultDelay()).to.equal(config.defaultDelay)
+
+      // Update with owner
+      await main.connect(owner).setDefaultDelay(newValue)
+
+      // Check value was updated
+      expect(await main.defaultDelay()).to.equal(newValue)
+    })
+
+    it('Should allow to update maxTradeSlippage if Owner', async () => {
+      const newValue: BigNumber = fp('0.02')
+
+      // Check existing value
+      expect(await main.maxTradeSlippage()).to.equal(config.maxTradeSlippage)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setMaxTradeSlippage(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.maxTradeSlippage()).to.equal(config.maxTradeSlippage)
+
+      // Update with owner
+      await main.connect(owner).setMaxTradeSlippage(newValue)
+
+      // Check value was updated
+      expect(await main.maxTradeSlippage()).to.equal(newValue)
+    })
+
+    it('Should allow to update maxAuctionSize if Owner', async () => {
+      const newValue: BigNumber = fp('0.02')
+
+      // Check existing value
+      expect(await main.maxAuctionSize()).to.equal(config.maxAuctionSize)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setMaxAuctionSize(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.maxAuctionSize()).to.equal(config.maxAuctionSize)
+
+      // Update with owner
+      await main.connect(owner).setMaxAuctionSize(newValue)
+
+      // Check value was updated
+      expect(await main.maxAuctionSize()).to.equal(newValue)
+    })
+
+    it('Should allow to update minRecapitalizationAuctionSize if Owner', async () => {
+      const newValue: BigNumber = fp('0.02')
+
+      // Check existing value
+      expect(await main.minRecapitalizationAuctionSize()).to.equal(
+        config.minRecapitalizationAuctionSize
+      )
+
+      // If not owner cannot update
+      await expect(
+        main.connect(other).setMinRecapitalizationAuctionSize(newValue)
+      ).to.be.revertedWith('Ownable: caller is not the owner')
+
+      // Check value did not change
+      expect(await main.minRecapitalizationAuctionSize()).to.equal(
+        config.minRecapitalizationAuctionSize
+      )
+
+      // Update with owner
+      await main.connect(owner).setMinRecapitalizationAuctionSize(newValue)
+
+      // Check value was updated
+      expect(await main.minRecapitalizationAuctionSize()).to.equal(newValue)
+    })
+
+    it('Should allow to update minRevenueAuctionSize if Owner', async () => {
+      const newValue: BigNumber = fp('0.02')
+
+      // Check existing value
+      expect(await main.minRevenueAuctionSize()).to.equal(config.minRevenueAuctionSize)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setMinRevenueAuctionSize(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.minRevenueAuctionSize()).to.equal(config.minRevenueAuctionSize)
+
+      // Update with owner
+      await main.connect(owner).setMinRevenueAuctionSize(newValue)
+
+      // Check value was updated
+      expect(await main.minRevenueAuctionSize()).to.equal(newValue)
+    })
+
+    it('Should allow to update migrationChunk if Owner', async () => {
+      const newValue: BigNumber = fp('0.5')
+
+      // Check existing value
+      expect(await main.migrationChunk()).to.equal(config.migrationChunk)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setMigrationChunk(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.migrationChunk()).to.equal(config.migrationChunk)
+
+      // Update with owner
+      await main.connect(owner).setMigrationChunk(newValue)
+
+      // Check value was updated
+      expect(await main.migrationChunk()).to.equal(newValue)
+    })
+
+    it('Should allow to update issuanceRate if Owner', async () => {
+      const newValue: BigNumber = fp('0.1')
+
+      // Check existing value
+      expect(await main.issuanceRate()).to.equal(config.issuanceRate)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setIssuanceRate(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.issuanceRate()).to.equal(config.issuanceRate)
+
+      // Update with owner
+      await main.connect(owner).setIssuanceRate(newValue)
+
+      // Check value was updated
+      expect(await main.issuanceRate()).to.equal(newValue)
+    })
+
+    it('Should allow to update defaultThreshold if Owner', async () => {
+      const newValue: BigNumber = fp('0.1')
+
+      // Check existing value
+      expect(await main.defaultThreshold()).to.equal(config.defaultThreshold)
+
+      // If not owner cannot update
+      await expect(main.connect(other).setDefaultThreshold(newValue)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+
+      // Check value did not change
+      expect(await main.defaultThreshold()).to.equal(config.defaultThreshold)
+
+      // Update with owner
+      await main.connect(owner).setDefaultThreshold(newValue)
+
+      // Check value was updated
+      expect(await main.defaultThreshold()).to.equal(newValue)
+    })
 
     it('Should return nextRewards correctly', async () => {
       // Check next immediate reward
