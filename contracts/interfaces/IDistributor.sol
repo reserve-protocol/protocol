1d5e1321c46a596be5f3a2a45ed6f2a278b80053
Taylor Brent <taylor.w.brent@gmail.com>
2022-03-14 11:58:58 -0400

    IDistributor

--- contracts/interfaces/IDistributor.sol
+++ contracts/interfaces/IDistributor.sol
@@ -5,10 +5,15 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./IComponent.sol";
 
 struct RevenueShare {
-    uint16 rTokenDist;
-    uint16 rsrDist;
+    uint16 rTokenDist; // {revShare}
+    uint16 rsrDist; // {revShare}
 }
 
+/**
+ * @title IDistributor
+ * @notice The Distributor Component maintains a revenue distribution table that dictates
+ *   how to divide revenue across the Furnace, StRSR, and any other destinations.
+ */
 interface IDistributor is IComponent {
     /// Emitted when a distribution is set
     /// @param dest The address set to receive the distribution
@@ -18,11 +23,14 @@ interface IDistributor is IComponent {
 
     function setDistribution(address dest, RevenueShare memory share) external;
 
+    /// Distribute the `erc20` token across all revenue destinations according to the table
     function distribute(
         IERC20 erc20,
         address from,
         uint256 amount
     ) external;
 
+    /// @return rTokenTotal {revShare} The total of all RToken destinations
+    /// @return rsrTotal {revShare} The total of all RSR destinations
     function totals() external view returns (uint256 rTokenTotal, uint256 rsrTotal);
 }
