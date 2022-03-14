1c477077f17ff5774c9f1b70b5626002e3636d20
Taylor Brent <taylor.w.brent@gmail.com>
2022-03-14 19:21:55 -0400

    IRevenueTrader

--- contracts/interfaces/IRevenueTrader.sol
+++ contracts/interfaces/IRevenueTrader.sol
@@ -4,6 +4,13 @@ pragma solidity 0.8.9;
 import "./IComponent.sol";
 import "./ITrading.sol";
 
+/**
+ * @title IRevenueTrader
+ * @notice The RevenueTrader is an extension of the trading mixin that trades all
+ *   assets at its address for a single target asset. There are two runtime instances
+ *   of the RevenueTrader, 1 for RToken and 1 for RSR.
+ */
 interface IRevenueTrader is IComponent, ITrading {
+    /// Run the central RevenueTrader loop; unpermissioned
     function manageFunds() external;
 }
