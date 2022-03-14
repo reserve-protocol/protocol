4803ee2c5b8336e971de13867eee1e4374a2cbfd
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-03-14 11:01:31 -0300

    trade aux functions and interfaces

---
+++ test/p0/utils/trades.ts
@@ -0,0 +1,29 @@
+import { BigNumber } from 'ethers'
+import { ethers } from 'hardhat'
+import { expect } from 'chai'
+import { TradingP0, GnosisTrade } from '../../../typechain'
+
+export const expectTrade = async (
+  trader: TradingP0,
+  index: number,
+  auctionInfo: Partial<TradeInfo>
+) => {
+  const trade = await getTrade(trader, index)
+  expect(await trade.sell()).to.equal(auctionInfo.sell)
+  expect(await trade.buy()).to.equal(auctionInfo.buy)
+  expect(await trade.endTime()).to.equal(auctionInfo.endTime)
+  expect(await trade.auctionId()).to.equal(auctionInfo.externalId)
+}
+
+// TODO use this in more places
+export const getTrade = async (trader: TradingP0, index: number): Promise<GnosisTrade> => {
+  const tradeAddr = await trader.trades(index)
+  return await ethers.getContractAt('GnosisTrade', tradeAddr)
+}
+
+export interface TradeInfo {
+  sell: string
+  buy: string
+  endTime: number
+  externalId: BigNumber
+}
