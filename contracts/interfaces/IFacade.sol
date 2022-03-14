35fe94281f335ee081cfa2c8f0418d1ed6142650
Taylor Brent <taylor.w.brent@gmail.com>
2022-03-14 18:52:09 -0400

    IFacade

--- contracts/interfaces/IFacade.sol
+++ contracts/interfaces/IFacade.sol
@@ -6,22 +6,40 @@ import "./IRToken.sol";
 
 /**
  * @title IFacade
- * @notice A read-only layer on top of the protocol for use from an off-chain explorer.
+ * @notice A UX-friendly layer for non-governance protocol interactions
+ *
+ * @dev
+ * - @custom:bundle-action - Bundle multiple transactions to make sure they run on the same block
+ * - @custom:static-call - Use ethers callStatic() in order to get result after update
+ * - @custom:view - Just expose a abstraction layer for getting protocol view data
  */
 interface IFacade {
+    /// Prompt all traders to run auctions
+    /// @custom:bundle-action
     function runAuctionsForAllTraders() external;
 
+    /// Prompt all traders and the RToken itself to claim rewards and sweep to BackingManager
+    /// @custom:bundle-action
     function claimRewards() external;
 
     /// @return How many RToken `account` can issue given current holdings
+    /// @custom:static-call
     function maxIssuable(address account) external returns (uint256);
 
-    /// @return tokens The addresses of the ERC20s backing the RToken
-    function basketTokens() external view returns (address[] memory tokens);
-
+    /// @return tokens Array of all known ERC20 asset addreses.
+    /// @return amounts {qTok} Array of balance that the protocol holds of this current asset
+    /// @custom:static-call
     function currentAssets() external returns (address[] memory tokens, uint256[] memory amounts);
 
+    /// @return The exchange rate between StRSR and RSR as a Fix
+    /// @custom:static-call
     function stRSRExchangeRate() external returns (Fix);
 
+    /// @return total {UoA} An estimate of the total value of all assets held at BackingManager
+    /// @custom:static-call
     function totalAssetValue() external returns (Fix total);
+
+    /// @return tokens The addresses of the ERC20s backing the RToken
+    /// @custom:view
+    function basketTokens() external view returns (address[] memory tokens);
 }
--- contracts/p0/aux/Facade.sol
+++ contracts/p0/aux/Facade.sol
@@ -12,13 +12,12 @@ import "contracts/libraries/Fixed.sol";
 
 /**
  * @title FacadeP0
- * @notice A UX-friendly layer that for all the non-governance protocol functions
+ * @notice A UX-friendly layer for non-governance protocol interactions
  *
- * Function types:
- * - `passthrough` - Make clear what actions are available (Cheaper to call the functions directly)
- * - `bundleAction` - Bundle multiple transactions to make sure they run on the same block
- * - `staticCall` - Change the state of the protocol and get a result for free (use staticCall)
- * - `view` - Just expose a abstraction layer for getting protocol view data
+ * @dev
+ * - @custom:bundle-action - Bundle multiple transactions to make sure they run on the same block
+ * - @custom:static-call - Use ethers callStatic() in order to get result after update
+ * - @custom:view - Just expose a abstraction layer for getting protocol view data
  */
 contract FacadeP0 is IFacade {
     using FixLib for Fix;
@@ -29,14 +28,16 @@ contract FacadeP0 is IFacade {
         main = MainP0(main_);
     }
 
-    /// `bundleAction`
+    /// Prompt all traders to run auctions
+    /// @custom:bundle-action
     function runAuctionsForAllTraders() external {
         main.backingManager().manageFunds();
         main.rsrTrader().manageFunds();
         main.rTokenTrader().manageFunds();
     }
 
-    /// `bundleAction`
+    /// Prompt all traders and the RToken itself to claim rewards and sweep to BackingManager
+    /// @custom:bundle-action
     function claimRewards() external {
         main.backingManager().claimAndSweepRewards();
         main.rsrTrader().claimAndSweepRewards();
@@ -44,21 +45,16 @@ contract FacadeP0 is IFacade {
         main.rToken().claimAndSweepRewards();
     }
 
-    /// @return tokens The addresses of the ERC20s backing the RToken
-    function basketTokens() external view returns (address[] memory tokens) {
-        (tokens, ) = main.basketHandler().quote(FIX_ONE, RoundingApproach.ROUND);
-    }
-
-    /// `staticCall`
     /// @return How many RToken `account` can issue given current holdings
+    /// @custom:static-call
     function maxIssuable(address account) external returns (uint256) {
         main.poke();
         return main.rToken().maxIssuable(account);
     }
 
-    /// `staticCall`
-    /// @return tokens Array of all know ERC20 asset addreses
-    /// @return amounts Array of balance {qTok} that the protocol holds of this current asset
+    /// @return tokens Array of all known ERC20 asset addreses.
+    /// @return amounts {qTok} Array of balance that the protocol holds of this current asset
+    /// @custom:static-call
     function currentAssets() external returns (address[] memory tokens, uint256[] memory amounts) {
         main.poke();
         IAssetRegistry reg = main.assetRegistry();
@@ -73,14 +69,15 @@ contract FacadeP0 is IFacade {
         }
     }
 
-    /// `staticCall`
+    /// @return The exchange rate between StRSR and RSR as a Fix
+    /// @custom:static-call
     function stRSRExchangeRate() external returns (Fix) {
         main.poke();
         return main.stRSR().exchangeRate();
     }
 
-    /// `staticCall`
-    /// @return total {UoA} An estimate of the total value of all assets held
+    /// @return total {UoA} An estimate of the total value of all assets held at BackingManager
+    /// @custom:static-call
     function totalAssetValue() external returns (Fix total) {
         main.poke();
         IAssetRegistry reg = main.assetRegistry();
@@ -96,5 +93,11 @@ contract FacadeP0 is IFacade {
                 total = total.plus(asset.bal(backingManager).mul(asset.price()));
             }
         }
+    }
+
+    /// @return tokens The addresses of the ERC20s backing the RToken
+    /// @custom:view
+    function basketTokens() external view returns (address[] memory tokens) {
+        (tokens, ) = main.basketHandler().quote(FIX_ONE, RoundingApproach.ROUND);
     }
 }
