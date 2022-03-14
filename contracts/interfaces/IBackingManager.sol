5bf0dfd2794a3f5e0adac6ca257b87257f186804
Taylor Brent <taylor.w.brent@gmail.com>
2022-03-14 11:32:55 -0400

    BackingManager

--- contracts/interfaces/IBackingManager.sol
+++ contracts/interfaces/IBackingManager.sol
@@ -6,11 +6,20 @@ import "contracts/libraries/Fixed.sol";
 import "./IComponent.sol";
 import "./ITrading.sol";
 
+/**
+ * @title IBackingManager
+ * @notice The BackingManager handles changes in the ERC20 balances that back an RToken.
+ *   - It computes which trades to perform, if any, and initiates these trades with the Broker.
+ *   - If already capitalized, excess assets are transferred to RevenueTraders.
+ */
 interface IBackingManager is IComponent, ITrading {
     event AuctionDelaySet(uint256 indexed oldVal, uint256 indexed newVal);
     event BackingBufferSet(Fix indexed oldVal, Fix indexed newVal);
 
+    // Give RToken max allowances over all registered tokens
     function grantAllowances() external;
 
+    /// Manage backing funds: maintain the overall backing policy
+    /// Collective Action
     function manageFunds() external;
 }
