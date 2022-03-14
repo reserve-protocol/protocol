bf73ff46c30a4023dd1c01ab0d7143313f186426
Taylor Brent <taylor.w.brent@gmail.com>
2022-03-14 19:24:53 -0400

    IRewardable

--- contracts/interfaces/IRewardable.sol
+++ contracts/interfaces/IRewardable.sol
@@ -4,9 +4,14 @@ pragma solidity 0.8.9;
 import "./IComponent.sol";
 import "./IMain.sol";
 
+/**
+ * @title IRewardable
+ * @notice A simple component mixin to support claiming + monetization of COMP/AAVE rewards
+ */
 interface IRewardable is IComponent {
     /// Emitted whenever rewards are claimed
     event RewardsClaimed(address indexed erc20, uint256 indexed amount);
 
+    /// Claim reward tokens from integrated defi protocols such as Compound/Aave
     function claimAndSweepRewards() external;
 }
