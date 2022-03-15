009a40bebc1338e332676c04e1aeae08caae3a24
Taylor Brent <taylor.w.brent@gmail.com>
2022-03-14 21:21:18 -0400

    ITrade

--- contracts/interfaces/ITrade.sol
+++ contracts/interfaces/ITrade.sol
@@ -3,17 +3,19 @@ pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// Independent of trading marketplace all oneshot trade contracts must adhere to this interface
+/// All Trade contracts deployed by the Broker must adhere to this interface.
 interface ITrade {
     function sell() external view returns (IERC20);
 
     function buy() external view returns (IERC20);
 
+    /// @return The timestamp at which the trade is projected to become settle-able
     function endTime() external view returns (uint256);
 
-    /// @return True if the trade can be settled; should be guaranteed to be true eventually
+    /// @return True if the trade can be settled
+    /// @dev Should be guaranteed to be true eventually as an invariant
     function canSettle() external view returns (bool);
 
-    /// Settle trade, transfer tokens to trader, and report bad trade if needed
+    /// Complete the trade and transfer tokens back to the origin trader
     function settle() external returns (uint256 soldAmt, uint256 boughtAmt);
 }
