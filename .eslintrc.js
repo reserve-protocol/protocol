d17bc35126abaec100dca1de7192d193212c6728
Matt Elder <matt.elder@reserve.org>
2022-04-01 14:25:26 -0700

    eslint reconfig (to work for the moment)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     node: true,
   },
   plugins: ['@typescript-eslint'],
-  extends: ['standard', 'plugin:prettier/recommended', 'plugin:node/recommended'],
+  extends: ['standard', 'plugin:node/recommended'], //, 'plugin:prettier/recommended', ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
