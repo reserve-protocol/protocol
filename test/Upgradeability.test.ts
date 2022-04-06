b120d0444c9a210e011eb4126b0e3861e5d34a5c
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-04-06 12:41:51 -0300

    Main upgradeability test

--- test/Upgradeability.test.ts
+++ test/Upgradeability.test.ts
@@ -18,6 +18,7 @@ import {
   GnosisMock,
   IBasketHandler,
   MainP1,
+  MainP1V2,
   RevenueTradingP1,
   RTokenAsset,
   RTokenP1,
@@ -72,6 +73,18 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
   let rTokenTrader: TestIRevenueTrader
   let tradingLib: TradingLibP1
 
+  // Factories
+  let MainFactory: ContractFactory
+  let RTokenFactory: ContractFactory
+  let FurnaceFactory: ContractFactory
+  let RevenueTraderFactory: ContractFactory
+  let BackingManagerFactory: ContractFactory
+  let AssetRegistryFactory: ContractFactory
+  let BasketHandlerFactory: ContractFactory
+  let DistributorFactory: ContractFactory
+  let BrokerFactory: ContractFactory
+  let StRSRFactory: ContractFactory
+
   let loadFixture: ReturnType<typeof createFixtureLoader>
   let wallet: Wallet
 
@@ -108,6 +121,36 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
     // Deploy TradingLib external library
     const TradingLibFactory: ContractFactory = await ethers.getContractFactory('TradingLibP1')
     tradingLib = <TradingLibP1>await TradingLibFactory.deploy()
+
+    // Setup factories
+    MainFactory = await ethers.getContractFactory('MainP1')
+    RTokenFactory = await ethers.getContractFactory('RTokenP1')
+    FurnaceFactory = await ethers.getContractFactory('FurnaceP1')
+    RevenueTraderFactory = await ethers.getContractFactory('RevenueTradingP1', {
+      libraries: { TradingLibP1: tradingLib.address },
+    })
+    BackingManagerFactory = await ethers.getContractFactory('BackingManagerP1', {
+      libraries: { TradingLibP1: tradingLib.address },
+    })
+    AssetRegistryFactory = await ethers.getContractFactory('AssetRegistryP1')
+
+    BasketHandlerFactory = await ethers.getContractFactory('BasketHandlerP1')
+    DistributorFactory = await ethers.getContractFactory('DistributorP1')
+    BrokerFactory = await ethers.getContractFactory('BrokerP1')
+    StRSRFactory = await ethers.getContractFactory('StRSRP1')
+
+    // Import deployed proxies
+    await upgrades.forceImport(main.address, MainFactory)
+    await upgrades.forceImport(rToken.address, RTokenFactory)
+    await upgrades.forceImport(furnace.address, FurnaceFactory)
+    await upgrades.forceImport(rsrTrader.address, RevenueTraderFactory)
+    await upgrades.forceImport(rTokenTrader.address, RevenueTraderFactory)
+    await upgrades.forceImport(backingManager.address, BackingManagerFactory)
+    await upgrades.forceImport(assetRegistry.address, AssetRegistryFactory)
+    await upgrades.forceImport(basketHandler.address, BasketHandlerFactory)
+    await upgrades.forceImport(distributor.address, DistributorFactory)
+    await upgrades.forceImport(broker.address, BrokerFactory)
+    await upgrades.forceImport(stRSR.address, StRSRFactory)
   })
 
   describe('Implementations', () => {
@@ -125,7 +168,6 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
         rTokenTrader: rTokenTrader.address,
       }
 
-      const MainFactory: ContractFactory = await ethers.getContractFactory('MainP1')
       const newMain: MainP1 = <MainP1>await upgrades.deployProxy(
         MainFactory,
         [components, rsr.address],
@@ -155,7 +197,6 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
     })
 
     it('Should deploy valid implementation - RToken', async () => {
-      const RTokenFactory: ContractFactory = await ethers.getContractFactory('RTokenP1')
       const newRToken: RTokenP1 = <RTokenP1>await upgrades.deployProxy(
         RTokenFactory,
         [main.address, 'RTKN RToken', 'RTKN', 'newConstitution', config.issuanceRate],
@@ -175,7 +216,6 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
     })
 
     it('Should deploy valid implementation - Furnace', async () => {
-      const FurnaceFactory: ContractFactory = await ethers.getContractFactory('FurnaceP1')
       const newFurnace: FurnaceP1 = <FurnaceP1>await upgrades.deployProxy(
         FurnaceFactory,
         [main.address, config.rewardPeriod, config.rewardRatio],
@@ -193,10 +233,6 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
     })
 
     it('Should deploy valid implementation - RevenueTrader', async () => {
-      const RevenueTraderFactory: ContractFactory = await ethers.getContractFactory(
-        'RevenueTradingP1',
-        { libraries: { TradingLibP1: tradingLib.address } }
-      )
       const newRevenueTrader: RevenueTradingP1 = <RevenueTradingP1>await upgrades.deployProxy(
         RevenueTraderFactory,
         [main.address, rsr.address, config.maxTradeSlippage, config.dustAmount],
@@ -215,10 +251,6 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
     })
 
     it('Should deploy valid implementation - BackingManager', async () => {
-      const BackingManagerFactory: ContractFactory = await ethers.getContractFactory(
-        'BackingManagerP1',
-        { libraries: { TradingLibP1: tradingLib.address } }
-      )
       const newBackingMgr: BackingManagerP1 = <BackingManagerP1>await upgrades.deployProxy(
         BackingManagerFactory,
         [
@@ -244,9 +276,6 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
     })
 
     it('Should deploy valid implementation - AssetRegistry', async () => {
-      const AssetRegistryFactory: ContractFactory = await ethers.getContractFactory(
-        'AssetRegistryP1'
-      )
       const newAssetRegistry: AssetRegistryP1 = <AssetRegistryP1>await upgrades.deployProxy(
         AssetRegistryFactory,
         [main.address, [rsrAsset.address, rTokenAsset.address]],
@@ -263,9 +292,6 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
     })
 
     it('Should deploy valid implementation - BasketHandler', async () => {
-      const BasketHandlerFactory: ContractFactory = await ethers.getContractFactory(
-        'BasketHandlerP1'
-      )
       const newBasketHandler: BasketHandlerP1 = <BasketHandlerP1>await upgrades.deployProxy(
         BasketHandlerFactory,
         [main.address],
@@ -280,7 +306,6 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
     })
 
     it('Should deploy valid implementation - Distributor', async () => {
-      const DistributorFactory: ContractFactory = await ethers.getContractFactory('DistributorP1')
       const newDistributor: DistributorP1 = <DistributorP1>await upgrades.deployProxy(
         DistributorFactory,
         [main.address, config.dist],
@@ -298,7 +323,6 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
     })
 
     it('Should deploy valid implementation - Broker', async () => {
-      const BrokerFactory: ContractFactory = await ethers.getContractFactory('BrokerP1')
       const newBroker: BrokerP1 = <BrokerP1>await upgrades.deployProxy(
         BrokerFactory,
         [main.address, gnosis.address, config.auctionLength],
@@ -316,7 +340,6 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
     })
 
     it('Should deploy valid implementation - StRSR', async () => {
-      const StRSRFactory: ContractFactory = await ethers.getContractFactory('StRSRP1')
       const newStRSR: StRSRP1 = <StRSRP1>await upgrades.deployProxy(
         StRSRFactory,
         [
@@ -342,6 +365,45 @@ describeP1(`Upgradeability - P${IMPLEMENTATION}`, () => {
       expect(await newStRSR.rewardPeriod()).to.equal(config.rewardPeriod)
       expect(await newStRSR.rewardRatio()).to.equal(config.rewardRatio)
       expect(await newStRSR.main()).to.equal(main.address)
+    })
+  })
+
+  describe('Upgrades', () => {
+    it('Should upgrade correctly - Main', async () => {
+      // Upgrading
+      const MainV2Factory: ContractFactory = await ethers.getContractFactory('MainP1V2')
+      const mainV2: MainP1V2 = <MainP1V2>await upgrades.upgradeProxy(main.address, MainV2Factory)
+
+      // Check address is maintained
+      expect(mainV2.address).to.equal(main.address)
+
+      // Check state is preserved
+      expect(await mainV2.paused()).to.equal(false)
+      expect(await mainV2.owner()).to.equal(owner.address)
+      expect(await mainV2.pauser()).to.equal(owner.address)
+
+      // Components
+      expect(await mainV2.stRSR()).to.equal(stRSR.address)
+      expect(await mainV2.rToken()).to.equal(rToken.address)
+      expect(await mainV2.assetRegistry()).to.equal(assetRegistry.address)
+      expect(await mainV2.basketHandler()).to.equal(basketHandler.address)
+      expect(await mainV2.backingManager()).to.equal(backingManager.address)
+      expect(await mainV2.distributor()).to.equal(distributor.address)
+      expect(await mainV2.furnace()).to.equal(furnace.address)
+      expect(await mainV2.broker()).to.equal(broker.address)
+      expect(await mainV2.rsrTrader()).to.equal(rsrTrader.address)
+      expect(await mainV2.rTokenTrader()).to.equal(rTokenTrader.address)
+
+      // Check new version is implemented
+      expect(await mainV2.version()).to.equal('V2')
+
+      expect(await mainV2.newValue()).to.equal(0)
+      await mainV2.connect(owner).setNewValue(bn(1000))
+      expect(await mainV2.newValue()).to.equal(bn(1000))
+
+      // Call new poke - even if paused
+      await mainV2.connect(owner).pause()
+      await expect(mainV2.poke()).to.emit(mainV2, 'PokedV2')
     })
   })
 })
