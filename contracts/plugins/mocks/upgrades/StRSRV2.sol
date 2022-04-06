36623941702a6257b99a84b82d9298d8b4a3bb74
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-04-06 14:56:00 -0300

    additional mocks for upgrade tests

---
+++ contracts/plugins/mocks/upgrades/StRSRV2.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BlueOak-1.0.0
+pragma solidity 0.8.9;
+
+import "contracts/p1/StRSR.sol";
+
+contract StRSRP1V2 is StRSRP1 {
+    uint256 public newValue;
+
+    function setNewValue(uint256 newValue_) external onlyOwner {
+        newValue = newValue_;
+    }
+
+    function version() external pure returns (string memory) {
+        return "V2";
+    }
+}
