d5f798eef3893afbae26c4e2da30fd6fcb510069
Matt Elder <matt.elder@reserve.org>
2022-04-29 15:06:43 -0700

    add --no-compile to test runners in fast-test

--- tools/fast-test.sh
+++ tools/fast-test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -euo pipefail
+#!/bin/bash -euxo pipefail
 # Run the subset of our unit tests tagged with '#fast'
 
 # cd to project root
@@ -8,8 +8,9 @@ export JOBS=3 # Fastest by actual test on 2022-04-28, though 2 and 4 are pretty 
 export ONLY_FAST=yes
 
 # Run these two test types in parallel
-PROTO_IMPL=0 yarn exec hardhat test $(find test -name '*.test.ts') & PID0=$!
-PROTO_IMPL=1 yarn exec hardhat test test/*.test.ts & PID1=$!
+yarn compile
+PROTO_IMPL=0 yarn exec hardhat test --no-compile $(find test -name '*.test.ts') & PID0=$!
+PROTO_IMPL=1 yarn exec hardhat test --no-compile test/*.test.ts & PID1=$!
 
 # Wait for both tests, and exit with error code if either failed
 wait $PID0 || exit $?
