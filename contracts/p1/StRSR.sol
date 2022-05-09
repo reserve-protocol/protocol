cf6cd5d3ba5c8a5fb4785d54fca84c090d4f6e9d
Taylor Brent <taylor.w.brent@gmail.com>
2022-05-08 21:49:41 -0400

    port StRSR to ERC20Votes

--- contracts/p1/StRSR.sol
+++ contracts/p1/StRSR.sol
@@ -1,14 +1,9 @@
 // SPDX-License-Identifier: BlueOak-1.0.0
 pragma solidity 0.8.9;
 
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "contracts/interfaces/IAsset.sol";
 import "contracts/interfaces/IStRSR.sol";
 import "contracts/interfaces/IMain.sol";
 import "contracts/libraries/Fixed.sol";
@@ -24,28 +19,9 @@ import "contracts/p1/mixins/Component.sol";
  * stakes that are in the process of being withdrawn and those that are not.
  */
 // solhint-disable max-states-count
-contract StRSRP1 is IStRSR, ComponentP1, EIP712Upgradeable {
+contract StRSRP1 is IStRSR, ERC20VotesUpgradeable, ComponentP1 {
     using SafeERC20Upgradeable for IERC20Upgradeable;
-    using EnumerableSet for EnumerableSet.AddressSet;
     using FixLib for int192;
-
-    // ==== ERC20Permit ====
-
-    using Counters for Counters.Counter;
-
-    mapping(address => Counters.Counter) private _nonces;
-
-    // solhint-disable-next-line var-name-mixedcase
-    bytes32 private constant _PERMIT_TYPEHASH =
-        keccak256(
-            "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
-        );
-
-    // ====
-
-    // Staking Token Name and Symbol
-    string private _name;
-    string private _symbol;
 
     // Era. If ever there's a total RSR wipeout, increment the era to zero old balances in one step.
     uint256 internal era;
@@ -64,9 +40,6 @@ contract StRSRP1 is IStRSR, ComponentP1, EIP712Upgradeable {
     mapping(uint256 => mapping(address => uint256)) public firstRemainingDraft;
     uint256 internal totalDrafts; // Total of all drafts {qDrafts}
     uint256 internal draftRSR; // Amount of RSR backing all drafts {qRSR}
-
-    // ERC20 allowances of stakes
-    mapping(address => mapping(address => uint256)) private allowances;
 
     // {qRSR} How much reward RSR was held the last time rewards were paid out
     uint256 internal rsrRewardsAtLastPayout;
@@ -96,9 +69,9 @@ contract StRSRP1 is IStRSR, ComponentP1, EIP712Upgradeable {
         int192 rewardRatio_
     ) external initializer {
         __Component_init(main_);
-        __EIP712_init(name_, "1");
-        _name = name_;
-        _symbol = symbol_;
+        __ERC20Permit_init(name_);
+        __ERC20_init(name_, symbol_);
+        __ERC20Votes_init();
         payoutLastPaid = uint32(block.timestamp);
         rsrRewardsAtLastPayout = main_.rsr().balanceOf(address(this));
         unstakingDelay = unstakingDelay_;
@@ -259,117 +232,8 @@ contract StRSRP1 is IStRSR, ComponentP1, EIP712Upgradeable {
         return right;
     }
 
-    // ==== ERC20 Interface ====
-    function name() external view returns (string memory) {
-        return _name;
-    }
-
-    function symbol() external view returns (string memory) {
-        return _symbol;
-    }
-
-    function decimals() public pure returns (uint8) {
-        return 18;
-    }
-
-    function totalSupply() external view returns (uint256) {
-        return totalStakes;
-    }
-
-    function balanceOf(address account) external view returns (uint256) {
-        return stakes[era][account];
-    }
-
-    function transfer(address to, uint256 amount) external returns (bool) {
-        _transfer(_msgSender(), to, amount);
-        return true;
-    }
-
-    function _transfer(
-        address from,
-        address to,
-        uint256 amount
-    ) private {
-        require(from != address(0), "ERC20: transfer from the zero address");
-        require(to != address(0), "ERC20: transfer to the zero address");
-
-        uint256 fromBalance = stakes[era][from];
-
-        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
-
-        unchecked {
-            stakes[era][from] = fromBalance - amount;
-        }
-
-        stakes[era][to] += amount;
-    }
-
-    function allowance(address owner_, address spender) public view returns (uint256) {
-        return allowances[owner_][spender];
-    }
-
-    function approve(address spender, uint256 amount) public returns (bool) {
-        _approve(_msgSender(), spender, amount);
-        return true;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) public returns (bool) {
-        _spendAllowance(from, _msgSender(), amount);
-        _transfer(from, to, amount);
-        return true;
-    }
-
-    function _approve(
-        address owner_,
-        address spender,
-        uint256 amount
-    ) private {
-        require(owner_ != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-
-        allowances[owner_][spender] = amount;
-
-        emit Approval(owner_, spender, amount);
-    }
-
-    function increaseAllowance(address spender, uint256 addedValue) external returns (bool) {
-        address owner_ = _msgSender();
-        _approve(owner_, spender, allowances[owner_][spender] + addedValue);
-        return true;
-    }
-
-    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
-        address owner_ = _msgSender();
-        uint256 currentAllowance = allowances[owner_][spender];
-        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
-        unchecked {
-            _approve(owner_, spender, currentAllowance - subtractedValue);
-        }
-
-        return true;
-    }
-
-    function _spendAllowance(
-        address owner_,
-        address spender,
-        uint256 amount
-    ) internal {
-        uint256 currentAllowance = allowance(owner_, spender);
-        if (currentAllowance != type(uint256).max) {
-            require(currentAllowance >= amount, "ERC20: insufficient allowance");
-            unchecked {
-                _approve(owner_, spender, currentAllowance - amount);
-            }
-        }
-    }
-
-    // ==== end ERC20 Interface ====
-
     // ==== Internal Functions ====
+
     /// @return {qRSR} The balance of RSR that this contract owns dedicated to future RSR rewards.
     function rsrRewards() internal view returns (uint256) {
         return main.rsr().balanceOf(address(this)) - stakeRSR - draftRSR;
@@ -468,50 +332,46 @@ contract StRSRP1 is IStRSR, ComponentP1, EIP712Upgradeable {
 
     // ==== end Internal Functions ====
 
-    // === ERC20Permit ====
-    // nonReentrant not required: no external calls
+    // ==== ERC20 Overrides ====
 
-    // From OZ 4.4 release at commit 6bd6b76
-
-    function permit(
-        address owner_,
-        address spender,
-        uint256 value,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) public {
-        require(block.timestamp <= deadline, "ERC20Permit: expired deadline");
-
-        bytes32 structHash = keccak256(
-            abi.encode(_PERMIT_TYPEHASH, owner_, spender, value, _useNonce(owner_), deadline)
-        );
-
-        bytes32 hash = _hashTypedDataV4(structHash);
-
-        address signer = ECDSAUpgradeable.recover(hash, v, r, s);
-        require(signer == owner_, "ERC20Permit: invalid signature");
-
-        _approve(owner_, spender, value);
+    function totalSupply()
+        public
+        view
+        override(ERC20Upgradeable, IERC20Upgradeable)
+        returns (uint256)
+    {
+        return totalStakes;
     }
 
-    function nonces(address owner_) public view returns (uint256) {
-        return _nonces[owner_].current();
+    function balanceOf(address account)
+        public
+        view
+        override(ERC20Upgradeable, IERC20Upgradeable)
+        returns (uint256)
+    {
+        return stakes[era][account];
     }
 
-    // solhint-disable-next-line func-name-mixedcase
-    function DOMAIN_SEPARATOR() external view returns (bytes32) {
-        return _domainSeparatorV4();
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+
+        uint256 fromBalance = stakes[era][from];
+
+        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
+
+        unchecked {
+            stakes[era][from] = fromBalance - amount;
+        }
+
+        stakes[era][to] += amount;
     }
 
-    function _useNonce(address owner_) internal returns (uint256 current) {
-        Counters.Counter storage nonce = _nonces[owner_];
-        current = nonce.current();
-        nonce.increment();
-    }
-
-    // ==== End ERC20Permit ====
+    // ==== endERC20 Overrides ====
 
     // ==== Gov Param Setters ====
 
