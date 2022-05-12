1da33695a679ae18ed567b627f60e90f7c876093
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-05-12 12:57:13 -0300

    fix formatting

--- contracts/libraries/Fixed.sol
+++ contracts/libraries/Fixed.sol
@@ -120,7 +120,6 @@ function divuu(uint256 x, uint256 y) pure returns (int192) {
     return _safeWrapu(mulDiv256(FIX_SCALE_U, x, y));
 }
 
-
 function fixMin(int192 x, int192 y) pure returns (int192) {
     return x < y ? x : y;
 }
