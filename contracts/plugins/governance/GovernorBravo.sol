6c1c5c27417df4f4357b7726ea6bd960df7b011b
Julian M. Rodriguez <56316686+julianmrodri@users.noreply.github.com>
2022-05-13 15:19:45 -0300

    Governor - Updateable settings (#242)
    * updateable settings
    
    * make proposalThreshold compatible with elastic supply tokens
    
    Co-authored-by: Taylor Brent <taylor.w.brent@gmail.com>

--- contracts/plugins/governance/GovernorBravo.sol
+++ contracts/plugins/governance/GovernorBravo.sol
@@ -1,58 +1,73 @@
 // SPDX-License-Identifier: BlueOak-1.0.0
 pragma solidity 0.8.9;
 
+import "@openzeppelin/contracts/governance/Governor.sol";
 import "@openzeppelin/contracts/governance/compatibility/GovernorCompatibilityBravo.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
-import "@openzeppelin/contracts/governance/Governor.sol";
 
 /*
  * @title Governance
  * @dev Decentralized Governance for the Reserve Protocol.
+ *
+ * Note that due to the elastic supply of StRSR, proposalThreshold is handled
+ *   very differently than the typical approach. It is in terms of micro %,
+ *   as is _getVotes().
  */
 contract Governance is
     Governor,
+    GovernorSettings,
+    GovernorCompatibilityBravo,
     GovernorVotes,
     GovernorVotesQuorumFraction,
-    GovernorCompatibilityBravo,
     GovernorTimelockControl
 {
-    uint256 public immutable _votingDelay;
-    uint256 public immutable _votingPeriod;
-
-    // TODO: Swap out ERC20Votes with our own custom InsurancePoolVotes. It should contain the
-    // functionality for both GovernorVotes + GovernorVotesQuorumFraction, and intimately
-    // the details of the InsurancePool.
+    // solhint-disable no-empty-blocks
     constructor(
         IVotes token_,
         TimelockController timelock_,
         uint256 votingDelay_, // in blocks
         uint256 votingPeriod_, // in blocks
+        uint256 proposalThresholdAsMicroPercent_, // e.g. 1e4 for 0.01%
         uint256 quorumPercent // e.g 4 for 4%
     )
         Governor("MyGovernor")
+        GovernorSettings(votingDelay_, votingPeriod_, proposalThresholdAsMicroPercent_)
         GovernorVotes(token_)
         GovernorVotesQuorumFraction(quorumPercent)
         GovernorTimelockControl(timelock_)
+    {}
+
+    // solhint-enable no-empty-blocks
+
+    function votingDelay() public view override(IGovernor, GovernorSettings) returns (uint256) {
+        return super.votingDelay();
+    }
+
+    function votingPeriod() public view override(IGovernor, GovernorSettings) returns (uint256) {
+        return super.votingPeriod();
+    }
+
+    /// @return The proposal threshold in units of micro %, e.g 1e6 for 1% of the supply
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
     {
-        _votingDelay = votingDelay_;
-        _votingPeriod = votingPeriod_;
+        return super.proposalThreshold();
     }
 
-    function votingDelay() public view override returns (uint256) {
-        return _votingDelay;
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(IGovernor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
     }
-
-    function votingPeriod() public view override returns (uint256) {
-        return _votingPeriod;
-    }
-
-    // Do we want a proposal threshold?
-    // function proposalThreshold() public pure override returns (uint256) {
-    //     // TODO: Integrate with InsurancePoolVotes
-    //     return 0e18;
-    // }
 
     function state(uint256 proposalId)
         public
@@ -69,7 +84,7 @@ contract Governance is
         bytes[] memory calldatas,
         string memory description
     ) public override(IGovernor, Governor, GovernorCompatibilityBravo) returns (uint256) {
-        return GovernorCompatibilityBravo.propose(targets, values, calldatas, description);
+        return super.propose(targets, values, calldatas, description);
     }
 
     function _execute(
@@ -79,8 +94,7 @@ contract Governance is
         bytes[] memory calldatas,
         bytes32 descriptionHash
     ) internal override(Governor, GovernorTimelockControl) {
-        // TODO: Maybe require sufficient stake here too?
-        GovernorTimelockControl._execute(proposalId, targets, values, calldatas, descriptionHash);
+        super._execute(proposalId, targets, values, calldatas, descriptionHash);
     }
 
     function _cancel(
@@ -89,7 +103,7 @@ contract Governance is
         bytes[] memory calldatas,
         bytes32 descriptionHash
     ) internal override(Governor, GovernorTimelockControl) returns (uint256) {
-        return GovernorTimelockControl._cancel(targets, values, calldatas, descriptionHash);
+        return super._cancel(targets, values, calldatas, descriptionHash);
     }
 
     function _executor()
@@ -98,7 +112,18 @@ contract Governance is
         override(Governor, GovernorTimelockControl)
         returns (address)
     {
-        return GovernorTimelockControl._executor();
+        return super._executor();
+    }
+
+    /// @return The percent of the StRSR supply the account has in terms of micro %: 1e5 = 0.1%
+    function _getVotes(
+        address account,
+        uint256 blockNumber,
+        bytes memory /*params*/
+    ) internal view override(Governor, GovernorVotes) returns (uint256) {
+        uint256 bal = token.getPastVotes(account, blockNumber);
+        uint256 totalSupply = token.getPastTotalSupply(blockNumber);
+        return (bal * 1e8) / totalSupply;
     }
 
     function supportsInterface(bytes4 interfaceId)
