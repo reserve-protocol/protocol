81b7b4e4594e5385dca39f91b06eb5f2c90b7ee0
Taylor Brent <taylor.w.brent@gmail.com>
2022-06-13 19:39:27 -0400

    add cWBTC collateral plugin CTokenPeggedCollateral

---
+++ contracts/plugins/assets/CTokenPeggedCollateral.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: BlueOak-1.0.0
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "contracts/plugins/assets/abstract/CompoundOracleMixin.sol";
+import "contracts/plugins/assets/CTokenFiatCollateral.sol";
+
+/**
+ * @title CTokenPeggedCollateral
+ * @notice Collateral plugin for a cToken of a pegged asset. For example:
+ *   - cWBTC
+ *   - ...
+ */
+contract CTokenPeggedCollateral is CompoundOracleMixin, Collateral {
+    using FixLib for uint192;
+    using SafeERC20 for IERC20Metadata;
+
+    // All cTokens have 8 decimals, but their underlying may have 18 or 6 or something else.
+
+    uint192 public prevReferencePrice; // previous rate, {collateral/reference}
+    IERC20 public override rewardERC20;
+
+    constructor(
+        IERC20Metadata erc20_,
+        uint192 maxTradeVolume_,
+        uint192 defaultThreshold_,
+        uint256 delayUntilDefault_,
+        IERC20Metadata referenceERC20_,
+        IComptroller comptroller_,
+        IERC20 rewardERC20_,
+        string memory targetName_
+    )
+        Collateral(
+            erc20_,
+            maxTradeVolume_,
+            defaultThreshold_,
+            delayUntilDefault_,
+            referenceERC20_,
+            bytes32(bytes(targetName_))
+        )
+        CompoundOracleMixin(comptroller_)
+    {
+        rewardERC20 = rewardERC20_;
+        prevReferencePrice = refPerTok(); // {collateral/reference}
+    }
+
+    /// @return {UoA/tok} Our best guess at the market price of 1 whole token in UoA
+    function price() public view virtual returns (uint192) {
+        // {UoA/tok} = {UoA/ref} * {ref/tok}
+        return consultOracle(referenceERC20.symbol()).mul(refPerTok());
+    }
+
+    /// Refresh exchange rates and update default status.
+    /// @custom:interaction RCEI
+    function refresh() external virtual override {
+        // == Refresh ==
+        // Update the Compound Protocol
+        ICToken(address(erc20)).exchangeRateCurrent();
+
+        if (whenDefault <= block.timestamp) return;
+        uint256 oldWhenDefault = whenDefault;
+
+        // Check for hard default
+        uint192 referencePrice = refPerTok();
+        if (referencePrice.lt(prevReferencePrice)) {
+            whenDefault = block.timestamp;
+            emit DefaultStatusChanged(oldWhenDefault, whenDefault, status());
+        }
+        prevReferencePrice = referencePrice;
+
+        // No interactions beyond the initial refresher
+    }
+
+    /// @return {ref/tok} Quantity of whole reference units per whole collateral tokens
+    function refPerTok() public view override returns (uint192) {
+        uint256 rate = ICToken(address(erc20)).exchangeRateStored();
+        int8 shiftLeft = 8 - int8(referenceERC20.decimals()) - 18;
+        return shiftl_toFix(rate, shiftLeft);
+    }
+
+    /// @return {UoA/target} The price of a target unit in UoA
+    function pricePerTarget() public view override returns (uint192) {
+        return consultOracle(referenceERC20.symbol());
+    }
+
+    /// Get the message needed to call in order to claim rewards for holding this asset.
+    /// @return _to The address to send the call to
+    /// @return _cd The calldata to send
+    function getClaimCalldata() external view override returns (address _to, bytes memory _cd) {
+        _to = address(comptroller);
+        _cd = abi.encodeWithSignature("claimComp(address)", msg.sender);
+    }
+}
