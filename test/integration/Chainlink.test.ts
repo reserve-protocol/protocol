69e9293600754239133961dc08eae5b5b9c220ae
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-06-17 09:19:15 -0300

    chainlink priced fiat collateral

--- test/integration/Chainlink.test.ts
+++ test/integration/Chainlink.test.ts
@@ -2,22 +2,35 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { expect } from 'chai'
 import { Wallet } from 'ethers'
 import { ethers, waffle } from 'hardhat'
-import { Collateral, IConfig, IMPLEMENTATION } from '../fixtures'
+import { IConfig, IMPLEMENTATION } from '../fixtures'
 import { defaultFixture } from './fixtures'
 import { ZERO_ADDRESS } from '../../common/constants'
-import { fp } from '../../common/numbers'
+import { bn, fp } from '../../common/numbers'
 
 import {
-  STAKEDAAVE_ADDRESS,
+  DAI_ADDRESS,
+  USDC_ADDRESS,
+  USDT_ADDRESS,
+  BUSD_ADDRESS,
   COMP_ADDRESS,
-  AAVE_ADDRESS,
+  STAKEDAAVE_ADDRESS,
   RSR_ADDRESS,
   RSR_USD_PRICE_FEED,
   AAVE_USD_PRICE_FEED,
   COMP_USD_PRICE_FEED,
+  DAI_USD_PRICE_FEED,
+  USDC_USD_PRICE_FEED,
+  USDT_USD_PRICE_FEED,
+  BUSD_USD_PRICE_FEED,
 } from './mainnet'
 
-import { ChainlinkPricedAsset, ERC20Mock, IERC20 } from '../../typechain'
+import {
+  ChainlinkPricedAsset,
+  ChainlinkPricedFiatCollateral,
+  ERC20Mock,
+  IERC20,
+  USDCMock,
+} from '../../typechain'
 
 const createFixtureLoader = waffle.createFixtureLoader
 
@@ -25,15 +38,23 @@ const describeFork = process.env.FORK ? describe : describe.skip
 
 describeFork(`Chainlink - Integration - Mainnet Forking P${IMPLEMENTATION}`, function () {
   let owner: SignerWithAddress
-  let addr1: SignerWithAddress
 
   // Tokens and Assets
   let rsrMainnet: ERC20Mock
-  let rsrCLAsset: ChainlinkPricedAsset
   let compToken: ERC20Mock
   let aaveToken: ERC20Mock
+  let dai: ERC20Mock
+  let usdc: USDCMock
+  let usdt: ERC20Mock
+  let busd: ERC20Mock
+  let rsrCLAsset: ChainlinkPricedAsset
   let compCLAsset: ChainlinkPricedAsset
   let aaveCLAsset: ChainlinkPricedAsset
+  let daiCLCollateral: ChainlinkPricedFiatCollateral
+  let usdcCLCollateral: ChainlinkPricedFiatCollateral
+  let usdtCLCollateral: ChainlinkPricedFiatCollateral
+  let busdCLCollateral: ChainlinkPricedFiatCollateral
+  let erc20s: IERC20[]
 
   // Contracts to retrieve after deploy
   let config: IConfig
@@ -42,6 +63,9 @@ describeFork(`Chainlink - Integration - Mainnet Forking P${IMPLEMENTATION}`, fun
   let wallet: Wallet
 
   describe('Assets/Collateral Setup', () => {
+    const DEFAULT_THRESHOLD = fp('0.05') // 5%
+    const DELAY_UNTIL_DEFAULT = bn('86400') // 24h
+
     before(async () => {
       ;[wallet] = (await ethers.getSigners()) as unknown as Wallet[]
       loadFixture = createFixtureLoader([wallet])
@@ -49,7 +73,13 @@ describeFork(`Chainlink - Integration - Mainnet Forking P${IMPLEMENTATION}`, fun
 
     beforeEach(async () => {
       ;[owner] = await ethers.getSigners()
-      ;({ compToken, aaveToken, config } = await loadFixture(defaultFixture))
+      ;({ compToken, aaveToken, erc20s, config } = await loadFixture(defaultFixture))
+
+      // Get tokens
+      dai = <ERC20Mock>erc20s[0] // DAI
+      usdc = <ERC20Mock>erc20s[1] // USDC
+      usdt = <ERC20Mock>erc20s[2] // USDT
+      busd = <ERC20Mock>erc20s[3] // BUSD
 
       // Create RSR Asset
       const ChainlinkAssetFactory = await ethers.getContractFactory('ChainlinkPricedAsset')
@@ -76,6 +106,48 @@ describeFork(`Chainlink - Integration - Mainnet Forking P${IMPLEMENTATION}`, fun
           aaveToken.address,
           config.maxTradeVolume,
           AAVE_USD_PRICE_FEED
+        )
+      )
+
+      // Create Collaterals
+      const ChainlinkCollFactory = await ethers.getContractFactory('ChainlinkPricedFiatCollateral')
+
+      daiCLCollateral = <ChainlinkPricedFiatCollateral>(
+        await ChainlinkCollFactory.deploy(
+          dai.address,
+          config.maxTradeVolume,
+          DEFAULT_THRESHOLD,
+          DELAY_UNTIL_DEFAULT,
+          DAI_USD_PRICE_FEED
+        )
+      )
+      usdcCLCollateral = <ChainlinkPricedFiatCollateral>(
+        await ChainlinkCollFactory.deploy(
+          usdc.address,
+          config.maxTradeVolume,
+          DEFAULT_THRESHOLD,
+          DELAY_UNTIL_DEFAULT,
+          USDC_USD_PRICE_FEED
+        )
+      )
+
+      usdtCLCollateral = <ChainlinkPricedFiatCollateral>(
+        await ChainlinkCollFactory.deploy(
+          usdt.address,
+          config.maxTradeVolume,
+          DEFAULT_THRESHOLD,
+          DELAY_UNTIL_DEFAULT,
+          USDT_USD_PRICE_FEED
+        )
+      )
+
+      busdCLCollateral = <ChainlinkPricedFiatCollateral>(
+        await ChainlinkCollFactory.deploy(
+          busd.address,
+          config.maxTradeVolume,
+          DEFAULT_THRESHOLD,
+          DELAY_UNTIL_DEFAULT,
+          BUSD_USD_PRICE_FEED
         )
       )
     })
@@ -108,6 +180,63 @@ describeFork(`Chainlink - Integration - Mainnet Forking P${IMPLEMENTATION}`, fun
       expect(await aaveCLAsset.price()).to.be.closeTo(fp('105'), fp('0.5')) // Close to $105 USD - June 2022 - Uses AAVE price
       expect(await aaveCLAsset.getClaimCalldata()).to.eql([ZERO_ADDRESS, '0x'])
       expect(await aaveCLAsset.rewardERC20()).to.equal(ZERO_ADDRESS)
+    })
+
+    it('Should setup collaterals correctly - Fiatcoins', async () => {
+      // Define interface required for each fiat coin
+      interface TokenInfo {
+        token: ERC20Mock
+        tokenDecimals: number
+        tokenAddress: string
+        tokenCollateral: ChainlinkPricedFiatCollateral
+      }
+
+      // DAI - USDC - USDT - BUSD
+      const tokenInfos: TokenInfo[] = [
+        {
+          token: dai,
+          tokenDecimals: 18,
+          tokenAddress: DAI_ADDRESS,
+          tokenCollateral: daiCLCollateral,
+        },
+        {
+          token: usdc,
+          tokenDecimals: 6,
+          tokenAddress: USDC_ADDRESS,
+          tokenCollateral: usdcCLCollateral,
+        },
+        {
+          token: usdt,
+          tokenDecimals: 6,
+          tokenAddress: USDT_ADDRESS,
+          tokenCollateral: usdtCLCollateral,
+        },
+        {
+          token: busd,
+          tokenDecimals: 18,
+          tokenAddress: BUSD_ADDRESS,
+          tokenCollateral: busdCLCollateral,
+        },
+      ]
+
+      for (const tkInf of tokenInfos) {
+        // Fiat Token Assets
+        expect(await tkInf.tokenCollateral.isCollateral()).to.equal(true)
+        expect(await tkInf.tokenCollateral.referenceERC20()).to.equal(tkInf.token.address)
+        expect(await tkInf.tokenCollateral.erc20()).to.equal(tkInf.token.address)
+        expect(await tkInf.tokenCollateral.erc20()).to.equal(tkInf.tokenAddress)
+        expect(await tkInf.token.decimals()).to.equal(tkInf.tokenDecimals)
+        expect(await tkInf.tokenCollateral.targetName()).to.equal(
+          ethers.utils.formatBytes32String('USD')
+        )
+        expect(await tkInf.tokenCollateral.refPerTok()).to.equal(fp('1'))
+        expect(await tkInf.tokenCollateral.targetPerRef()).to.equal(fp('1'))
+        expect(await tkInf.tokenCollateral.pricePerTarget()).to.equal(fp('1'))
+        expect(await tkInf.tokenCollateral.price()).to.be.closeTo(fp('1'), fp('0.05')) // Should always be close to $1
+
+        expect(await tkInf.tokenCollateral.getClaimCalldata()).to.eql([ZERO_ADDRESS, '0x'])
+        expect(await tkInf.tokenCollateral.rewardERC20()).to.equal(ZERO_ADDRESS)
+      }
     })
   })
 })
