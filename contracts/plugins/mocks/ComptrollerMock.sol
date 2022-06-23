df878e48af9859ce4993461c5002b07adf9a7ce6
Taylor Brent <taylor.w.brent@gmail.com>
2022-06-22 22:22:27 -0400

    keep Comptroller around for rewards

---
+++ contracts/plugins/mocks/ComptrollerMock.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: BlueOak-1.0.0
+pragma solidity 0.8.9;
+
+import "./ERC20Mock.sol";
+
+contract ComptrollerMock {
+    mapping(address => uint256) public compBalances;
+
+    ERC20Mock public compToken;
+
+    constructor() {}
+
+    function setCompToken(address compToken_) external {
+        compToken = ERC20Mock(compToken_);
+    }
+
+    function setRewards(address recipient, uint256 amount) external {
+        compBalances[recipient] = amount;
+    }
+
+    function claimComp(address holder) external {
+        // Mint amount and update internal balances
+        if (address(compToken) != address(0)) {
+            uint256 amount = compBalances[holder];
+            compBalances[holder] = 0;
+            compToken.mint(holder, amount);
+        }
+    }
+}
