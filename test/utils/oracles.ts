e012c74db43cb5c08a769a7107bfdc33bcf590b5
Taylor Brent <taylor.w.brent@gmail.com>
2022-06-27 11:04:37 -0400

    add ts helper for setting oracle prices

--- test/utils/oracles.ts
+++ test/utils/oracles.ts
@@ -1,8 +1,5 @@
 import { BigNumber } from 'ethers'
 import { ethers } from 'hardhat'
-import { expect } from 'chai'
-import { ZERO_ADDRESS } from '../../common/constants'
-import { TestIOracle, MockV3Aggregator } from '../../typechain'
 
 // TODO
 // export const getPrice = async (oracle: TestIOracle, symbol: string) => {
@@ -11,12 +8,9 @@ import { TestIOracle, MockV3Aggregator } from '../../typechain'
 //   ...
 // }
 
-export const setPrice = async (oracle: TestIOracle, symbol: string, price: BigNumber) => {
-  const symbolBytes = ethers.utils.formatBytes32String(symbol)
-  const aggregatorAddr = await oracle.chainlink(symbolBytes)
-  if (aggregatorAddr == ZERO_ADDRESS) {
-    throw new Error('Missing chainlink aggregator deployment')
-  }
-  const v3Aggregator = await ethers.getContractAt('MockV3Aggregator', aggregatorAddr)
+export const setOraclePrice = async (assetAddr: string, price: BigNumber) => {
+  const asset = await ethers.getContractAt('TestIAsset', assetAddr)
+  const chainlinkFeedAddr = await asset.chainlinkFeed()
+  const v3Aggregator = await ethers.getContractAt('MockV3Aggregator', chainlinkFeedAddr)
   await v3Aggregator.updateAnswer(price)
 }
