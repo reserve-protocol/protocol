8711af5eaa0a54b6f2c936763c1fd7cd5a55f700
Taylor Brent <taylor.w.brent@gmail.com>
2022-07-26 22:11:16 -0400

    init new scenario test for when target/ref != 1

---
+++ test/scenario/NonIdentityPeg.test.ts
@@ -0,0 +1,13 @@
+// // Should support collateral types with pegs other than the identity (target/ref != 1)
+// describe(`The peg (target/ref) should be arbitrary - P${IMPLEMENTATION}`, () => {
+// for each of i in [1e-9, 0.5, 2, 1e9] ({
+//   beforeEach(async () => {
+//     // Set up basket of equal parts 2 fiatcoins:
+//     // -  1 with a (target/ref) of i
+//     // -  1 with a (target/ref) of one
+//   })
+//   it('should respect differing scales during issuance', async () => {})
+//   it('should respect differing scales during redemption', async () => {})
+//   it('should remain stable and not become undercapitalized or produce revenue', async () => {})
+// })
+// })
