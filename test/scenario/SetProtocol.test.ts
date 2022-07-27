7852b98dcccbabc06f67b332600863df91019597
Taylor Brent <taylor.w.brent@gmail.com>
2022-07-26 21:54:36 -0400

    init new scenario test for the SetProtocol case

---
+++ test/scenario/SetProtocol.test.ts
@@ -0,0 +1,10 @@
+// // Our protocol should contain the SetProtocol as a subset of functionality
+// describe(`Only non-appreciating collateral - P${IMPLEMENTATION}`, () => {
+//   beforeEach(async () => {
+//     // Set up basket of tokens against themselves, or pegged versions of themselves
+//     // Issue RToken
+//   })
+//   it('should not produce revenue', async () => {})
+//   it('should maintain a constant issuance basket', async () => {})
+//   it('should appreciate when altruists/idiots donate', async () => {})
+// })
