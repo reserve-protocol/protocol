12c3b5c3db5932e62363dedf533966a6533a09ca
Taylor Brent <taylor.w.brent@gmail.com>
2022-07-26 21:53:01 -0400

    init new scenario test case for when f = 0

---
+++ test/scenario/OnlyRTokenRevenue.test.ts
@@ -0,0 +1,10 @@
+// // The RToken retains 100% of collateral appreciation
+// describe(`100% of the cash flow to the RToken - P${IMPLEMENTATION}`, () => {
+//   beforeEach(async () => {
+//     // Set up basket of only appreciating assets
+//     // Issue RToken
+//   })
+//   it('should decrease quantities immediately after appreciation while maintaining >= ref units', async () => {})
+//   it('should melt revenue RToken slowly after appreciation', async () => {})
+//   it('should sell single assets for RToken', async () => {})
+// })
