d8d800038c946068fa91ef58d4b0718718311b36
Matt Elder <matt.elder@reserve.org>
2022-08-01 19:00:41 -0700

    draft of cooperative-transparency approach to the setSender() problem

---
+++ contracts/fuzz/ERC20Fuzz.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BlueOak-1.0.0
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "contracts/plugins/mocks/ERC20Mock.sol";
+import "contracts/fuzz/IFuzz.sol";
+
+// An ERC20Fuzz is an ERC20Mock that knows about main, and performs the _msgSender override that our
+// components also do.
+contract ERC20Fuzz is ERC20Mock {
+    IMainFuzz internal main;
+
+    // solhint-disable-next-line no-empty-blocks
+    constructor(
+        string memory name,
+        string memory symbol,
+        IMainFuzz _main
+    ) ERC20Mock(name, symbol) {
+        main = _main;
+    }
+
+    function _msgSender() internal view virtual override returns (address) {
+        return main.sender(msg.sender);
+    }
+}
