7300910eb61866180a198d6751c00905e9d3f234
Taylor Brent <taylor.w.brent@gmail.com>
2022-09-16 17:01:45 -0400

    switch the order of hi/lo in fullMul

--- contracts/libraries/Fixed.sol
+++ contracts/libraries/Fixed.sol
@@ -402,7 +402,7 @@ function mulDiv256(
     uint256 z
 ) pure returns (uint256 result) {
     unchecked {
-        (uint256 lo, uint256 hi) = fullMul(x, y);
+        (uint256 hi, uint256 lo) = fullMul(x, y);
         if (hi >= z) revert UIntOutOfBounds();
         uint256 mm = mulmod(x, y, z);
         if (mm > lo) hi -= 1;
@@ -449,7 +449,7 @@ function mulDiv256(
 ///   Adapted from sources:
 ///   https://medium.com/wicketh/27650fec525d, https://medium.com/coinmonks/4db014e080b1
 /// @dev Intended to be internal to this library
-function fullMul(uint256 x, uint256 y) pure returns (uint256 lo, uint256 hi) {
+function fullMul(uint256 x, uint256 y) pure returns (uint256 hi, uint256 lo) {
     unchecked {
         uint256 mm = mulmod(x, y, uint256(0) - uint256(1));
         lo = x * y;
