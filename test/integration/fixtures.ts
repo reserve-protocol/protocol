988942b6fd1dcd999e90fec1a93cf481077b9f53
Taylor Brent <taylor.w.brent@gmail.com>
2022-09-29 18:26:18 -0400

    update integration tests fixture

--- test/integration/fixtures.ts
+++ test/integration/fixtures.ts
@@ -701,7 +701,7 @@ export const defaultFixture: Fixture<DefaultFixture> = async function ([
     libraries: { TradingLibP0: tradingLib.address },
   })
   let deployer: TestIDeployer = <DeployerP0>(
-    await DeployerFactory.deploy(rsr.address, gnosis.address, facade.address, rsrAsset.address)
+    await DeployerFactory.deploy(rsr.address, gnosis.address, rsrAsset.address)
   )
 
   if (IMPLEMENTATION == Implementation.P1) {
@@ -777,13 +777,7 @@ export const defaultFixture: Fixture<DefaultFixture> = async function ([
 
     const DeployerFactory: ContractFactory = await ethers.getContractFactory('DeployerP1')
     deployer = <DeployerP1>(
-      await DeployerFactory.deploy(
-        rsr.address,
-        gnosis.address,
-        facade.address,
-        rsrAsset.address,
-        implementations
-      )
+      await DeployerFactory.deploy(rsr.address, gnosis.address, rsrAsset.address, implementations)
     )
   }
 
