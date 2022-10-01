0fcfad5d19c6934b0270fc93f58e0a39d73646c2
Matt Elder <matt.elder@reserve.org>
2022-09-30 19:37:53 -0700

    Initial draft, now yet even compiling, for very refactored TradeLib

--- contracts/p1/mixins/TradeLib.sol
+++ contracts/p1/mixins/TradeLib.sol
@@ -21,22 +21,17 @@ library TradeLib {
     /// Prepare a trade to sell `trade.sellAmount` that guarantees a reasonable closing price,
     /// without explicitly aiming at a particular quantity to purchase.
     /// @param trade:
-    ///   sell != 0
-    ///   buy != 0
-    ///   sellAmount >= 0 {sellTok}
-    ///   buyAmount (unused) {buyTok}
-    ///   sellPrice >= 0 {UoA/sellTok}
-    ///   buyPrice > 0 {UoA/buyTok}
+    ///   sell != 0, sellAmount >= 0 {sellTok}, sellPrice >= 0 {UoA/sellTok},
+    ///   buy != 0, buyAmount (unused) {buyTok}, buyPrice > 0 {UoA/buyTok}
     /// @return notDust True when the trade is larger than the dust amount
     /// @return req The prepared trade request to send to the Broker
     //
     // If notDust is true, then the returned trade request satisfies:
     //   req.sell == trade.sell and req.buy == trade.buy,
-    //   req.minBuyAmount ~=
-    //        trade.sellAmount * trade.sellPrice / trade.buyPrice * (1-maxTradeSlippage),
-    //   req.sellAmount <= trade.sell.maxTradeSize().toQTok()
+    //   req.minBuyAmount * trade.buyPrice ~==
+    //        trade.sellAmount * trade.sellPrice * (1-rules.maxTradeSlippage),
+    //   req.sellAmount == min(trade.sell.maxTradeSize().toQTok(), trade.sellAmount.toQTok(sell))
     //   1 < req.sellAmount
-    //   and req.sellAmount is maximal such that req.sellAmount <= trade.sellAmount.toQTok(sell)
     //
     // If notDust is false, no trade exists that satisfies those constraints.
     function prepareTradeSell(TradeInfo memory trade, TradingRules memory rules)
@@ -52,21 +47,20 @@ library TradeLib {
             return (false, req);
         }
 
-        // {sellTok} - reads trade.sell.price(true)
-        uint192 s = fixMin(trade.sellAmount, maxTradeSize(trade.sell));
-
-        // {qSellTok}
-        req.sellAmount = s.shiftl_toUint(int8(trade.sell.erc20Decimals()), FLOOR);
+        // {sellTok}.  reads trade.sell.price(true)
+        uint192 sellAmt = fixMin(trade.sellAmount, maxTradeSize(trade.sell));
 
         // {buyTok} = {sellTok} * {UoA/sellTok} / {UoA/buyTok}
-        uint192 b = s.mul(FIX_ONE.minus(rules.maxTradeSlippage)).mulDiv(
-            trade.sellPrice,
-            trade.buyPrice,
-            CEIL
-        );
-        req.minBuyAmount = b.shiftl_toUint(int8(trade.buy.erc20Decimals()), CEIL);
+        uint192 buyAmt = sellAmt
+            .mul(FIX_ONE.minus(rules.maxTradeSlippage))
+            .mul(trade.sellPrice)
+            .div(trade.buyPrice, CEIL);
+
+        // Build the TradeRequest by converting to uint256 {QTok} values:
         req.sell = trade.sell;
         req.buy = trade.buy;
+        req.sellAmount = sellAmt.shiftl_toUint(int8(trade.sell.erc20Decimals()), FLOOR);
+        req.minBuyAmount = buyAmt.shiftl_toUint(int8(trade.buy.erc20Decimals()), CEIL);
 
         return (true, req);
     }
@@ -74,53 +68,64 @@ library TradeLib {
     /// Assuming we have `trade.sellAmount` sell tokens available, prepare a trade to cover as
     /// much of our deficit of `trade.buyAmount` buy tokens as possible, given expected trade
     /// slippage and the sell asset's maxTradeVolume().
-    /// @param trade:
-    ///   sell != 0
-    ///   buy != 0
-    ///   sellAmount (unused) {sellTok}
-    ///   buyAmount >= 0 {buyTok}
-    ///   sellPrice > 0 {UoA/sellTok}
-    ///   buyPrice > 0 {UoA/buyTok}
-    /// @return notDust Whether the prepared trade is large enough to be worth trading
+    /// @param trade checks:
+    ///   sell != 0, sellAmount (unused) {sellTok}, sellPrice > 0 {UoA/sellTok},
+    ///   buy != 0, buyAmount >= 0 {buyTok}, buyPrice > 0 {UoA/buyTok}
+    /// @return notDust Whether the supplied assets are large enough to be worth trading
     /// @return req The prepared trade request to send to the Broker
     //
-    // Returns prepareTradeSell(trade, rules), where
-    //   req.sellAmount = min(trade.sellAmount,
-    //                trade.buyAmount * (trade.buyPrice / trade.sellPrice) / (1-maxTradeSlippage))
-    //   i.e, the minimum of trade.sellAmount and (a sale amount that, at current prices and
-    //   maximum slippage, will yield at least the requested trade.buyAmount)
-    //
-    // Which means we should get that, if notDust is true, then:
+    // If notDust is true, then:
     //   req.sell = sell and req.buy = buy
-    //
     //   1 <= req.minBuyAmount <= max(trade.buyAmount, buy.minTradeSize()).toQTok(trade.buy)
-    //   1 < req.sellAmount <= max(trade.sellAmount.toQTok(trade.sell),
+    //   1 < req.sellAmount <= min(trade.sellAmount.toQTok(trade.sell),  TODO: should this be min?
     //                               sell.maxTradeSize().toQTok(trade.sell))
-    //   req.minBuyAmount ~= trade.sellAmount * sellPrice / buyPrice * (1-maxTradeSlippage)
-    //
+    //   req.minBuyAmount * trade.buyPrice ~=
+    //       trade.sellAmount * trade.sellPrice * (1-rules.maxTradeSlippage)
     //   req.sellAmount (and req.minBuyAmount) are maximal satisfying all these conditions
-    function prepareTradeToCoverDeficit(TradeInfo memory trade, TradingRules memory rules)
+    function prepareTradeToCoverDeficit(TradeInfo memory trade, TradingRules calldata rules)
         internal
         view
         returns (bool notDust, TradeRequest memory req)
     {
         assert(trade.sellPrice > 0 && trade.buyPrice > 0);
 
-        // Don't buy dust.
-        trade.buyAmount = fixMax(trade.buyAmount, minTradeSize(trade.buy, rules.minTradeVolume));
+        // If you only have dust to sell then don't sell anything
+        if (!isEnoughToSell(trade.sell, trade.sellAmount, rules.minTradeVolume)) {
+            return (false, req);
+        }
 
-        // {sellTok} = {buyTok} * {UoA/buyTok} / {UoA/sellTok}
-        uint192 exactSellAmount = trade.buyAmount.mulDiv(trade.buyPrice, trade.sellPrice, CEIL);
-        // exactSellAmount: Amount to sell to buy `deficitAmount` if there's no slippage
+        // Do not aim to buy mere dust; Aim to buy more.
+        uint192 maxBuyAmt = fixMax(trade.buyAmount, minTradeSize(trade.buy, rules.minTradeVolume));
+        // Sell at most the offered token balance and at most the maxTradeSize()
+        uint192 maxSellAmt = fixMin(trade.sellAmount, maxTradeSize(trade.sell));
 
-        // slippedSellAmount: Amount needed to sell to buy `deficitAmount`, counting slippage
-        uint192 slippedSellAmount = exactSellAmount.div(
-            FIX_ONE.minus(rules.maxTradeSlippage),
-            CEIL
+        // We want buyAmt * buyPrice ~== sellAmt * sellPrice * afterSlippage,
+        // And so our request should happen at the following biy-per-sell price:
+        // {sellTok/buyTok} = {UoA / buyTok} / {UoA / sellTok} / {1}
+        //
+        uint192 requestPrice = (trade.buyPrice).div(trade.sellPrice).div(
+            FIX_ONE.minus(rules.maxTradeSlippage)
         );
 
-        trade.sellAmount = fixMin(slippedSellAmount, trade.sellAmount); // {sellTok}
-        return prepareTradeSell(trade, rules);
+        // Now, buyAmt <= maxBuyAmt, sellAmt <= maxSellAmt, buyAmt / sellAmt = requestPrice
+        // and at least one of those inequalities should be an equality. Thus:
+        uint192 buyAmt;
+        uint192 sellAmt;
+        if (maxBuyAmt.mul(requestPrice) >= maxSellAmt) {
+            sellAmt = maxSellAmt;
+            buyAmt = maxSellAmt.div(requestPrice);
+        } else {
+            sellAmt = maxBuyAmt.mul(requestPrice);
+            buyAmt = maxBuyAmt;
+        }
+
+        // Build the TradeRequest by converting to uint256 {QTok} values:
+        req.sell = trade.sell;
+        req.buy = trade.buy;
+        req.sellAmount = sellAmt.shiftl_toUint(int8(trade.sell.erc20Decimals()), FLOOR);
+        req.minBuyAmount = buyAmt.shiftl_toUint(int8(trade.buy.erc20Decimals()), CEIL);
+
+        return (true, req);
     }
 
     /// @param asset The asset in question
@@ -136,7 +141,7 @@ library TradeLib {
         // for it to keep 1 qTok for itself. Therefore we should not sell 1 qTok. This is
         // likely to be true of all the trading platforms we integrate with.
         return
-            amt.gte(minTradeSize(asset, minTradeVolume_)) &&
+            amt.gte(minTradeSize(minTradeVolume_, asset.price()) &&
             // {qTok} = {tok} / {tok/qTok}
             amt.shiftl_toUint(int8(asset.erc20Decimals())) > 1;
     }
@@ -144,21 +149,14 @@ library TradeLib {
     /// Calculates the minTradeSize for an asset based on the given minTradeVolume and price
     /// @param minTradeVolume_ {UoA} The min trade volume, passed in for gas optimization
     /// @return {tok} The min trade size for the asset in whole tokens
-    function minTradeSize(IAsset asset, uint192 minTradeVolume_) internal view returns (uint192) {
-        (, uint192 price) = asset.price(true); // {UoA/tok}
-        if (price == 0) return FIX_MAX;
-
+    function minTradeSize(uint192 minTradeVolume_, uint192 price) internal view returns (uint192) {
         // {tok} = {UoA} / {UoA/tok}
-        return minTradeVolume_.div(price);
+        return price == 0 ? FIX_MAX : minTradeVolume_.div(price);
     }
 
     /// Calculates the maxTradeSize for an asset based on the asset's maxTradeVolume and price
     /// @return {tok} The max trade size for the asset in whole tokens
-    function maxTradeSize(IAsset asset) internal view returns (uint192) {
-        (, uint192 price) = asset.price(true); // {UoA/tok}
-        if (price == 0) return FIX_MAX;
-
-        // {tok} = {UoA} / {UoA/tok}
-        return asset.maxTradeVolume().div(price);
+    function maxTradeSize(IAsset asset, uint192 price) internal view returns (uint192) {
+        return price == 0 ? FIX_MAX : asset.maxTradeVolume().div(price);
     }
 }
