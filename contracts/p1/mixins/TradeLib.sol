0cb372d447498ea61638f83634978745dfe2f224
Taylor Brent <taylor.w.brent@gmail.com>
2022-10-02 22:42:29 -0400

    add rounding + unit comments; differences from p0 to resolve

--- contracts/p1/mixins/TradeLib.sol
+++ contracts/p1/mixins/TradeLib.sol
@@ -12,7 +12,7 @@ import "./RecollateralizationLib.sol";
  * @title TradeLib
  * @notice An internal lib for preparing individual trades on particular asset pairs
  *   Users:
- *     - BackingManagerLib
+ *     - RecollateralizationLib
  *     - RevenueTrader
  */
 library TradeLib {
@@ -40,15 +40,18 @@ library TradeLib {
         returns (bool notDust, TradeRequest memory req)
     {
         assert(trade.buyPrice > 0); // checked for in RevenueTrader / CollateralizatlionLib
-        // assert(trade.buyPrice >= 0);
+        // assert(trade.sellPrice >= 0);
 
         // Don't sell dust
         if (!isEnoughToSell(trade.sell, trade.sellAmount, rules.minTradeVolume)) {
             return (false, req);
         }
 
-        // {sellTok}.  reads trade.sell.price(true)
-        uint192 sellAmt = fixMin(trade.sellAmount, maxTradeSize(trade.sell));
+        (, uint192 price) = trade.sell.price(true); // {UoA/tok}
+        // may use fallback price for sell asset
+
+        // {sellTok} - reads trade.sell.price(true)
+        uint192 sellAmt = fixMin(trade.sellAmount, maxTradeSize(trade.sell, price));
 
         // {buyTok} = {sellTok} * {UoA/sellTok} / {UoA/buyTok}
         uint192 buyAmt = sellAmt
@@ -77,12 +80,12 @@ library TradeLib {
     // If notDust is true, then:
     //   req.sell = sell and req.buy = buy
     //   1 <= req.minBuyAmount <= max(trade.buyAmount, buy.minTradeSize()).toQTok(trade.buy)
-    //   1 < req.sellAmount <= min(trade.sellAmount.toQTok(trade.sell),  TODO: should this be min?
+    //   1 < req.sellAmount <= min(trade.sellAmount.toQTok(trade.sell),
     //                               sell.maxTradeSize().toQTok(trade.sell))
     //   req.minBuyAmount * trade.buyPrice ~=
     //       trade.sellAmount * trade.sellPrice * (1-rules.maxTradeSlippage)
     //   req.sellAmount (and req.minBuyAmount) are maximal satisfying all these conditions
-    function prepareTradeToCoverDeficit(TradeInfo memory trade, TradingRules calldata rules)
+    function prepareTradeToCoverDeficit(TradeInfo memory trade, TradingRules memory rules)
         internal
         view
         returns (bool notDust, TradeRequest memory req)
@@ -95,27 +98,35 @@ library TradeLib {
         }
 
         // Do not aim to buy mere dust; Aim to buy more.
-        uint192 maxBuyAmt = fixMax(trade.buyAmount, minTradeSize(trade.buy, rules.minTradeVolume));
+        uint192 maxBuyAmt = fixMax(
+            trade.buyAmount,
+            minTradeSize(rules.minTradeVolume, trade.buyPrice)
+        ); // {buyTok}
+
         // Sell at most the offered token balance and at most the maxTradeSize()
-        uint192 maxSellAmt = fixMin(trade.sellAmount, maxTradeSize(trade.sell));
+        uint192 maxSellAmt = fixMin(trade.sellAmount, maxTradeSize(trade.sell, trade.sellPrice));
 
         // We want buyAmt * buyPrice ~== sellAmt * sellPrice * afterSlippage,
         // And so our request should happen at the following biy-per-sell price:
         // {sellTok/buyTok} = {UoA / buyTok} / {UoA / sellTok} / {1}
-        //
-        uint192 requestPrice = (trade.buyPrice).div(trade.sellPrice).div(
-            FIX_ONE.minus(rules.maxTradeSlippage)
+        uint192 requestPrice = (trade.buyPrice).div(trade.sellPrice, CEIL).div(
+            FIX_ONE.minus(rules.maxTradeSlippage),
+            CEIL
         );
 
         // Now, buyAmt <= maxBuyAmt, sellAmt <= maxSellAmt, buyAmt / sellAmt = requestPrice
         // and at least one of those inequalities should be an equality. Thus:
-        uint192 buyAmt;
-        uint192 sellAmt;
+        uint192 buyAmt; // {buyTok}
+        uint192 sellAmt; // {sellTok}
+
+        // {buyTok} * {sellTok/buyTok} >= {sellTok}
         if (maxBuyAmt.mul(requestPrice) >= maxSellAmt) {
+            // {buyTok} = {sellTok} / {sellTok/buyTok}
+            buyAmt = maxSellAmt.div(requestPrice); // FLOOR
             sellAmt = maxSellAmt;
-            buyAmt = maxSellAmt.div(requestPrice);
         } else {
-            sellAmt = maxBuyAmt.mul(requestPrice);
+            // {sellTok} = {buyTok} * {sellTok/buyTok}
+            sellAmt = maxBuyAmt.mul(requestPrice, CEIL); // TODO rounding
             buyAmt = maxBuyAmt;
         }
 
@@ -124,6 +135,7 @@ library TradeLib {
         req.buy = trade.buy;
         req.sellAmount = sellAmt.shiftl_toUint(int8(trade.sell.erc20Decimals()), FLOOR);
         req.minBuyAmount = buyAmt.shiftl_toUint(int8(trade.buy.erc20Decimals()), CEIL);
+        // TODO why these rounds...
 
         return (true, req);
     }
@@ -137,26 +149,31 @@ library TradeLib {
         uint192 amt,
         uint192 minTradeVolume_
     ) internal view returns (bool) {
+        (, uint192 price) = asset.price(true); // {UoA/tok}
+        // can be a fallback price
+
         // The Gnosis EasyAuction trading platform rounds defensively, meaning it is possible
         // for it to keep 1 qTok for itself. Therefore we should not sell 1 qTok. This is
         // likely to be true of all the trading platforms we integrate with.
         return
-            amt.gte(minTradeSize(minTradeVolume_, asset.price()) &&
+            amt.gte(minTradeSize(minTradeVolume_, price)) &&
             // {qTok} = {tok} / {tok/qTok}
             amt.shiftl_toUint(int8(asset.erc20Decimals())) > 1;
     }
 
+    // === Private ===
+
     /// Calculates the minTradeSize for an asset based on the given minTradeVolume and price
     /// @param minTradeVolume_ {UoA} The min trade volume, passed in for gas optimization
     /// @return {tok} The min trade size for the asset in whole tokens
-    function minTradeSize(uint192 minTradeVolume_, uint192 price) internal view returns (uint192) {
+    function minTradeSize(uint192 minTradeVolume_, uint192 price) private pure returns (uint192) {
         // {tok} = {UoA} / {UoA/tok}
-        return price == 0 ? FIX_MAX : minTradeVolume_.div(price);
+        return price == 0 ? FIX_MAX : minTradeVolume_.div(price, CEIL);
     }
 
     /// Calculates the maxTradeSize for an asset based on the asset's maxTradeVolume and price
     /// @return {tok} The max trade size for the asset in whole tokens
-    function maxTradeSize(IAsset asset, uint192 price) internal view returns (uint192) {
-        return price == 0 ? FIX_MAX : asset.maxTradeVolume().div(price);
+    function maxTradeSize(IAsset asset, uint192 price) private view returns (uint192) {
+        return price == 0 ? FIX_MAX : asset.maxTradeVolume().div(price, CEIL);
     }
 }
