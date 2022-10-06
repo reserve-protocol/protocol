8db1927aa516384eff7c806a9243e4c401bbcc72
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-10-06 12:45:40 -0300

    improve block handling in fork

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -32,7 +32,9 @@ const config: any = {
     hardhat: {
       forking: {
         url: MAINNET_RPC_URL,
-        blockNumber: forkBlockNumber['default'],
+        blockNumber: process.env.MAINNET_BLOCK
+          ? Number(process.env.MAINNET_BLOCK)
+          : forkBlockNumber['default'],
         enabled: !!process.env.FORK,
       },
       gas: 0x1ffffffff,
