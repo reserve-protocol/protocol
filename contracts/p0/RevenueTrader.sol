9a1d38b1a272c39642264efbbba494043d382cc6
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-10-24 13:11:22 -0300

    p0 safeApprove changes for USDT

--- contracts/p0/RevenueTrader.sol
+++ contracts/p0/RevenueTrader.sol
@@ -37,8 +37,9 @@ contract RevenueTraderP0 is TradingP0, IRevenueTrader {
         uint256 bal = erc20.balanceOf(address(this));
         if (bal == 0) return;
 
-        if (erc20 == tokenToBuy) {
-            erc20.safeIncreaseAllowance(address(main.distributor()), bal);
+        if (erc20 == tokenToBuy) { 
+            erc20.safeApprove(address(main.distributor()), 0);
+            erc20.safeApprove(address(main.distributor()), bal);
             main.distributor().distribute(erc20, address(this), bal);
             return;
         }
