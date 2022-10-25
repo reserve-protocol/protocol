c85b4df55be973b5b637fb597a187e9282597e0e
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-10-25 11:43:39 -0300

    running out of gas tests for collateral

---
+++ contracts/plugins/mocks/InvalidChainlinkMock.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ChainlinkMock.sol";
+
+/**
+ * @title OOGMockV3Aggregator
+ * @notice Use this contract when you need to test our of gas errors
+ * on Chainlink feeds
+ *
+ */
+contract InvalidMockV3Aggregator is MockV3Aggregator {
+    bool public simplyRevert;
+
+    constructor(uint8 _decimals, int256 _initialAnswer)
+        MockV3Aggregator(_decimals, _initialAnswer)
+    {}
+
+    function latestRoundData()
+        external
+        view
+        override
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRoundyarn
+        )
+    {
+        if (simplyRevert) {
+            revert();  // Revert with no reason
+        } else {
+            // Run out of gas
+            this.infiniteLoop{ gas: 10 }();
+        }
+        return (
+            uint80(latestRound),
+            getAnswer[latestRound],
+            block.timestamp,
+            getTimestamp[latestRound],
+            uint80(latestRound)
+        );
+    }
+
+    function setSimplyRevert(bool on) external {
+        simplyRevert = on;
+    }
+
+    function infiniteLoop() external pure {
+        uint256 i = 0;
+        uint256[1] memory array;
+        while (true) {
+            array[0] = i;
+            i++;
+        }
+    }
+}
