8e7cd0e431ecb6c98d6908e4ece771799435c7e8
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-10-26 11:50:10 -0300

    add regression test for deploying USDT

---
+++ test/integration/Deployment.test.ts
@@ -0,0 +1,125 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { expect } from 'chai'
+import { ContractFactory, Wallet } from 'ethers'
+import hre, { ethers, waffle } from 'hardhat'
+import { Collateral, IMPLEMENTATION } from '../fixtures'
+import { defaultFixture } from './fixtures'
+import { getChainId } from '../../common/blockchain-utils'
+import {
+  IConfig,
+  IRTokenConfig,
+  IRTokenSetup,
+  networkConfig,
+} from '../../common/configuration'
+import { ZERO_ADDRESS } from '../../common/constants'
+import { expectInIndirectReceipt } from '../../common/events'
+import { bn, fp } from '../../common/numbers'
+import {
+  ERC20Mock,
+  FacadeWrite,
+  FiatCollateral,
+  IERC20,
+  TestIDeployer,
+  TestIMain,
+} from '../../typechain'
+
+const createFixtureLoader = waffle.createFixtureLoader
+
+const describeFork = process.env.FORK ? describe : describe.skip
+
+describeFork(`Deployment - Integration - Mainnet Forking P${IMPLEMENTATION}`, function () {
+  let owner: SignerWithAddress
+  let addr1: SignerWithAddress
+
+  // Assets
+  let collateral: Collateral[]
+
+  // Tokens and Assets
+  let usdt: ERC20Mock
+  let usdtCollateral: FiatCollateral
+
+  // Contracts to retrieve after deploy
+  let main: TestIMain
+  let deployer: TestIDeployer
+  let facadeWrite: FacadeWrite
+  let config: IConfig
+
+  let rTokenConfig: IRTokenConfig
+  let rTokenSetup: IRTokenSetup
+  
+  let erc20s: IERC20[]
+
+  let loadFixture: ReturnType<typeof createFixtureLoader>
+  let wallet: Wallet
+
+  let chainId: number
+
+  describe('Deployment', () => {
+    before(async () => {
+      ;[wallet] = (await ethers.getSigners()) as unknown as Wallet[]
+      loadFixture = createFixtureLoader([wallet])
+
+      chainId = await getChainId(hre)
+      if (!networkConfig[chainId]) {
+        throw new Error(`Missing network configuration for ${hre.network.name}`)
+      }
+    })
+
+    beforeEach(async () => {
+      ;[owner, addr1] = await ethers.getSigners()
+      ;({ erc20s, collateral, deployer, config } = await loadFixture(
+        defaultFixture
+      ))
+
+      // Deploy DFacadeWriteLib lib
+      const facadeWriteLib = await (await ethers.getContractFactory('FacadeWriteLib')).deploy()
+      const facadeWriteLibAddr = facadeWriteLib.address
+
+      // Deploy Facade
+      const FacadeFactory: ContractFactory = await ethers.getContractFactory('FacadeWrite', {
+        libraries: {
+          FacadeWriteLib: facadeWriteLibAddr,
+        },
+      })
+      facadeWrite = <FacadeWrite>await FacadeFactory.deploy(deployer.address)
+
+      // Get tokens
+      usdt = <ERC20Mock>erc20s[2] // USDT
+      usdtCollateral = <FiatCollateral>collateral[2] // USDT   
+    })
+
+    it('Should allow to deploy USDT in both prime and backup basket', async () => {
+       // Set parameters
+       rTokenConfig = {
+        name: 'RTKN RToken',
+        symbol: 'RTKN',
+        mandate: 'mandate',
+        params: config,
+      }
+
+      rTokenSetup = {
+        assets: [],
+        primaryBasket: [usdtCollateral.address],
+        weights: [fp('1')],
+        backups: [
+          {
+            backupUnit: ethers.utils.formatBytes32String('USD'),
+            diversityFactor: bn(1),
+            backupCollateral: [usdtCollateral.address],
+          },
+        ],
+      }
+      // Deploy RToken via FacadeWrite
+      const receipt = await (
+        await facadeWrite.connect(addr1).deployRToken(rTokenConfig, rTokenSetup)
+      ).wait()
+
+      const mainAddr = expectInIndirectReceipt(receipt, deployer.interface, 'RTokenCreated').args
+        .main
+
+      main = <TestIMain>await ethers.getContractAt('TestIMain', mainAddr)
+
+      expect(main.address).to.not.equal(ZERO_ADDRESS)
+    })
+  })
+})
