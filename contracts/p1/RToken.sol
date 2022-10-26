04a6ac4c4fda4a37571d6b483a4ceb25b1856272
Taylor Brent <taylor.w.brent@gmail.com>
2022-10-25 22:22:07 -0400

    bump gap

--- contracts/p1/RToken.sol
+++ contracts/p1/RToken.sol
@@ -758,5 +758,5 @@ contract RTokenP1 is ComponentP1, IRewardable, ERC20PermitUpgradeable, IRToken {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[36] private __gap;
+    uint256[38] private __gap;
 }
