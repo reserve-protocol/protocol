898e6053d8f2ef3acf1fdbc66577a55607122647
lazycoder1 <gautamgsabhahit@gmail.com>
2022-11-07 23:56:45 +0530

    Adding convexStakingWrapper contract

---
+++ contracts/plugins/convex/interfaces/IBooster.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+interface IBooster {
+    function owner() external view returns(address);
+    function setVoteDelegate(address _voteDelegate) external;
+    function vote(uint256 _voteId, address _votingAddress, bool _support) external returns(bool);
+    function voteGaugeWeight(address[] calldata _gauge, uint256[] calldata _weight ) external returns(bool);
+    function poolInfo(uint256 _pid) external returns(address _lptoken, address _token, address _gauge, address _crvRewards, address _stash, bool _shutdown);
+}
