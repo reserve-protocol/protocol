b39ba791b2df624d5acbf4d4b50e7b89164280ce
shr1ftyy <syeamtechdemon@gmail.com>
2022-11-11 14:02:08 +1030

    FraxSwap fiat collateral draft

---
+++ contracts/plugins/frax/FraxSwapPairFiatCollateral.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: BlueOak-1.0.0
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "contracts/plugins/assets/AbstractCollateral.sol";
+import "contracts/libraries/Fixed.sol";
+import "./IFraxSwapPair.sol";
+
+/**
+ * @title CTokenFiatCollateral
+ * @notice Collateral plugin for a cToken of fiat collateral, like cUSDC or cUSDP
+ * Expected: {tok} != {ref}, {ref} is pegged to {target} unless defaulting, {target} == {UoA}
+ */
+contract FraxSwapPairFiatCollateral is Collateral {
+    using OracleLib for AggregatorV3Interface;
+    using FixLib for uint192;
+
+    // All cTokens have 8 decimals, but their underlying may have 18 or 6 or something else.
+
+    int8 public immutable referenceERC20Decimals;
+
+    uint192 public immutable defaultThreshold; // {%} e.g. 0.05
+
+    uint192 public prevReferencePrice; // previous rate, {collateral/reference}
+    address public immutable comptrollerAddr;
+
+    /// @param chainlinkFeed_ Feed units: {UoA/ref}
+    /// @param maxTradeVolume_ {UoA} The max trade volume, in UoA
+    /// @param oracleTimeout_ {s} The number of seconds until a oracle value becomes invalid
+    /// @param defaultThreshold_ {%} A value like 0.05 that represents a deviation tolerance
+    /// @param delayUntilDefault_ {s} The number of seconds deviation must occur before default
+    constructor(
+        uint192 fallbackPrice_,
+        AggregatorV3Interface chainlinkFeed_,
+        IERC20Metadata erc20_,
+        IERC20Metadata rewardERC20_,
+        uint192 maxTradeVolume_,
+        uint48 oracleTimeout_,
+        bytes32 targetName_,
+        uint192 defaultThreshold_,
+        uint256 delayUntilDefault_,
+        int8 referenceERC20Decimals_,
+        address comptrollerAddr_
+    )
+        Collateral(
+            fallbackPrice_,
+            chainlinkFeed_,
+            erc20_,
+            rewardERC20_,
+            maxTradeVolume_,
+            oracleTimeout_,
+            targetName_,
+            delayUntilDefault_
+        )
+    {
+        require(address(rewardERC20_) != address(0), "rewardERC20 missing");
+        require(defaultThreshold_ > 0, "defaultThreshold zero");
+        require(referenceERC20Decimals_ > 0, "referenceERC20Decimals missing");
+        require(address(comptrollerAddr_) != address(0), "comptrollerAddr missing");
+        defaultThreshold = defaultThreshold_;
+        referenceERC20Decimals = referenceERC20Decimals_;
+
+        prevReferencePrice = refPerTok();
+        comptrollerAddr = comptrollerAddr_;
+    }
+
+    /// @return {UoA/tok} Our best guess at the market price of 1 whole token in UoA
+    function strictPrice() public view virtual override returns (uint192) {
+        // {UoA/tok} = {UoA/ref} * {ref/tok}
+        return chainlinkFeed.price(oracleTimeout).mul(refPerTok());
+    }
+
+    /// Refresh exchange rates and update default status.
+    /// @custom:interaction RCEI
+    function refresh() external virtual override {
+        // == Refresh ==
+
+        if (alreadyDefaulted()) return;
+        CollateralStatus oldStatus = status();
+
+        // Check for hard default
+        uint192 referencePrice = refPerTok();
+        // uint192(<) is equivalent to Fix.lt
+        if (referencePrice < prevReferencePrice) {
+            markStatus(CollateralStatus.DISABLED);
+        } else {
+            try chainlinkFeed.price_(oracleTimeout) returns (uint192 p) {
+                // Check for soft default of underlying reference token
+                // D18{UoA/ref} = D18{UoA/target} * D18{target/ref} / D18
+                uint192 peg = (pricePerTarget() * targetPerRef()) / FIX_ONE;
+
+                // D18{UoA/ref}= D18{UoA/ref} * D18{1} / D18
+                uint192 delta = (peg * defaultThreshold) / FIX_ONE; // D18{UoA/ref}
+
+                // If the price is below the default-threshold price, default eventually
+                // uint192(+/-) is the same as Fix.plus/minus
+                if (p < peg - delta || p > peg + delta) markStatus(CollateralStatus.IFFY);
+                else markStatus(CollateralStatus.SOUND);
+            } catch (bytes memory errData) {
+                // see: docs/solidity-style.md#Catching-Empty-Data
+                if (errData.length == 0) revert(); // solhint-disable-line reason-string
+                markStatus(CollateralStatus.IFFY);
+            }
+        }
+        prevReferencePrice = referencePrice;
+
+        CollateralStatus newStatus = status();
+        if (oldStatus != newStatus) {
+            emit DefaultStatusChanged(oldStatus, newStatus);
+        }
+
+        // No interactions beyond the initial refresher
+    }
+
+    /// @return {ref/tok} Quantity of whole reference units per whole collateral tokens
+    function refPerTok() public view override returns (uint192) {
+        // gets supply of token0 and token1
+        (uint112 _reserve0, uint112 _reserve1,) = IFraxswapPair(address(erc20));
+
+        // rate = sqrt(x * y)/L
+        uint256 rate = (Math.sqrt(uint256(_reserve0) * uint256(_reserve1)) /
+                        IFraxswapPair(address(erc20)).totalSupply());
+
+        int8 shiftLeft = 8 - referenceERC20Decimals - 18;
+        return shiftl_toFix(rate, shiftLeft);
+    }
+}
