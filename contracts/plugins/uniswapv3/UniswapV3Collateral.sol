843d485c5e53669b753a4cf08c570bff76a3047c
Victor Galkin <vicglarson@gmail.com>
2022-11-19 18:57:51 +0300

    implement fallback price as price for one liquidity

--- contracts/plugins/uniswapv3/UniswapV3Collateral.sol
+++ contracts/plugins/uniswapv3/UniswapV3Collateral.sol
@@ -33,19 +33,43 @@ contract UniswapV3Collateral is Collateral {
         chainlinkFeedSecondAsset = chainlinkFeedSecondAsset_;
     }
 
-    /// Can return 0, can revert
-    /// Shortcut for price(false)
-    /// @return {UoA/tok} The current price(), without considering fallback prices
-    function strictPrice() external view override returns (uint192) {
-        (address token0, address token1, uint256 amount0, uint256 amount1) = IUniswapV3Wrapper(address(erc20))
-            .principal();
+    
+
+    function _calculatePrice(
+        address token0,
+        address token1,
+        uint256 amount0,
+        uint256 amount1,
+        uint256 liquidity
+    ) internal view returns (uint192) {
         uint192 price0 = chainlinkFeed.price(oracleTimeout);
         uint192 price1 = chainlinkFeedSecondAsset.price(oracleTimeout);
-        uint256 price0adj = price0 * amount0;
-        uint256 price1adj = price1 * amount1;
+        uint256 price0adj = (price0 * amount0) / liquidity;
+        uint256 price1adj = (price1 * amount1) / liquidity;
         int8 shift0 = -int8(IERC20Metadata(token0).decimals()) - 18;
         int8 shift1 = -int8(IERC20Metadata(token1).decimals()) - 18;
         return uint192(shiftl_toFix(price0adj, shift0) + shiftl_toFix(price1adj, shift1));
+    }
+
+    function strictPrice() external view override returns (uint192) {
+        (address token0, address token1, uint256 amount0, uint256 amount1) = IUniswapV3Wrapper(address(erc20))
+            .principal();
+        return _calculatePrice(token0, token1, amount0, amount1, IERC20(erc20).totalSupply());
+    }
+
+    function _fallbackPrice() internal view returns (uint192) {
+        (address token0, address token1, uint256 amount0, uint256 amount1) = IUniswapV3Wrapper(address(erc20))
+            .pricePerOneLiquidity();
+        return _calculatePrice(token0, token1, amount0, amount1, 1);
+    }
+
+    function price(bool allowFallback) public view override returns (bool isFallback, uint192) {
+        try this.strictPrice() returns (uint192 p) {
+            return (false, p);
+        } catch {
+            require(allowFallback, "price reverted without failover enabled");
+            return (true, _fallbackPrice());
+        }
     }
 
     //TODO RefPerTok() always equals 1 but we need to implement check
