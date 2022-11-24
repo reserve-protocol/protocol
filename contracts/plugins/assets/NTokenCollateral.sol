ad4fec4ca1298fe782f0e308023aa0ea0cbe97ee
sandro <devspuig@pm.me>
2022-11-24 18:50:06 +0100

    Use RevenueHiding abstract in NToken plugin

--- contracts/plugins/assets/NTokenCollateral.sol
+++ contracts/plugins/assets/NTokenCollateral.sol
@@ -3,9 +3,9 @@ pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "contracts/plugins/assets/AbstractCollateral.sol";
 import "contracts/plugins/assets/INotionalProxy.sol";
 import "contracts/plugins/assets/INTokenERC20Proxy.sol";
+import "contracts/plugins/assets/RevenueHiding.sol";
 import "contracts/libraries/Fixed.sol";
 
 /**
@@ -13,15 +13,13 @@ import "contracts/libraries/Fixed.sol";
  * @notice Collateral plugin for a NToken of fiat collateral
  * Expected: {tok} != {ref}, {ref} is pegged to {target} unless defaulting, {target} == {UoA}
  */
-contract NTokenCollateral is Collateral {
+contract NTokenCollateral is RevenueHiding {
     using OracleLib for AggregatorV3Interface;
     using FixLib for uint192;
 
     INTokenERC20Proxy public immutable nToken;
     INotionalProxy public immutable notionalProxy;
     uint192 public immutable defaultThreshold; // {%} percentage allowed of de-peg // D18
-    uint192 private immutable marginRatio; // max drop allowed // D0
-    uint192 private maxRefPerTok; // max rate previously seen {ref/tok} // D18
 
     constructor(
         uint192 _fallbackPrice,
@@ -35,101 +33,59 @@ contract NTokenCollateral is Collateral {
         uint192 _defaultThreshold,
         uint192 _allowedDrop
     )
-    Collateral(
+    RevenueHiding(
         _fallbackPrice,
         _chainlinkFeed,
         _erc20Collateral,
         _maxTradeVolume,
         _oracleTimeout,
         _targetName,
-        _delayUntilDefault
+        _delayUntilDefault,
+        _allowedDrop
     )
     {
         require(_notionalProxy != address(0), "Notional proxy address missing");
-        require(_allowedDrop < FIX_ONE, "Allowed refPerTok drop out of range");
 
         nToken = INTokenERC20Proxy(address(_erc20Collateral));
         notionalProxy = INotionalProxy(_notionalProxy);
         defaultThreshold = _defaultThreshold;
-        marginRatio = FIX_ONE - _allowedDrop;
     }
 
-    /// Can return 0, can revert
-    /// Shortcut for price(false)
-    /// @return {UoA/tok} The current price(), without considering fallback prices
-    function strictPrice() external view returns (uint192) {
-        return chainlinkFeed.price(oracleTimeout).mul(actualRefPerTok());
-    }
+    function checkReferencePeg() internal override {
+        try chainlinkFeed.price_(oracleTimeout) returns (uint192 currentPrice) {
+            // the peg of our reference is always ONE target
+            uint192 peg = FIX_ONE;
 
-    /// Refresh exchange rates and update default status.
-    /// The Reserve protocol calls this at least once per transaction, before relying on
-    /// this collateral's prices or default status.
-    function refresh() external override {
-        if (alreadyDefaulted()) return;
+            // since peg is ONE we dont need to operate the threshold to get the delta
+            uint192 delta = defaultThreshold;
 
-        CollateralStatus oldStatus = status();
-
-        uint192 _actualRefPerTok = actualRefPerTok();
-
-        // check if refPerTok rate has decreased below accepted threshold
-        if (_actualRefPerTok < refPerTok()) {
-            markStatus(CollateralStatus.DISABLED);
-        } else {
-            // if it didn't, check the peg of the reference
-            try chainlinkFeed.price_(oracleTimeout) returns (uint192 currentPrice) {
-                // the peg of our reference is always ONE target
-                uint192 peg = FIX_ONE;
-
-                // since peg is ONE we dont need to operate the threshold to get the delta
-                uint192 delta = defaultThreshold;
-
-                // If the price is below the default-threshold price, default eventually
-                // uint192(+/-) is the same as Fix.plus/minus
-                if (
-                    currentPrice < peg - delta ||
-                    currentPrice > peg + delta
-                ) {
-                    markStatus(CollateralStatus.IFFY);
-                }
-                else {
-                    markStatus(CollateralStatus.SOUND);
-                }
-            } catch (bytes memory errData) {
-                // see: docs/solidity-style.md#Catching-Empty-Data
-                if (errData.length == 0) revert();
-                // solhint-disable-line reason-string
+            // If the price is below the default-threshold price, default eventually
+            // uint192(+/-) is the same as Fix.plus/minus
+            if (
+                currentPrice < peg - delta ||
+                currentPrice > peg + delta
+            ) {
                 markStatus(CollateralStatus.IFFY);
             }
-        }
-
-        // store refRate for the next iteration
-        if (_actualRefPerTok > maxRefPerTok) {
-            maxRefPerTok = _actualRefPerTok;
-        }
-
-        // check if updated status
-        CollateralStatus newStatus = status();
-        if (oldStatus != newStatus) {
-            emit DefaultStatusChanged(oldStatus, newStatus);
+            else {
+                markStatus(CollateralStatus.SOUND);
+            }
+        } catch (bytes memory errData) {
+            // see: docs/solidity-style.md#Catching-Empty-Data
+            if (errData.length == 0) revert();
+            // solhint-disable-line reason-string
+            markStatus(CollateralStatus.IFFY);
         }
     }
 
     /// @return {ref/tok} Actual quantity of whole reference units per whole collateral tokens
-    function actualRefPerTok() public view returns (uint192) {
+    function actualRefPerTok() public view override returns (uint192) {
         // fetch value of all current liquidity
         uint192 valueOfAll = _safeWrap(uint256(nToken.getPresentValueUnderlyingDenominated()));
         // fetch total supply of tokens
         uint192 totalSupply = _safeWrap(nToken.totalSupply());
         // divide to get the value of one token
         return valueOfAll.div(totalSupply);
-    }
-
-    /// @return {ref/tok} Quantity of whole reference units per whole collateral tokens
-    /// @notice This amount has a {margin} space discounted to allow a certain drop on value
-    function refPerTok() public view override returns (uint192) {
-        // We can do this because we know {margin_ratio} is a
-        // small controlled number so it won't overflow
-        return maxRefPerTok.div(FIX_ONE).mul(marginRatio);
     }
 
     /// Claim rewards earned by holding a balance of the ERC20 token
