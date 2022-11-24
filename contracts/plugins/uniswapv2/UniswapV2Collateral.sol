40a9da255ed84481a791dc5d0d8f7414c1097bcc
Victor Galkin <vicglarson@gmail.com>
2022-11-24 15:48:51 +0300

    take liquidity calc from UniswapV2Pair.sol mint

--- contracts/plugins/uniswapv2/UniswapV2Collateral.sol
+++ contracts/plugins/uniswapv2/UniswapV2Collateral.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 import "../assets/AbstractCollateral.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "hardhat/console.sol";
 
@@ -64,11 +65,50 @@ contract UniswapV2Collateral is Collateral {
         (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = pair.getReserves();
         uint256 rootK = Math.sqrt(reserve0 * reserve1);
         return uint192(rootK / pair.totalSupply());
-    } 
+    }
 
     function _fallbackPrice() public view returns (uint192) {
         //TODO calculate expected price for one liquidity
         return strictPrice();
+    }
+
+    // from UniswapV2Pair
+    // this low-level function should be called from a contract which performs important safety checks
+    function mint(address to) external returns (uint256 liquidity) {
+        IUniswapV2Pair pair = IUniswapV2Pair(address(erc20));
+        (uint112 _reserve0, uint112 _reserve1, ) = pair.getReserves(); // gas savings
+        uint256 balance0 = IERC20(pair.token0()).balanceOf(address(this));
+        uint256 balance1 = IERC20(pair.token1()).balanceOf(address(this));
+        uint256 amount0 = balance0 - _reserve0;
+        uint256 amount1 = balance1 - _reserve1;
+
+        address feeTo = IUniswapV2Factory(pair.factory()).feeTo();
+        bool feeOn = feeTo != address(0);
+        uint256 _kLast = pair.kLast();
+        uint256 liquidityFee = 0;
+        uint256 _totalSupply = pair.totalSupply();
+        // at first mint kLast = 0 totalSupply = 0
+        if (feeOn) {
+            //dont mint once after feeOn we need liquidity = sqrt(a0, a1).sub(minimumLiquidity)
+            if (_kLast != 0) {
+                uint256 rootK = Math.sqrt(_reserve0 * _reserve1);
+                uint256 rootKLast = Math.sqrt(_kLast);
+                if (rootK > rootKLast) {
+                    uint256 numerator = _totalSupply * (rootK - rootKLast);
+                    uint256 denominator = (rootK * 5) + rootKLast;
+                    //to mint to feeOn
+                    liquidityFee = numerator / denominator;
+                    _totalSupply += liquidityFee;
+                }
+            }
+        }
+  
+        if (_totalSupply == 0) {
+            liquidity = Math.sqrt(amount0 * amount1) - pair.MINIMUM_LIQUIDITY();
+        } else {
+            liquidity = Math.min(amount0 * _totalSupply / _reserve0, amount1 * _totalSupply / _reserve1);
+        }
+        liquidity += liquidityFee;
     }
 
     function price(bool allowFallback) public view override returns (bool isFallback, uint192) {
@@ -80,7 +120,6 @@ contract UniswapV2Collateral is Collateral {
         }
     }
 
-    
     function claimRewards() external override {
         IUniswapV2Pair pair = IUniswapV2Pair(address(erc20));
         emit RewardsClaimed(IERC20(pair.token0()), 0);
