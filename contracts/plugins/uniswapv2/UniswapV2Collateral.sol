48924130e44982df824d1a594e2d5211d88ce026
Victor Galkin <vicglarson@gmail.com>
2022-11-28 22:25:31 +0300

    Cleanup sell price for univ2

--- contracts/plugins/uniswapv2/UniswapV2Collateral.sol
+++ contracts/plugins/uniswapv2/UniswapV2Collateral.sol
@@ -56,59 +56,61 @@ contract UniswapV2Collateral is Collateral {
 
     function strictPrice() external view override returns (uint192) {
         IUniswapV2Pair pair = IUniswapV2Pair(address(erc20));
-        (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = pair.getReserves();
-        return _calculatePrice(pair.token0(), pair.token0(), reserve0, reserve1, IERC20(erc20).totalSupply());
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
+        return _calculatePrice(pair.token0(), pair.token1(), reserve0, reserve1, IERC20(erc20).totalSupply());
     }
 
+    // При инициализации мы создаём позицию на юнисвопе получаем некую ликвидити
+    // {tok} это erc20 токен который можно продавать
+    // {ref} это константа удерживаемая протоколом в качестве обеспечения
+    // если refPerTok - цена за которую можно продать наше количество токенов растёт,
+    // протокол продаёт избыток токенов и получает revenue
+    // amount0 * amount1 = k
+    // k может оставаться константой или расти
+    // totalSupply может убывать или расти, но убывает или растёт медленней чем k
+    // мы используем reserve0 и reserve1, а не балансы, хотя чтобы учитывать свопы,
+    // нужны балансы, но если учитывать балансы, то в момент включения, выключения
+    // фи ценность позиции, видимо, будет меняться
     function refPerTok() public view override returns (uint192) {
         IUniswapV2Pair pair = IUniswapV2Pair(address(erc20));
-        (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = pair.getReserves();
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
         uint256 rootK = Math.sqrt(reserve0 * reserve1);
         return uint192(rootK / pair.totalSupply());
     }
 
     function _fallbackPrice() public view returns (uint192) {
-        //TODO calculate expected price for one liquidity
+        //TODO or sellPrice
+        //TODO cant revert
         return strictPrice();
     }
 
-    // from UniswapV2Pair
-    // this low-level function should be called from a contract which performs important safety checks
-    function mint(address to) external returns (uint256 liquidity) {
+    // returns amounts can be obtained on burn liquidity
+    function sellPrice(uint256 liquidity) internal view returns (uint256 amount0, uint256 amount1) {
         IUniswapV2Pair pair = IUniswapV2Pair(address(erc20));
-        (uint112 _reserve0, uint112 _reserve1, ) = pair.getReserves(); // gas savings
-        uint256 balance0 = IERC20(pair.token0()).balanceOf(address(this));
-        uint256 balance1 = IERC20(pair.token1()).balanceOf(address(this));
-        uint256 amount0 = balance0 - _reserve0;
-        uint256 amount1 = balance1 - _reserve1;
-
+        (uint112 _reserve0, uint112 _reserve1, ) = pair.getReserves();
+        address _token0 = pair.token0();
+        address _token1 = pair.token1();
+        uint256 balance0 = IERC20(_token0).balanceOf(address(pair));
+        uint256 balance1 = IERC20(_token1).balanceOf(address(pair));
         address feeTo = IUniswapV2Factory(pair.factory()).feeTo();
         bool feeOn = feeTo != address(0);
         uint256 _kLast = pair.kLast();
         uint256 liquidityFee = 0;
         uint256 _totalSupply = pair.totalSupply();
-        // at first mint kLast = 0 totalSupply = 0
         if (feeOn) {
-            //dont mint once after feeOn we need liquidity = sqrt(a0, a1).sub(minimumLiquidity)
             if (_kLast != 0) {
                 uint256 rootK = Math.sqrt(_reserve0 * _reserve1);
                 uint256 rootKLast = Math.sqrt(_kLast);
                 if (rootK > rootKLast) {
                     uint256 numerator = _totalSupply * (rootK - rootKLast);
                     uint256 denominator = (rootK * 5) + rootKLast;
-                    //to mint to feeOn
                     liquidityFee = numerator / denominator;
                     _totalSupply += liquidityFee;
                 }
             }
         }
-  
-        if (_totalSupply == 0) {
-            liquidity = Math.sqrt(amount0 * amount1) - pair.MINIMUM_LIQUIDITY();
-        } else {
-            liquidity = Math.min(amount0 * _totalSupply / _reserve0, amount1 * _totalSupply / _reserve1);
-        }
-        liquidity += liquidityFee;
+        amount0 = (liquidity * balance0) / _totalSupply;
+        amount1 = (liquidity * balance1) / _totalSupply;
     }
 
     function price(bool allowFallback) public view override returns (bool isFallback, uint192) {
