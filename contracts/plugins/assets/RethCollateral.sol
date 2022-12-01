fa08347e2d1f52b4ba2fdcee74a3ff716c2a45ce
stobiewan <getsforwarded@gmail.com>
2022-12-01 15:23:43 +1300

    add reth collateral and interface

---
+++ contracts/plugins/assets/RethCollateral.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: BlueOak-1.0.0
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "contracts/plugins/assets/AbstractCollateral.sol";
+import "contracts/plugins/assets/IReth.sol";
+import "contracts/plugins/assets/OracleLib.sol";
+import "contracts/libraries/Fixed.sol";
+
+/**
+ * @title RethCollateral
+ * @notice Collateral plugin for RETH,
+ * tok = RETH
+ * ref = ETH
+ * tar = ETH
+ * UoA = USD
+ */
+contract RethCollateral is Collateral {
+    using FixLib for uint192;
+    using OracleLib for AggregatorV3Interface;
+
+    uint192 public prevReferencePrice; // previous rate, {collateral/reference}
+
+    /// @param chainlinkFeed_ Feed units: {UoA/ref}
+    /// @param maxTradeVolume_ {UoA} The max trade volume, in UoA
+    /// @param oracleTimeout_ {s} The number of seconds until a oracle value becomes invalid
+    /// @param delayUntilDefault_ {s} The number of seconds deviation must occur before default
+    constructor(
+        uint192 fallbackPrice_,
+        AggregatorV3Interface chainlinkFeed_,
+        IERC20Metadata erc20_,
+        uint192 maxTradeVolume_,
+        uint48 oracleTimeout_,
+        bytes32 targetName_,
+        uint256 delayUntilDefault_
+    )
+        Collateral(
+            fallbackPrice_,
+            chainlinkFeed_,
+            erc20_,
+            maxTradeVolume_,
+            oracleTimeout_,
+            targetName_,
+            delayUntilDefault_
+        )
+    {
+        prevReferencePrice = refPerTok();
+    }
+
+    /// @return {UoA/tok} Our best guess at the market price of 1 whole reth in UoA
+    function strictPrice() public view virtual override returns (uint192) {
+        // {UoA/tok} = {UoA/ref} * {ref/tok}
+        return chainlinkFeed.price(oracleTimeout).mul(refPerTok());
+    }
+
+    /// Refresh exchange rates and update default status.
+    /// @custom:interaction RCEI
+    function refresh() external virtual override {
+        if (alreadyDefaulted()) return;
+        CollateralStatus oldStatus = status();
+
+        // Check for hard default
+        uint192 referencePrice = refPerTok();
+        // uint192(<) is equivalent to Fix.lt
+        if (referencePrice < prevReferencePrice) {
+            markStatus(CollateralStatus.DISABLED);
+        } else {
+            try chainlinkFeed.price_(oracleTimeout) returns (uint192) {
+                markStatus(CollateralStatus.SOUND);
+            } catch (bytes memory errData) {
+                // see: docs/solidity-style.md#Catching-Empty-Data
+                if (errData.length == 0) revert(); // solhint-disable-line reason-string
+                markStatus(CollateralStatus.IFFY);
+            }
+        }
+        prevReferencePrice = referencePrice;
+
+        CollateralStatus newStatus = status();
+        if (oldStatus != newStatus) {
+            emit DefaultStatusChanged(oldStatus, newStatus);
+        }
+    }
+
+    /// @return {ref/tok} Quantity of whole reference units per whole collateral tokens
+    function refPerTok() public view override returns (uint192) {
+        uint256 rate = IReth(address(erc20)).getExchangeRate();
+        return shiftl_toFix(rate, 8);
+    }
+
+    /// @return {UoA/target} The price of a target unit in UoA
+    function pricePerTarget() public view override returns (uint192) {
+        return chainlinkFeed.price(oracleTimeout);
+    }
+}
