01419f19b782891c642222829ac5f975a08fc20d
Taylor Brent <taylor.w.brent@gmail.com>
2022-12-05 18:51:50 -0400

    refresh all assets not just collateral

--- contracts/p0/AssetRegistry.sol
+++ contracts/p0/AssetRegistry.sol
@@ -30,8 +30,7 @@ contract AssetRegistryP0 is ComponentP0, IAssetRegistry {
     function refresh() public {
         // It's a waste of gas to require notPausedOrFrozen because assets can be updated directly
         for (uint256 i = 0; i < _erc20s.length(); i++) {
-            IAsset asset = assets[IERC20(_erc20s.at(i))];
-            if (asset.isCollateral()) ICollateral(address(asset)).refresh();
+            assets[IERC20(_erc20s.at(i))].refresh();
         }
     }
 
--- contracts/p1/AssetRegistry.sol
+++ contracts/p1/AssetRegistry.sol
@@ -47,8 +47,7 @@ contract AssetRegistryP1 is ComponentP1, IAssetRegistry {
         // It's a waste of gas to require notPausedOrFrozen because assets can be updated directly
         uint256 length = _erc20s.length();
         for (uint256 i = 0; i < length; ++i) {
-            IAsset asset = assets[IERC20(_erc20s.at(i))];
-            if (asset.isCollateral()) ICollateral(address(asset)).refresh();
+            assets[IERC20(_erc20s.at(i))].refresh();
         }
     }
 
