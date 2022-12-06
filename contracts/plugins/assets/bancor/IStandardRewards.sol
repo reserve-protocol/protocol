cd947457f5cc979cac3500b2ec9e6e5d633da66b
Hublax <hubert.baciocha@gmail.com>
2022-12-06 15:38:04 +0100

    Add Rewards interface and fill claimRewards()

---
+++ contracts/plugins/assets/bancor/IStandardRewards.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BlueOak-1.0.0
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IStandardRewards is IERC20{
+    function latestProgramId(address pool) external view returns (uint256);
+
+    function claimRewards(uint256[] calldata ids) external returns (uint256);
+}
