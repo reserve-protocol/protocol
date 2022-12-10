91de4aabbbfe4d3ff49b723b71e7daba6924aad7
Nirmal Manoj <nimmumanoj@gmail.com>
2022-12-11 03:50:27 +0530

    truefi block update

--- test/integration/fork-block-numbers.ts
+++ test/integration/fork-block-numbers.ts
@@ -1,7 +1,7 @@
 const forkBlockNumber = {
   'aave-compound-rewards': 12521999, // Ethereum
-  // 'mainnet-deployment': 15690042, // Ethereum
-  'mainnet-deployment': 16156193, // Ethereum - 11/12/22 12:18 AM latest, https://etherscan.io/block/16156193#consensusinfo
+  'mainnet-deployment': 15690042, // Ethereum
+  'trueFi-deployment': 16156193, // Ethereum - 11/12/22 12:18 AM latest, https://etherscan.io/block/16156193#consensusinfo
   default: 14916729, // Ethereum
 }
 
