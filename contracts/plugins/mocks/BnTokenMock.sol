f3b9209bc3705ec0670d20cfc3cf74d53f7fda0d
Hublax <hubert.baciocha@gmail.com>
2022-12-11 16:17:51 +0100

    edit mocks and add tests

--- contracts/plugins/mocks/BnTokenMock.sol
+++ contracts/plugins/mocks/BnTokenMock.sol
@@ -6,46 +6,18 @@ import "contracts/libraries/Fixed.sol";
 import "./ERC20Mock.sol";
 
 contract BnTokenMock is ERC20Mock {
-    using FixLib for uint192;
-    address internal _underlyingToken;
-
-    uint256 internal _exchangeRate;
+    uint256 private underlying;
 
     constructor(
         string memory name,
-        string memory symbol,
-        address underlyingToken
-    ) ERC20Mock(name, symbol) {
-        _underlyingToken = underlyingToken;
-        _exchangeRate = _toExchangeRate(FIX_ONE);
+        string memory symbol
+    ) ERC20Mock(name, symbol) {}
+
+    function setUnderlying(uint256 amount) external {
+        underlying = amount;
     }
 
-    function decimals() public pure override returns (uint8) {
-        return 18;
-    }
-
-    function exchangeRateCurrent() external returns (uint256) {
-        _exchangeRate = _exchangeRate; // just to avoid sol warning
-        return _exchangeRate;
-    }
-
-    function exchangeRateStored() external view returns (uint256) {
-        return _exchangeRate;
-    }
-
-    /// @param fiatcoinRedemptionRate {fiatTok/tok}
-    function setExchangeRate(uint192 fiatcoinRedemptionRate) external {
-        _exchangeRate = _toExchangeRate(fiatcoinRedemptionRate);
-    }
-
-    function underlying() external view returns (address) {
-        return _underlyingToken;
-    }
-
-    function _toExchangeRate(uint192 fiatcoinRedemptionRate) internal view returns (uint256) {
-        /// From Compound Docs: The current exchange rate, scaled by 10^(18 - 8 + Underlying Token Decimals).
-        uint192 start = shiftl_toFix(2, -2); // 0.02
-        int8 leftShift = 18 - int8(decimals()) + int8(IERC20Metadata(_underlyingToken).decimals());
-        return fiatcoinRedemptionRate.shiftl(leftShift).mul_toUint(start);
+    function poolTokenToUnderlying() external view returns (uint256){
+        return underlying;
     }
 }
