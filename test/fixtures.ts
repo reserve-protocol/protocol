4fe4d5a8e11da0511005ab611644347d24c3d058
pmckelvy1 <pmckelvy1@gmail.com>
2022-12-12 11:30:12 -0500

    add coverage line for GnosisTrade.init edge case.

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -211,6 +211,26 @@ async function collateralFixture(
     )
     return [erc20, coll]
   }
+  const make0DecimalCollateral = async (symbol: string): Promise<[ZeroDecimalMock, Collateral]> => {
+    const erc20: ZeroDecimalMock = <ZeroDecimalMock>await USDC.deploy(symbol + ' Token', symbol)
+    const chainlinkFeed: MockV3Aggregator = <MockV3Aggregator>(
+      await MockV3AggregatorFactory.deploy(0, bn('1'))
+    )
+
+    const coll = <FiatCollateral>(
+      await FiatCollateralFactory.deploy(
+        fp('1'),
+        chainlinkFeed.address,
+        erc20.address,
+        config.rTokenMaxTradeVolume,
+        ORACLE_TIMEOUT,
+        ethers.utils.formatBytes32String('USD'),
+        defaultThreshold,
+        delayUntilDefault
+      )
+    )
+    return [erc20, coll]
+  }
   const makeCTokenCollateral = async (
     symbol: string,
     referenceERC20: ERC20Mock,
@@ -288,6 +308,7 @@ async function collateralFixture(
     await busd[1].chainlinkFeed(),
     aaveToken
   )
+  const zcoin = await make0DecimalCollateral('ZCOIN') // zero decimals
   const erc20s = [
     dai[0],
     usdc[0],
@@ -300,6 +321,7 @@ async function collateralFixture(
     ausdc[0],
     ausdt[0],
     abusd[0],
+    zcoin[0]
   ]
   const collateral = [
     dai[1],
@@ -313,6 +335,7 @@ async function collateralFixture(
     ausdc[1],
     ausdt[1],
     abusd[1],
+    zcoin[1]
   ]
 
   // Create the initial basket
