009f4b1c137b4d21b3d4bee6437c3cc3ad34a8d5
Nirmal Manoj <nimmumanoj@gmail.com>
2022-12-14 12:21:11 +0530

    Code documentation for TrueFi Plugin Interfaces

--- contracts/plugins/truefi/ITFToken.sol
+++ contracts/plugins/truefi/ITFToken.sol
@@ -1,19 +1,28 @@
 // SPDX-License-Identifier: BlueOak-1.0.0
 pragma solidity 0.8.9;
 
-/// External Interface for CTokens
-// See: https://github.com/compound-finance/compound-protocol/blob/master/contracts/CToken.sol
+/// External Interface for TFTokens and TrueMultiFarm contract.
+/// Interface for only the required functions are implemented here.
+/// tfUSDC contract: https://etherscan.io/address/0xA991356d261fbaF194463aF6DF8f0464F8f1c742
+/// TrueMultiFarm contract: https://etherscan.io/address/0xec6c3FD795D6e6f202825Ddb56E01b3c128b0b10
 
 interface ITFToken {
+    /// @dev From TrueFi Docs:
+    /// The current pool value in underlying token.
     function poolValue() external view returns (uint256);
 
+    /// @dev The current total supply of the token.
     function totalSupply() external view returns (uint256);
 }
 
 interface ITRUFarm {
+    /// @dev From TrueFi Docs (TrueMultiFarm contract):
+    /// Number of accrued TRU rewards.
     function claimable(address token, address account) external view returns (uint256);
-
+    
+    /// @dev Lenders can claim TRU rewards using claim()
     function claim(address[] calldata tokens) external;
 
+    /// @dev Contract address of the reward token (TRU)
     function rewardToken() external view returns (address);
 }
