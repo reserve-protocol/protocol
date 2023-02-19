a915d18cafb5ba3e536584ee0dc06ab5f02de5d9
Taylor Brent <taylor.w.brent@gmail.com>
2023-02-18 21:08:45 -0400

    basket range.top

--- contracts/interfaces/IBasketHandler.sol
+++ contracts/interfaces/IBasketHandler.sol
@@ -6,6 +6,11 @@ import "../libraries/Fixed.sol";
 import "./IAsset.sol";
 import "./IComponent.sol";
 
+struct BasketRange {
+    uint192 bottom; // {BU}
+    uint192 top; // {BU}
+}
+
 /**
  * @title IBasketHandler
  * @notice The BasketHandler aims to maintain a reference basket of constant target unit amounts.
@@ -94,8 +99,9 @@ interface IBasketHandler is IComponent {
         view
         returns (address[] memory erc20s, uint256[] memory quantities);
 
-    /// @return baskets {BU} The quantity of complete baskets at an address. A balance for BUs
-    function basketsHeldBy(address account) external view returns (uint192 baskets);
+    /// @return top {BU} The number of partial basket units: e.g max(coll.map((c) => c.balAsBUs())
+    ///         bottom {BU} The number of whole basket units held by the account
+    function basketsHeldBy(address account) external view returns (BasketRange memory);
 
     /// Should not revert
     /// @return low {UoA/BU} The lower end of the price estimate
