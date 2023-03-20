698a2acc00c38e6d2973869d6cf8be256f75ae3d
Taylor Brent <taylor.w.brent@gmail.com>
2023-03-19 21:40:50 -0400

    add SfraxEth revenue hiding tests

---
+++ contracts/plugins/mocks/SfraxEthMock.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: BlueOak-1.0.0
+pragma solidity 0.8.17;
+
+import "./ERC20Mock.sol";
+
+contract SfraxEthMock is ERC20Mock {
+    uint256 public pricePerShare;
+
+    constructor() ERC20Mock("Mock SfrxETH", "SfrxEth") {}
+
+    function setPricePerShare(uint256 mockValue) external {
+        pricePerShare = mockValue;
+    }
+}
