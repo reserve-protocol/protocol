83bc65206a083da0bcc1b123634103e623a4255e
pmckelvy1 <pmckelvy1@gmail.com>
2023-04-03 20:57:25 -0400

    refactor to simplify file, add checks for 2.1.0 release.

---
+++ tasks/testing/upgrade-checker-utils/constants.ts
@@ -0,0 +1,6 @@
+import { networkConfig } from "#/common/configuration";
+
+export const whales: { [key: string]: string } = {
+  [networkConfig['1'].tokens.USDT!]: '0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503',
+  [networkConfig['1'].tokens.RSR!]: '0x6bab6EB87Aa5a1e4A8310C73bDAAA8A5dAAd81C1',
+}
