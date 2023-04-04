06429a3056ec276a9fd89f365a03fbc44e89a0d3
pmckelvy1 <pmckelvy1@gmail.com>
2023-04-04 15:19:57 -0400

    add md file for writing collateral plugins. (#751)
    * add md file for writing collateral plugins.
    
    * lint fixes.
    
    * edit based on comments.

--- test/plugins/individual-collateral/pluginTestTypes.ts
+++ test/plugins/individual-collateral/pluginTestTypes.ts
@@ -4,6 +4,10 @@ import { MockV3Aggregator, TestICollateral, IERC20Metadata } from '../../../type
 
 type Fixture<T> = () => Promise<T>
 
+// The basic fixture context used in the collateral plugin tests
+//  extend this to include other contracts that are deployed or needed as part of the
+//  tests that will be run for the plugin (ie. if the collateral uses a wrapper, or extra
+//  chainlink feed)
 export interface CollateralFixtureContext {
   collateral: TestICollateral
   chainlinkFeed: MockV3Aggregator
@@ -12,6 +16,8 @@ export interface CollateralFixtureContext {
   alice?: SignerWithAddress
 }
 
+// The basic constructor arguments for a collateral plugin
+//  extend this to define the constructor arguments for the collateral plugin being tested
 export interface CollateralOpts {
   erc20?: string
   targetName?: string
@@ -26,37 +32,84 @@ export interface CollateralOpts {
   revenueHiding?: BigNumberish
 }
 
+// A function to deploy the collateral plugin and return the deployed instance of the contract
 export type DeployCollateralFunc = (opts: CollateralOpts) => Promise<TestICollateral>
+
+// A function to deploy and return the plugin-specific test suite context
 export type MakeCollateralFixtureFunc<T extends CollateralFixtureContext> = (
   alice: SignerWithAddress,
   opts: CollateralOpts
 ) => Fixture<T>
+
+// A function to mint a certain amount of collateral to a target address
 export type MintCollateralFunc<T extends CollateralFixtureContext> = (
   ctx: T,
   amount: BigNumberish,
   user: SignerWithAddress,
   recipient: string
 ) => Promise<void>
+
+// The interface that defines the test suite for the collateral plugin
 export interface CollateralTestSuiteFixtures<T extends CollateralFixtureContext> {
+  // a function to deploy the collateral plugin and return the deployed instance of the contract
   deployCollateral: DeployCollateralFunc
+
+  // a group of tests, specific to the collateral plugin, focused on the plugin's constructor
   collateralSpecificConstructorTests: () => void
+
+  // a group of tests, specific to the collateral plugin, focused on status checks
   collateralSpecificStatusTests: () => void
+
+  // a function to be run in the `beforeEach` block of the rewards tests
   beforeEachRewardsTest: (ctx: T) => void
+
+  // a function to deploy and return the plugin-specific test suite context
   makeCollateralFixtureContext: MakeCollateralFixtureFunc<T>
+
+  // a function to mint a certain amount of collateral to a target address
   mintCollateralTo: MintCollateralFunc<T>
+
+  // a function to reduce the value of `targetPerRef`
   reduceTargetPerRef: (ctx: T, pctDecrease: BigNumberish) => void
+
+  // a function to increase the value of `targetPerRef`
   increaseTargetPerRef: (ctx: T, pctIncrease: BigNumberish) => void
+
+  // a function to reduce the value of `refPerTok`
   reduceRefPerTok: (ctx: T, pctDecrease: BigNumberish) => void
+
+  // a function to increase the value of `refPerTok`
   increaseRefPerTok: (ctx: T, pctIncrease: BigNumberish) => void
+
+  // a function to calculate the expected price (ignoring oracle error)
+  //  that should be returned from `plugin.price()`
   getExpectedPrice: (ctx: T) => Promise<BigNumber>
+
+  // toggle on or off: tests that claim rewards (off if the plugin does not receive rewards)
   itClaimsRewards: Mocha.TestFunction | Mocha.PendingTestFunction
+
+  // toggle on or off: tests that focus on a targetPerRef default
   itChecksTargetPerRefDefault: Mocha.TestFunction | Mocha.PendingTestFunction
+
+  // toggle on or off: tests that focus on a refPerTok default
   itChecksRefPerTokDefault: Mocha.TestFunction | Mocha.PendingTestFunction
+
+  // toggle on or off: tests that focus on price changes
   itChecksPriceChanges: Mocha.TestFunction | Mocha.PendingTestFunction
+
+  // toggle on or off: tests that focus on revenue hiding (off if plugin does not hide revenue)
   itHasRevenueHiding: Mocha.TestFunction | Mocha.PendingTestFunction
-  itIsPricedByPeg?: boolean // does the peg price matter for the results of tryPrice()?
+
+  // does the peg price matter for the results of tryPrice()?
+  itIsPricedByPeg?: boolean
+
+  // a function to reset the fork to a desired block
   resetFork: () => void
+
+  // the name of the collateral plugin being tested
   collateralName: string
+
+  // the default answer that will come from the chainlink feed after deployment
   chainlinkDefaultAnswer: BigNumberish
 }
 
