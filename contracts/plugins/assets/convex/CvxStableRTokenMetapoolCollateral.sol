e8c0cade900841834481096f7364cb97f8bf88e1
Taylor Brent <taylor.w.brent@gmail.com>
2023-04-08 20:32:43 -0400

    CvxStableRTokenMetapoolCollateral: use RToken price directly

--- contracts/plugins/assets/convex/CvxStableRTokenMetapoolCollateral.sol
+++ contracts/plugins/assets/convex/CvxStableRTokenMetapoolCollateral.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: ISC
 pragma solidity 0.8.17;
 
-import "../../../interfaces/IRTokenOracle.sol";
 import "./CvxStableMetapoolCollateral.sol";
 
 /**
@@ -17,7 +16,7 @@ import "./CvxStableMetapoolCollateral.sol";
 contract CvxStableRTokenMetapoolCollateral is CvxStableMetapoolCollateral {
     using FixLib for uint192;
 
-    IRTokenOracle public immutable rTokenOracle;
+    IAssetRegistry internal immutable reg; // AssetRegistry of pairedToken
 
     /// @param config.chainlinkFeed Feed units: {UoA/pairedTok}
     /// @dev config.chainlinkFeed/oracleError/oracleTimeout are unused; set chainlinkFeed to 0x1
@@ -27,8 +26,7 @@ contract CvxStableRTokenMetapoolCollateral is CvxStableMetapoolCollateral {
         uint192 revenueHiding,
         PTConfiguration memory ptConfig,
         ICurveMetaPool metapoolToken_,
-        uint192 pairedTokenDefaultThreshold_,
-        IRTokenOracle rTokenOracle_
+        uint192 pairedTokenDefaultThreshold_
     )
         CvxStableMetapoolCollateral(
             config,
@@ -38,13 +36,11 @@ contract CvxStableRTokenMetapoolCollateral is CvxStableMetapoolCollateral {
             pairedTokenDefaultThreshold_
         )
     {
-        require(address(rTokenOracle_) != address(0), "rTokenOracle missing");
-        rTokenOracle = rTokenOracle_;
+        reg = IRToken(address(pairedToken)).main().assetRegistry();
     }
 
     /// Can revert, used by `_anyDepeggedOutsidePool()`
     /// Should not return FIX_MAX for low
-    /// Should only return FIX_MAX for high if low is 0
     /// @return lowPaired {UoA/pairedTok} The low price estimate of the paired token
     /// @return highPaired {UoA/pairedTok} The high price estimate of the paired token
     function tryPairedPrice()
@@ -54,24 +50,6 @@ contract CvxStableRTokenMetapoolCollateral is CvxStableMetapoolCollateral {
         override
         returns (uint192 lowPaired, uint192 highPaired)
     {
-        // refresh price in oracle if needed
-        (Price memory p, ) = rTokenOracle.priceView(IRToken(address(pairedToken)), false);
-        return (p.low, p.high);
-    }
-
-    /// Should not revert (other than out-of-gas error / empty data)
-    /// Refresh exchange rates and update default status.
-    /// Have to override to add custom default checks
-    function refresh() public virtual override {
-        // refresh price in rTokenOracle
-        // solhint-disable no-empty-blocks
-        try rTokenOracle.price(IRToken(address(pairedToken)), false) {} catch (
-            bytes memory errData
-        ) {
-            // see: docs/solidity-style.md#Catching-Empty-Data
-            if (errData.length == 0) revert(); // solhint-disable-line reason-string
-            markStatus(CollateralStatus.IFFY);
-        }
-        super.refresh();
+        return reg.toAsset(pairedToken).price();
     }
 }
