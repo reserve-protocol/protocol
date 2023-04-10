6b19e54c47cc7da717609eff0fd1cc0b63e47691
Taylor Brent <taylor.w.brent@gmail.com>
2023-04-10 12:55:05 -0400

    move safeMul into library

--- contracts/plugins/assets/convex/CvxStableMetapoolCollateral.sol
+++ contracts/plugins/assets/convex/CvxStableMetapoolCollateral.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: ISC
 pragma solidity 0.8.17;
 
+import ".../../../libraries/SafeMul.sol";
 import "./CvxStableCollateral.sol";
 
 // solhint-disable no-empty-blocks
@@ -169,58 +170,8 @@ contract CvxStableMetapoolCollateral is CvxStableCollateral {
         aumLow += lowPaired.mul(pairedBal, FLOOR);
 
         // Add-in high part carefully
-        uint192 toAdd = safeMul(highPaired, pairedBal, CEIL);
+        uint192 toAdd = SafeMul.safeMul(highPaired, pairedBal, CEIL);
         if (aumHigh + uint256(toAdd) >= FIX_MAX) aumHigh = FIX_MAX;
         else aumHigh += toAdd;
-    }
-
-    /// Multiply two fixes, rounding up to FIX_MAX and down to 0
-    /// @param a First param to multiply
-    /// @param b Second param to multiply
-    function safeMul(
-        uint192 a,
-        uint192 b,
-        RoundingMode rounding
-    ) internal pure returns (uint192) {
-        // untestable:
-        //      a will never = 0 here because of the check in _price()
-        if (a == 0 || b == 0) return 0;
-        // untestable:
-        //      a = FIX_MAX iff b = 0
-        if (a == FIX_MAX || b == FIX_MAX) return FIX_MAX;
-
-        // return FIX_MAX instead of throwing overflow errors.
-        unchecked {
-            // p and mul *are* Fix values, so have 18 decimals (D18)
-            uint256 rawDelta = uint256(b) * a; // {D36} = {D18} * {D18}
-            // if we overflowed, then return FIX_MAX
-            if (rawDelta / b != a) return FIX_MAX;
-            uint256 shiftDelta = rawDelta;
-
-            // add in rounding
-            if (rounding == RoundingMode.ROUND) shiftDelta += (FIX_ONE / 2);
-            else if (rounding == RoundingMode.CEIL) shiftDelta += FIX_ONE - 1;
-
-            // untestable (here there be dragons):
-            // (below explanation is for the ROUND case, but it extends to the FLOOR/CEIL too)
-            //          A)  shiftDelta = rawDelta + (FIX_ONE / 2)
-            //      shiftDelta overflows if:
-            //          B)  shiftDelta = MAX_UINT256 - FIX_ONE/2 + 1
-            //              rawDelta + (FIX_ONE/2) = MAX_UINT256 - FIX_ONE/2 + 1
-            //              b * a = MAX_UINT256 - FIX_ONE + 1
-            //      therefore shiftDelta overflows if:
-            //          C)  b = (MAX_UINT256 - FIX_ONE + 1) / a
-            //      MAX_UINT256 ~= 1e77 , FIX_MAX ~= 6e57 (6e20 difference in magnitude)
-            //      a <= 1e21 (MAX_TARGET_AMT)
-            //      a must be between 1e19 & 1e20 in order for b in (C) to be uint192,
-            //      but a would have to be < 1e18 in order for (A) to overflow
-            if (shiftDelta < rawDelta) return FIX_MAX;
-
-            // return FIX_MAX if return result would truncate
-            if (shiftDelta / FIX_ONE > FIX_MAX) return FIX_MAX;
-
-            // return _div(rawDelta, FIX_ONE, rounding)
-            return uint192(shiftDelta / FIX_ONE); // {D18} = {D36} / {D18}
-        }
     }
 }
