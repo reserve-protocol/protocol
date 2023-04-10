f3b4c19c52b60a739dbd02313a1ede87d8f7f78c
pmckelvy1 <pmckelvy1@gmail.com>
2023-04-10 17:10:38 -0400

    add task to load address with RSR.

--- tasks/testing/tenderly.ts
+++ tasks/testing/tenderly.ts
@@ -1,13 +1,12 @@
 import { task } from 'hardhat/config'
 import { networkConfig } from '../../common/configuration'
 import { getChainId } from '../../common/blockchain-utils'
+import { fp } from '#/common/numbers'
 
-task('give-eth', 'Mints all the tokens to an address')
+task('give-eth', 'Mints ETH to an address on a tenderly fork')
   .addParam('address', 'Ethereum address to receive the tokens')
   .addParam('rpc', 'The Tenderly RPC endpoint')
   .setAction(async (params, hre) => {
-    const [deployer] = await hre.ethers.getSigners()
-
     const chainId = await getChainId(hre)
 
     // ********** Read config **********
@@ -21,4 +20,42 @@ task('give-eth', 'Mints all the tokens to an address')
         [params.address],
         hre.ethers.utils.hexValue(hre.ethers.utils.parseUnits('10', 'ether').toHexString()),
     ]);
+
+    console.log(`10 ETH sent to ${params.address}`)
+  })
+
+task('give-rsr', 'Mints RSR to an address on a tenderly fork')
+  .addParam('address', 'Ethereum address to receive the tokens')
+  .addParam('rpc', 'The Tenderly RPC endpoint')
+  .setAction(async (params, hre) => {
+    const chainId = await getChainId(hre)
+
+    // ********** Read config **********
+    if (!networkConfig[chainId]) {
+      throw new Error(`Missing network configuration for ${hre.network.name}`)
+    }
+
+    const rsr = await hre.ethers.getContractAt('ERC20Mock', networkConfig[chainId].tokens.RSR!)
+
+    const unsignedTx = await rsr.populateTransaction['transfer'](params.address, fp('100e6'))
+    const rsrWhale = "0x6bab6EB87Aa5a1e4A8310C73bDAAA8A5dAAd81C1"
+    const transactionParameters = [{
+        to: rsr.address,
+        from: rsrWhale,
+        data: unsignedTx.data,
+        gas: hre.ethers.utils.hexValue(300000),
+        gasPrice: hre.ethers.utils.hexValue(1),
+        value: hre.ethers.utils.hexValue(0)
+    }];
+
+    const forkProvider = new hre.ethers.providers.JsonRpcProvider(params.rpc);
+
+    await forkProvider.send('tenderly_setBalance', [
+        [rsrWhale],
+        hre.ethers.utils.hexValue(hre.ethers.utils.parseUnits('1', 'ether').toHexString()),
+    ]);
+
+    const txHash = await forkProvider.send('eth_sendTransaction', transactionParameters)
+
+    console.log(`100m RSR sent to ${params.address}`)
   })
