2d2cdd0d2b33d49c53034aa0c858c8f001a3033f
Julian R <56316686+julianmrodri@users.noreply.github.com>
2023-04-12 11:27:51 -0300

    remove __gap in p0

--- contracts/p0/BasketHandler.sol
+++ contracts/p0/BasketHandler.sol
@@ -709,11 +709,4 @@ contract BasketHandlerP0 is ComponentP0, IBasketHandler {
             return false;
         }
     }
-
-    /**
-     * @dev This empty reserved space is put in place to allow future versions to add new
-     * variables without shifting down storage in the inheritance chain.
-     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-     */
-    uint256[47] private __gap;
 }
