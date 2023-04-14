d324ef3fd04015760e62dc43851ab3453a847c73
joaovwfreire <jvwfreire@gmail.com>
2023-04-14 18:13:20 -0300

    helpers cleanup

--- test/plugins/individual-collateral/maker/helpers.ts
+++ test/plugins/individual-collateral/maker/helpers.ts
@@ -2,19 +2,8 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { IERC20Metadata, IWSTETH } from '../../../../typechain'
 import { whileImpersonating } from '../../../utils/impersonation'
 import { BigNumberish } from 'ethers'
-import { FORK_BLOCK, GUNIV3DAIUSDC1_WHALE, WSTETH_WHALE } from './constants'
+import { FORK_BLOCK, GUNIV3DAIUSDC1_WHALE } from './constants'
 import { getResetFork } from '../helpers'
-
-export const mintWSTETH = async (
-  wsteth: IWSTETH,
-  account: SignerWithAddress,
-  amount: BigNumberish,
-  recipient: string
-) => {
-  await whileImpersonating(WSTETH_WHALE, async (wstethWhale) => {
-    await wsteth.connect(wstethWhale).transfer(recipient, amount)
-  })
-}
 
 export const mintGUNIDAIUSCD = async (
   guni: IERC20Metadata,
