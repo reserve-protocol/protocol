95bf2d8d1b6ca8597acd68190c7ed87ae76d8b43
Taylor Brent <taylor.w.brent@gmail.com>
2023-05-03 17:59:29 -0400

    RewardableERC20Vault nits + documentation

--- contracts/plugins/assets/vaults/RewardableERC20Vault.sol
+++ contracts/plugins/assets/vaults/RewardableERC20Vault.sol
@@ -6,16 +6,23 @@ import "../../../vendor/oz/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+/**
+ * @title RewardableERC20Vault
+ * @notice A transferrable vault token wrapping an inner ERC20 that earns rewards.
+ *   Holding the vault token for a period of time earns the holder the right to
+ *   their prorata share of the global rewards earned during that time.
+ * @dev To inherit, override _claimAssetRewards()
+ */
 abstract contract RewardableERC20Vault is IRewardable, ERC4626 {
     using SafeERC20 for ERC20;
 
-    uint256 public immutable one;
+    uint256 public immutable one; // {qShare/share}
     ERC20 public immutable rewardToken;
 
-    uint256 public rewardsPerShare;
-    mapping(address => uint256) public lastRewardsPerShare;
-    mapping(address => uint256) public accumulatedRewards;
-    mapping(address => uint256) public claimedRewards;
+    uint256 public rewardsPerShare; // {qRewards/share}
+    mapping(address => uint256) public lastRewardsPerShare; // {qRewards/share}
+    mapping(address => uint256) public accumulatedRewards; // {qRewards}
+    mapping(address => uint256) public claimedRewards; // {qRewards}
 
     constructor(
         ERC20 _asset,
@@ -36,9 +43,11 @@ abstract contract RewardableERC20Vault is IRewardable, ERC4626 {
     function _syncAccount(address account) internal {
         if (account == address(0)) return;
         uint256 shares = balanceOf(account);
-        uint256 accountRewardsPershare = lastRewardsPerShare[account];
-        if (rewardsPerShare == accountRewardsPershare) return;
-        uint256 delta = rewardsPerShare - accountRewardsPershare;
+        uint256 accountRewardsPerShare = lastRewardsPerShare[account];
+        if (rewardsPerShare == accountRewardsPerShare) return;
+        uint256 delta = rewardsPerShare - accountRewardsPerShare;
+
+        // {qRewards} = {qRewards/share} * {qShare} / {qShare/share}
         uint256 newRewards = (delta * shares) / one;
         lastRewardsPerShare[account] = rewardsPerShare;
         accumulatedRewards[account] += newRewards;
@@ -52,6 +61,8 @@ abstract contract RewardableERC20Vault is IRewardable, ERC4626 {
             _claimAssetRewards();
             uint256 endingBal = rewardToken.balanceOf(address(this));
             delta = endingBal - initialBal;
+
+            // {qRewards/share} += {qRewards} * {qShare/share} / {qShare}
             rewardsPerShare += ((delta) * one) / _totalSupply;
         }
     }
