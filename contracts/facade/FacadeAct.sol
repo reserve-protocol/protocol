c78210db3710af06f798e08ee6c84c2d3888359e
Julian R <56316686+julianmrodri@users.noreply.github.com>
2023-08-22 14:17:13 -0300

    avoid reverting and update tests

--- contracts/facade/FacadeAct.sol
+++ contracts/facade/FacadeAct.sol
@@ -257,9 +257,11 @@ contract FacadeAct is IFacadeAct, Multicall {
             try bm.rebalance(TradeKind.DUTCH_AUCTION) {} catch {}
         } else if (majorVersion == MAJOR_VERSION_2 || majorVersion == MAJOR_VERSION_1) {
             IERC20[] memory emptyERC20s = new IERC20[](0);
-            address(bm).functionCall(
+            // solhint-disable-next-line avoid-low-level-calls
+            (bool success, ) = address(bm).call{ value: 0 }(
                 abi.encodeWithSignature("manageTokens(address[])", emptyERC20s)
             );
+            success = success; // hush warning
         } else {
             _revertUnrecognizedVersion();
         }
