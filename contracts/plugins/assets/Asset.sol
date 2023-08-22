dd24648a5561463de5636ca0780b25d690eb21d9
Patrick McKelvy <pmckelvy1@gmail.com>
2023-08-22 18:06:38 -0400

    update Asset price comment.

--- contracts/plugins/assets/Asset.sol
+++ contracts/plugins/assets/Asset.sol
@@ -112,6 +112,12 @@ contract Asset is IAsset, VersionedAsset {
     /// @dev Should be general enough to not need to be overridden
     /// @return _low {UoA/tok} The lower end of the price estimate
     /// @return _high {UoA/tok} The upper end of the price estimate
+    /// @notice If the price feed is broken, _low will decay downwards and _high will decay upwards
+    ///     If the price feed is broken for more than `oracleTimeout + priceTimeout` seconds,
+    ///     _low will be 0 and _high will be FIX_MAX.
+    ///     Because the price decay begins at `oracleTimeout` seconds and not `updateTime` from the
+    ///     price feed, the price feed can be broken for up to `priceTimeout` seconds without
+    ///     affecting the price estimate.
     function price() public view virtual returns (uint192 _low, uint192 _high) {
         try this.tryPrice() returns (uint192 low, uint192 high, uint192) {
             // if the price feed is still functioning, use that
