5ac55073533c4b0c09aa07a7fb59b59507e2d2c5
Taylor Brent <taylor.w.brent@gmail.com>
2023-09-29 17:23:42 -0400

    make CTokenFiatCollateral work for both Flux + CompoundV2

--- contracts/plugins/assets/compoundv2/CTokenFiatCollateral.sol
+++ contracts/plugins/assets/compoundv2/CTokenFiatCollateral.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "../../../plugins/assets/compoundv2/CTokenWrapper.sol";
 import "../../../libraries/Fixed.sol";
 import "../AppreciatingFiatCollateral.sol";
 import "../../../interfaces/IRewardable.sol";
@@ -12,7 +13,7 @@ import "./ICToken.sol";
  * @title CTokenFiatCollateral
  * @notice Collateral plugin for a cToken of fiat collateral, like cUSDC or cUSDP
  * Expected: {tok} != {ref}, {ref} is pegged to {target} unless defaulting, {target} == {UoA}
- * Also used for FluxFinance
+ * Also used for FluxFinance. Flexible enough to work with and without CTokenWrapper.
  */
 contract CTokenFiatCollateral is AppreciatingFiatCollateral {
     using OracleLib for AggregatorV3Interface;
@@ -24,13 +25,25 @@ contract CTokenFiatCollateral is AppreciatingFiatCollateral {
 
     ICToken public immutable cToken; // gas-optimization: access underlying cToken directly
 
-    /// @param config.erc20 Should be a CTokenWrapper
+    /// @param config.erc20 May be a CTokenWrapper or the cToken itself
     /// @param revenueHiding {1} A value like 1e-6 that represents the maximum refPerTok to hide
     constructor(CollateralConfig memory config, uint192 revenueHiding)
         AppreciatingFiatCollateral(config, revenueHiding)
     {
-        cToken = ICToken(address(RewardableERC20Wrapper(address(config.erc20)).underlying()));
-        referenceERC20Decimals = IERC20Metadata(cToken.underlying()).decimals();
+        ICToken _cToken = ICToken(address(config.erc20));
+        address _underlying = _cToken.underlying();
+        uint8 _decimals;
+
+        // _underlying might be a wrapper at this point, try to go one level further
+        try ICToken(_underlying).underlying() returns (address _mostUnderlying) {
+            _cToken = ICToken(_underlying);
+            _decimals = IERC20Metadata(_mostUnderlying).decimals();
+        } catch {
+            _decimals = IERC20Metadata(_underlying).decimals();
+        }
+
+        cToken = _cToken;
+        referenceERC20Decimals = _decimals;
         require(referenceERC20Decimals > 0, "referenceERC20Decimals missing");
     }
 
@@ -67,6 +80,8 @@ contract CTokenFiatCollateral is AppreciatingFiatCollateral {
     /// Claim rewards earned by holding a balance of the ERC20 token
     /// DEPRECATED: claimRewards() will be removed from all assets and collateral plugins
     function claimRewards() external virtual override(Asset, IRewardable) {
-        IRewardable(address(erc20)).claimRewards();
+        // solhint-ignore-next-line no-empty-blocks
+        try IRewardable(address(erc20)).claimRewards() {} catch {}
+        // erc20 may not be a CTokenWrapper
     }
 }
