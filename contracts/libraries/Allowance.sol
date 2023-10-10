700f202c9730f1eb8123b45deb52a463ae40d799
Taylor Brent <taylor.w.brent@gmail.com>
2023-10-10 12:23:20 -0400

    implementation nits

--- contracts/libraries/Allowance.sol
+++ contracts/libraries/Allowance.sol
@@ -8,6 +8,8 @@ library AllowanceLib {
     ///   1. Sets initial allowance to 0
     ///   2. Tries to set the provided allowance
     ///   3. Falls back to setting a maximum allowance, if (2) fails
+    /// Context: Some new-age ERC20s think it's a good idea to revert for allowances
+    /// that are > 0 but < type(uint256).max.
     function safeApproveFallbackToMax(
         IERC20 token,
         address spender,
@@ -15,6 +17,8 @@ library AllowanceLib {
     ) internal {
         // 1. Set initial allowance to 0
         require(token.approve(spender, 0), "failed to set 0 allowance");
+
+        if (value == 0) return;
 
         // 2. Try to set the provided allowance
         bool success; // bool success = false;
