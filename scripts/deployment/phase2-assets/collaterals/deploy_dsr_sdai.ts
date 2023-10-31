6c7e44945776f97e165bee83b2231dc7efab051d
Julian M. Rodriguez <56316686+julianmrodri@users.noreply.github.com>
2023-10-31 13:12:28 -0300

    TRUST QA-8: No revenue hiding in SDAI (#1001)

--- scripts/deployment/phase2-assets/collaterals/deploy_dsr_sdai.ts
+++ scripts/deployment/phase2-assets/collaterals/deploy_dsr_sdai.ts
@@ -59,7 +59,7 @@ async function main() {
       defaultThreshold: fp('0.0125').toString(), // 1.25%
       delayUntilDefault: bn('86400').toString(), // 24h
     },
-    fp('1e-6').toString(), // revenueHiding = 0.0001%
+    bn(0), // does not require revenue hiding
     POT
   )
   await collateral.deployed()
