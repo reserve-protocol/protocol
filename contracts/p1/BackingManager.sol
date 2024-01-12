b2596de87f8f29246dbed52be477efe06be6e5e2
Julian M. Rodriguez <56316686+julianmrodri@users.noreply.github.com>
2024-01-12 17:11:40 -0300

    TRUST-M3: Fix Storage Gap (#1032)

--- contracts/p1/BackingManager.sol
+++ contracts/p1/BackingManager.sol
@@ -351,5 +351,5 @@ contract BackingManagerP1 is TradingP1, IBackingManager {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[37] private __gap;
+    uint256[38] private __gap;
 }
