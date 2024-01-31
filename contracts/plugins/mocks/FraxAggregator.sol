02c9bd7b7c3b3bbf39ab6eed7e416dc16171f6f9
Taylor Brent <taylor.w.brent@gmail.com>
2024-01-30 19:49:22 -0500

    remove FraxOracleLib

---
+++ contracts/plugins/mocks/FraxAggregator.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BlueOak-1.0.0
+pragma solidity 0.8.19;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+interface FraxAggregatorV3Interface is AggregatorV3Interface {
+    function priceSource() external view returns (address);
+
+    function addRoundData(
+        bool _isBadData,
+        uint104 _priceLow,
+        uint104 _priceHigh,
+        uint40 _timestamp
+    ) external;
+}
