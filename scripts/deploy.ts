23a4bf9e3b2fce3cb933ea0c9a9ccd4f197f636f
Taylor Brent <taylor.w.brent@gmail.com>
2024-03-07 13:57:06 -0500

    deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -58,6 +58,7 @@ async function main() {
       'phase2-assets/collaterals/deploy_flux_finance_collateral.ts',
       'phase2-assets/collaterals/deploy_ctokenv3_usdc_collateral.ts',
       'phase2-assets/collaterals/deploy_convex_3pool_collateral.ts',
+      'phase2-assets/collaterals/deploy_convex_crvusd_usdc_collateral.ts',
       'phase2-assets/collaterals/deploy_convex_rToken_metapool_plugin.ts',
       'phase2-assets/collaterals/deploy_convex_stable_metapool_plugin.ts',
       'phase2-assets/collaterals/deploy_curve_stable_plugin.ts',
