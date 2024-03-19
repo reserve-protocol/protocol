c1cde3cea98d3578736dd8b00e6f77914b6ba4a4
Taylor Brent <taylor.w.brent@gmail.com>
2024-03-18 23:11:23 -0400

    revert

--- test/plugins/individual-collateral/morpho-aave/mintCollateralTo.ts
+++ test/plugins/individual-collateral/morpho-aave/mintCollateralTo.ts
@@ -2,6 +2,8 @@ import { CollateralFixtureContext, MintCollateralFunc } from '../pluginTestTypes
 import hre from 'hardhat'
 import { BigNumberish, constants } from 'ethers'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { networkConfig } from '#/common/configuration'
+import { whales } from '#/tasks/testing/upgrade-checker-utils/constants'
 import { whileImpersonating } from '#/utils/impersonation'
 import { IERC20 } from '@typechain/IERC20'
 import { MockV3Aggregator } from '@typechain/MockV3Aggregator'
@@ -18,6 +20,9 @@ export interface MorphoAaveCollateralFixtureContext extends CollateralFixtureCon
   targetPrRefFeed?: MockV3Aggregator
 }
 
+// TODO do we need this?
+whales[networkConfig['1'].tokens.USDC!.toLowerCase()] = '0x756D64Dc5eDb56740fC617628dC832DDBCfd373c'
+
 /**
  * Mint collateral to a recipient using the MorphoAAVEPositionWrapperMock contract.
  * @param ctx The MorphoAaveCollateralFixtureContext object.
@@ -31,15 +36,12 @@ export const mintCollateralTo: MintCollateralFunc<MorphoAaveCollateralFixtureCon
   _: SignerWithAddress,
   recipient: string
 ) => {
-  await whileImpersonating(
-    hre,
-    '0x756D64Dc5eDb56740fC617628dC832DDBCfd373c',
-    async (whaleSigner) => {
-      await ctx.underlyingErc20.connect(whaleSigner).approve(ctx.morphoWrapper.address, 0)
-      await ctx.underlyingErc20
-        .connect(whaleSigner)
-        .approve(ctx.morphoWrapper.address, constants.MaxUint256)
-      await ctx.morphoWrapper.connect(whaleSigner).mint(amount, recipient)
-    }
-  )
+  console.log('mintCollateralTo', whales[ctx.underlyingErc20.address], whales)
+  await whileImpersonating(hre, whales[ctx.underlyingErc20.address], async (whaleSigner) => {
+    await ctx.underlyingErc20.connect(whaleSigner).approve(ctx.morphoWrapper.address, 0)
+    await ctx.underlyingErc20
+      .connect(whaleSigner)
+      .approve(ctx.morphoWrapper.address, constants.MaxUint256)
+    await ctx.morphoWrapper.connect(whaleSigner).mint(amount, recipient)
+  })
 }
