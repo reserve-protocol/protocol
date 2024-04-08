7ed3436a96fbff7ec46a2bcb912008b60dd0d946
Taylor Brent <taylor.w.brent@gmail.com>
2024-04-08 12:43:44 -0400

    Arbitrum initial plugins (#1101)

--- test/plugins/individual-collateral/aave-v3/constants.ts
+++ test/plugins/individual-collateral/aave-v3/constants.ts
@@ -11,3 +11,11 @@ export const USDC_MAINNET_ORACLE_ERROR = fp('0.0025')
 export const USDC_BASE_MAX_TRADE_VOLUME = fp('0.5e6')
 export const USDC_BASE_ORACLE_TIMEOUT = bn('86400')
 export const USDC_BASE_ORACLE_ERROR = fp('0.003')
+
+export const USDC_ARBITRUM_MAX_TRADE_VOLUME = fp('1e6')
+export const USDC_ARBITRUM_ORACLE_TIMEOUT = bn('86400')
+export const USDC_ARBITRUM_ORACLE_ERROR = fp('0.001')
+
+export const USDT_ARBITRUM_MAX_TRADE_VOLUME = fp('1e6')
+export const USDT_ARBITRUM_ORACLE_TIMEOUT = bn('86400')
+export const USDT_ARBITRUM_ORACLE_ERROR = fp('0.001')
