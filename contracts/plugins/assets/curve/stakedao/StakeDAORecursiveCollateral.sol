62ff9917d7cd656b7d71bb939bb7df89e70ddb50
Taylor Brent <taylor.w.brent@gmail.com>
2024-04-23 22:13:21 -0400

    move interfaces to own file

--- contracts/plugins/assets/curve/stakedao/StakeDAORecursiveCollateral.sol
+++ contracts/plugins/assets/curve/stakedao/StakeDAORecursiveCollateral.sol
@@ -1,28 +1,8 @@
 // SPDX-License-Identifier: BlueOak-1.0.0
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "../CurveRecursiveCollateral.sol";
-
-interface IStakeDAOVault is IERC20Metadata {
-    function token() external view returns (IERC20Metadata);
-
-    function liquidityGauge() external view returns (IStakeDAOGauge);
-}
-
-interface IStakeDAOGauge {
-    function claimer() external view returns (IStakeDAOClaimer);
-
-    // solhint-disable-next-line func-name-mixedcase
-    function reward_count() external view returns (uint256);
-
-    // solhint-disable-next-line func-name-mixedcase
-    function reward_tokens(uint256 index) external view returns (IERC20Metadata);
-}
-
-interface IStakeDAOClaimer {
-    function claimRewards(address[] memory gauges, bool claimVeSDT) external;
-}
+import "./IStakeDAO.sol";
 
 /**
  * @title StakeDAORecursiveCollateral
