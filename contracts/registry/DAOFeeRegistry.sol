6fbd0898f0e9059bfbc1aec4c992e045fc4b6e3a
Akshat Mittal <itsreallyakshat@gmail.com>
2024-05-31 23:43:45 +0530

    Fee Registry Changes

--- contracts/registry/DAOFeeRegistry.sol
+++ contracts/registry/DAOFeeRegistry.sol
@@ -8,7 +8,13 @@ uint256 constant FEE_DENOMINATOR = 100_00;
 contract DAOFeeRegistry is Ownable {
     address private feeRecipient;
     uint256 private defaultFeeNumerator; // 1e4 = 100% fee
+
     mapping(address => uint256) private rTokenFeeNumerator;
+    mapping(address => bool) private rTokenFeeSet;
+
+    event FeeRecipientSet(address indexed feeRecipient);
+    event DefaultFeeNumeratorSet(uint256 defaultFeeNumerator);
+    event RTokenFeeNumeratorSet(address indexed rToken, uint256 feeNumerator, bool isActive);
 
     constructor(address owner_) Ownable() {
         _transferOwnership(owner_); // Ownership to DAO
@@ -19,20 +25,31 @@ contract DAOFeeRegistry is Ownable {
         require(feeRecipient_ != feeRecipient, "already set");
 
         feeRecipient = feeRecipient_;
+        emit FeeRecipientSet(feeRecipient_);
     }
 
     function setDefaultFeeNumerator(uint256 feeNumerator_) external onlyOwner {
         // TODO: Need a more sensible max limit here...
-        require(feeNumerator_ != 0 && feeNumerator_ < 1e4, "invalid fee numerator");
+        require(feeNumerator_ != 0 && feeNumerator_ < FEE_DENOMINATOR, "invalid fee numerator");
 
         defaultFeeNumerator = feeNumerator_;
+        emit DefaultFeeNumeratorSet(defaultFeeNumerator);
     }
 
     function setRTokenFeeNumerator(address rToken, uint256 feeNumerator_) external onlyOwner {
         // TODO: Need a more sensible max limit here...
-        require(feeNumerator_ != 0 && feeNumerator_ < 1e4, "invalid fee numerator");
+        require(feeNumerator_ != 0 && feeNumerator_ < FEE_DENOMINATOR, "invalid fee numerator");
 
         rTokenFeeNumerator[rToken] = feeNumerator_;
+        rTokenFeeSet[rToken] = true;
+        emit RTokenFeeNumeratorSet(rToken, feeNumerator_, true);
+    }
+
+    function resetRTokenFee(address rToken) external onlyOwner {
+        rTokenFeeNumerator[rToken] = 0;
+        rTokenFeeSet[rToken] = false;
+
+        emit RTokenFeeNumeratorSet(rToken, 0, false);
     }
 
     function getFeeDetails(address rToken)
@@ -45,9 +62,7 @@ contract DAOFeeRegistry is Ownable {
         )
     {
         recipient = feeRecipient;
-        feeNumerator = rTokenFeeNumerator[rToken] == 0
-            ? defaultFeeNumerator
-            : rTokenFeeNumerator[rToken];
+        feeNumerator = rTokenFeeSet[rToken] ? rTokenFeeNumerator[rToken] : defaultFeeNumerator;
         feeDenominator = FEE_DENOMINATOR;
     }
 }
