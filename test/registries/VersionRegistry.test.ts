4684498b8ebf81f42f0c5bd3cf3bdf972dbca959
Julian R <julianmrodri@gmail.com>
2024-06-06 16:32:58 -0300

    enhance version registry tests

--- test/registries/VersionRegistry.test.ts
+++ test/registries/VersionRegistry.test.ts
@@ -2,8 +2,10 @@ import { ethers } from 'hardhat'
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { VersionRegistry } from '@typechain/VersionRegistry'
+import { DeployerMock } from '@typechain/DeployerMock'
 import { expect } from 'chai'
-import { ZERO_BYTES } from '#/common/constants'
+import { ONE_ADDRESS, ZERO_ADDRESS, ZERO_BYTES } from '#/common/constants'
+import { IImplementations } from '#/common/configuration'
 
 async function createVersionRegistry() {
   const [owner] = await ethers.getSigners()
@@ -19,6 +21,7 @@ async function createVersionRegistry() {
 
 describe('Version Registry', () => {
   let versionRegistry: VersionRegistry
+  let deployerMock: DeployerMock
   let owner: SignerWithAddress
 
   before(async () => {
@@ -34,36 +37,87 @@ describe('Version Registry', () => {
   describe('Version Management', () => {
     before(async () => {
       const DeployerMockFactory = await ethers.getContractFactory('DeployerMock')
-      const deployerMock = await DeployerMockFactory.deploy()
+      deployerMock = await DeployerMockFactory.deploy()
 
       await versionRegistry.registerVersion(deployerMock.address)
     })
 
-    it('Register Version', async () => {
+    it('Registered version correctly', async () => {
       const versionData = await versionRegistry.getLatestVersion()
 
       expect(versionData.versionHash).not.be.eq(ZERO_BYTES)
       expect(versionData.deprecated).be.eq(false)
+
+      const expectedVersionHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('4.0.0'))
+      expect(versionData.versionHash).to.eq(expectedVersionHash)
+      expect(await versionRegistry.deployments(expectedVersionHash)).to.not.equal(ZERO_ADDRESS)
+      expect(await versionRegistry.deployments(expectedVersionHash)).to.equal(deployerMock.address)
     })
 
-    it('Denies Duplicate Registration', async () => {
+    it('Handles multiple versions', async () => {
+      const initialVersionData = await versionRegistry.getLatestVersion()
+
+      // Register new version
+      const DeployerMockV2Factory = await ethers.getContractFactory('DeployerMockV2')
+      const deployerMockV2 = await DeployerMockV2Factory.deploy()
+      const expectedV2Hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('V2'))
+      await expect(versionRegistry.registerVersion(deployerMockV2.address))
+        .to.emit(versionRegistry, 'VersionRegistered')
+        .withArgs(expectedV2Hash, deployerMockV2.address)
+
+      // Check V2 properly registered
+      const v2VersionData = await versionRegistry.getLatestVersion()
+      expect(v2VersionData.versionHash).to.eq(expectedV2Hash)
+      expect(v2VersionData.versionHash).not.be.eq(ZERO_BYTES)
+      expect(v2VersionData.deprecated).be.eq(false)
+      expect(await versionRegistry.deployments(expectedV2Hash)).to.not.equal(ZERO_ADDRESS)
+      expect(await versionRegistry.deployments(expectedV2Hash)).to.equal(deployerMockV2.address)
+
+      // Original deployment still registered
+      expect(await versionRegistry.deployments(initialVersionData.versionHash)).to.equal(
+        deployerMock.address
+      )
+    })
+
+    it('Denies Duplicate and Invalid Registration', async () => {
       // Same version, different deployer, should be rejected.
       const DeployerMockFactory = await ethers.getContractFactory('DeployerMock')
-      const deployerMock = await DeployerMockFactory.deploy()
+      const deployerMockDup = await DeployerMockFactory.deploy()
 
       await expect(
-        versionRegistry.registerVersion(deployerMock.address)
+        versionRegistry.registerVersion(deployerMockDup.address)
       ).to.be.revertedWithCustomError(versionRegistry, 'VersionRegistry__InvalidRegistration')
+
+      // Invalid registration with zero address is also rejected
+      await expect(versionRegistry.registerVersion(ZERO_ADDRESS)).to.be.revertedWithCustomError(
+        versionRegistry,
+        'VersionRegistry__ZeroAddress'
+      )
     })
 
     it('Deprecate Version', async () => {
       let versionData = await versionRegistry.getLatestVersion()
 
-      await versionRegistry.deprecateVersion(versionData.versionHash)
+      await expect(versionRegistry.deprecateVersion(versionData.versionHash))
+        .to.emit(versionRegistry, 'VersionDeprecated')
+        .withArgs(versionData.versionHash)
       versionData = await versionRegistry.getLatestVersion()
 
       expect(versionData.versionHash).not.be.eq(ZERO_BYTES)
       expect(versionData.deprecated).be.eq(true)
+
+      // Cannot deprecate again
+      await expect(
+        versionRegistry.deprecateVersion(versionData.versionHash)
+      ).to.be.revertedWithCustomError(versionRegistry, 'VersionRegistry__AlreadyDeprecated')
+    })
+
+    it('Returns implementations correctly', async () => {
+      let versionData = await versionRegistry.getLatestVersion()
+      const implementations: IImplementations = await versionRegistry.getImplementationForVersion(
+        versionData.versionHash
+      )
+      expect(implementations.main).to.eq(ONE_ADDRESS)
     })
   })
 })
