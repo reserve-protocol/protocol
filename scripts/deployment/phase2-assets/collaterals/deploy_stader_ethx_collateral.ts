1aed874590d14a10b515b134128af8d3140be7e3
Dulguun <99845398+dulguun-staderlabs@users.noreply.github.com>
2024-07-28 21:43:47 +0800

    Add deployment scripts

---
+++ scripts/deployment/phase2-assets/collaterals/deploy_stader_ethx_collateral.ts
@@ -0,0 +1,101 @@
+import fs from 'fs'
+import hre from 'hardhat'
+import { getChainId } from '../../../../common/blockchain-utils'
+import { networkConfig } from '../../../../common/configuration'
+import { bn, fp } from '../../../../common/numbers'
+import { expect } from 'chai'
+import { CollateralStatus } from '../../../../common/constants'
+import {
+  getDeploymentFile,
+  getAssetCollDeploymentFilename,
+  IAssetCollDeployments,
+  getDeploymentFilename,
+  fileExists,
+} from '../../common'
+import { priceTimeout, combinedError } from '../../utils'
+import { ETHxCollateral } from '../../../../typechain'
+import { ContractFactory } from 'ethers'
+
+async function main() {
+  // ==== Read Configuration ====
+  const [deployer] = await hre.ethers.getSigners()
+
+  const chainId = await getChainId(hre)
+
+  console.log(`Deploying Collateral to network ${hre.network.name} (${chainId})
+    with burner account: ${deployer.address}`)
+
+  if (!networkConfig[chainId]) {
+    throw new Error(`Missing network configuration for ${hre.network.name}`)
+  }
+
+  // Get phase1 deployment
+  const phase1File = getDeploymentFilename(chainId)
+  if (!fileExists(phase1File)) {
+    throw new Error(`${phase1File} doesn't exist yet. Run phase 1`)
+  }
+  // Check previous step completed
+  const assetCollDeploymentFilename = getAssetCollDeploymentFilename(chainId)
+  const assetCollDeployments = <IAssetCollDeployments>getDeploymentFile(assetCollDeploymentFilename)
+
+  const deployedCollateral: string[] = []
+
+  const deployedOracle: string[] = []
+
+  /********  Deploy Mock Oracle (if needed)  **************************/
+  let ETHxOracleAddress: string = networkConfig[chainId].chainlinkFeeds.ETHx!
+  if (chainId == 5) {
+    const MockOracleFactory = await hre.ethers.getContractFactory('MockV3Aggregator')
+    const mockOracle = await MockOracleFactory.connect(deployer).deploy(8, fp(2000))
+    await mockOracle.deployed()
+    console.log(
+      `Deployed MockV3Aggregator on ${hre.network.name} (${chainId}): ${mockOracle.address} `
+    )
+    deployedOracle.push(mockOracle.address)
+    ETHxOracleAddress = mockOracle.address
+  }
+
+  /********  Deploy Stader ETH Collateral - ETHx  **************************/
+  const ETHxCollateralFactory: ContractFactory = await hre.ethers.getContractFactory(
+    'ETHxCollateral'
+  )
+
+  const oracleError = combinedError(fp('0.005'), fp('0.02')) // 0.5% & 2%
+
+  const collateral = <ETHxCollateral>await ETHxCollateralFactory.connect(deployer).deploy(
+    {
+      priceTimeout: priceTimeout.toString(),
+      chainlinkFeed: networkConfig[chainId].chainlinkFeeds.ETH,
+      oracleError: oracleError.toString(), // 0.5% & 2%
+      erc20: networkConfig[chainId].tokens.ETHx,
+      maxTradeVolume: fp('1e6').toString(), // $1m,
+      oracleTimeout: '3600', // 1 hr,
+      targetName: hre.ethers.utils.formatBytes32String('ETH'),
+      defaultThreshold: fp('0.02').add(oracleError).toString(), // ~4.5%
+      delayUntilDefault: bn('86400').toString(), // 24h
+    },
+    fp('1e-4').toString(), // revenueHiding = 0.01%
+    ETHxOracleAddress, // refPerTokChainlinkFeed
+    '86400' // refPerTokChainlinkTimeout
+  )
+  await collateral.deployed()
+  await (await collateral.refresh()).wait()
+  expect(await collateral.status()).to.equal(CollateralStatus.SOUND)
+
+  console.log(`Deployed Stader ETHx to ${hre.network.name} (${chainId}): ${collateral.address}`)
+
+  assetCollDeployments.collateral.ETHx = collateral.address
+  assetCollDeployments.erc20s.ETHx = networkConfig[chainId].tokens.ETHx
+  deployedCollateral.push(collateral.address.toString())
+
+  fs.writeFileSync(assetCollDeploymentFilename, JSON.stringify(assetCollDeployments, null, 2))
+
+  console.log(`Deployed collateral to ${hre.network.name} (${chainId})
+        New deployments: ${deployedCollateral}
+        Deployment file: ${assetCollDeploymentFilename}`)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
---
+++ scripts/verification/collateral-plugins/verify_ethx.ts
@@ -0,0 +1,56 @@
+import hre from 'hardhat'
+import { getChainId } from '../../../common/blockchain-utils'
+import { developmentChains, networkConfig } from '../../../common/configuration'
+import { fp, bn } from '../../../common/numbers'
+import {
+  getDeploymentFile,
+  getAssetCollDeploymentFilename,
+  IAssetCollDeployments,
+} from '../../deployment/common'
+import { priceTimeout, verifyContract, combinedError } from '../../deployment/utils'
+
+let deployments: IAssetCollDeployments
+
+async function main() {
+  // ********** Read config **********
+  const chainId = await getChainId(hre)
+  if (!networkConfig[chainId]) {
+    throw new Error(`Missing network configuration for ${hre.network.name}`)
+  }
+
+  if (developmentChains.includes(hre.network.name)) {
+    throw new Error(`Cannot verify contracts for development chain ${hre.network.name}`)
+  }
+
+  const assetCollDeploymentFilename = getAssetCollDeploymentFilename(chainId)
+  deployments = <IAssetCollDeployments>getDeploymentFile(assetCollDeploymentFilename)
+
+  /********  Verify Stader ETH - ETHx  **************************/
+  const oracleError = combinedError(fp('0.005'), fp('0.02')) // 0.5% & 2%
+  await verifyContract(
+    chainId,
+    deployments.collateral.ETHx,
+    [
+      {
+        priceTimeout: priceTimeout.toString(),
+        chainlinkFeed: networkConfig[chainId].chainlinkFeeds.ETH,
+        oracleError: oracleError.toString(), // 0.5% & 2%,
+        erc20: networkConfig[chainId].tokens.ETHx,
+        maxTradeVolume: fp('1e6').toString(), // $1m,
+        oracleTimeout: '3600', // 1 hr,
+        targetName: hre.ethers.utils.formatBytes32String('ETH'),
+        defaultThreshold: fp('0.02').add(oracleError).toString(), // ~4.5%
+        delayUntilDefault: bn('86400').toString(), // 24h
+      },
+      fp('1e-4'), // revenueHiding = 0.01%
+      networkConfig[chainId].chainlinkFeeds.ETHx, // refPerTokChainlinkFeed
+      '86400', // refPerTokChainlinkTimeout
+    ],
+    'contracts/plugins/assets/stader/ETHxCollateral.sol:ETHxCollateral'
+  )
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
