adcdb23efb170afbe2d24b26cbb58740a54a804e
Taylor Brent <taylor.w.brent@gmail.com>
2024-07-30 17:45:37 -0400

    make interface names distinct

--- contracts/facade/factories/CurveOracleFactory.sol
+++ contracts/facade/factories/CurveOracleFactory.sol
@@ -4,21 +4,25 @@ pragma solidity 0.8.19;
 import { divuu } from "../../libraries/Fixed.sol";
 
 // weird circular inheritance preventing us from using proper IRToken, not worth figuring out
-interface IRToken {
+interface IMinimalRToken {
     function basketsNeeded() external view returns (uint192);
 
     function totalSupply() external view returns (uint256);
 }
 
 contract CurveOracle {
-    IRToken public immutable rToken;
+    address public immutable rToken;
 
-    constructor(IRToken _rToken) {
+    constructor(address _rToken) {
         rToken = _rToken;
     }
 
     function exchangeRate() external view returns (uint256) {
-        return divuu(uint256(rToken.basketsNeeded()), rToken.totalSupply());
+        return
+            divuu(
+                uint256(IMinimalRToken(rToken).basketsNeeded()),
+                IMinimalRToken(rToken).totalSupply()
+            );
     }
 }
 
@@ -31,9 +35,9 @@ contract CurveOracleFactory {
 
     event CurveOracleDeployed(address indexed rToken, address indexed curveOracle);
 
-    mapping(IRToken => CurveOracle) public curveOracles;
+    mapping(address => CurveOracle) public curveOracles;
 
-    function deployCurveOracle(IRToken rToken) external returns (address) {
+    function deployCurveOracle(address rToken) external returns (address) {
         if (address(curveOracles[rToken]) != address(0)) revert CurveOracleAlreadyDeployed();
         CurveOracle curveOracle = new CurveOracle(rToken);
         curveOracle.exchangeRate(); // ensure it works
