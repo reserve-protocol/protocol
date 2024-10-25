aeb65773cdfee82dc02c5d2830e61716287dccd4
Julian R <julianmrodri@gmail.com>
2024-10-25 09:58:17 -0300

    advance block for ci tests

--- test/integration/fork-block-numbers.ts
+++ test/integration/fork-block-numbers.ts
@@ -11,7 +11,7 @@ const forkBlockNumber = {
   'mainnet-3.4.0': 20328530, // Ethereum
 
   // TODO add all the block numbers we fork from to benefit from caching
-  default: 20679946, // Ethereum
+  default: 20890018, // Ethereum
 }
 
 export default forkBlockNumber
