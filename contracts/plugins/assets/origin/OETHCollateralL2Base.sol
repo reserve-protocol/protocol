0ab90a5bb07415708b75cae2f3a8216661a1d699
Clément <clemmoller@gmail.com>
2024-10-29 18:16:14 +0100

    lint and prettier

--- contracts/plugins/assets/origin/OETHCollateralL2Base.sol
+++ contracts/plugins/assets/origin/OETHCollateralL2Base.sol
@@ -10,45 +10,96 @@ interface IWSuperOETHb {
     event Approval(address indexed owner, address indexed spender, uint256 value);
     event Deposit(address indexed caller, address indexed owner, uint256 assets, uint256 shares);
     event GovernorshipTransferred(address indexed previousGovernor, address indexed newGovernor);
-    event PendingGovernorshipTransfer(address indexed previousGovernor, address indexed newGovernor);
+    event PendingGovernorshipTransfer(
+        address indexed previousGovernor,
+        address indexed newGovernor
+    );
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Withdraw(
-        address indexed caller, address indexed receiver, address indexed owner, uint256 assets, uint256 shares
+        address indexed caller,
+        address indexed receiver,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
     );
 
     function allowance(address owner, address spender) external view returns (uint256);
+
     function approve(address spender, uint256 amount) external returns (bool);
+
     function asset() external view returns (address);
+
     function balanceOf(address account) external view returns (uint256);
+
     function claimGovernance() external;
+
     function convertToAssets(uint256 shares) external view returns (uint256 assets);
+
     function convertToShares(uint256 assets) external view returns (uint256 shares);
+
     function decimals() external view returns (uint8);
+
     function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
+
     function deposit(uint256 assets, address receiver) external returns (uint256);
+
     function governor() external view returns (address);
+
     function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
+
     function initialize() external;
+
     function isGovernor() external view returns (bool);
+
     function maxDeposit(address) external view returns (uint256);
+
     function maxMint(address) external view returns (uint256);
+
     function maxRedeem(address owner) external view returns (uint256);
+
     function maxWithdraw(address owner) external view returns (uint256);
+
     function mint(uint256 shares, address receiver) external returns (uint256);
+
     function name() external view returns (string memory);
+
     function previewDeposit(uint256 assets) external view returns (uint256);
+
     function previewMint(uint256 shares) external view returns (uint256);
+
     function previewRedeem(uint256 shares) external view returns (uint256);
+
     function previewWithdraw(uint256 assets) external view returns (uint256);
-    function redeem(uint256 shares, address receiver, address owner) external returns (uint256);
+
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) external returns (uint256);
+
     function symbol() external view returns (string memory);
+
     function totalAssets() external view returns (uint256);
+
     function totalSupply() external view returns (uint256);
+
     function transfer(address recipient, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
     function transferGovernance(address _newGovernor) external;
+
     function transferToken(address asset_, uint256 amount_) external;
-    function withdraw(uint256 assets, address receiver, address owner) external returns (uint256);
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) external returns (uint256);
 }
 
 interface IMorphoChainlinkOracleV2 {
@@ -99,7 +150,16 @@ contract OETHCollateralL2Base is AppreciatingFiatCollateral {
     /// @return low {UoA/tok} The low price estimate
     /// @return high {UoA/tok} The high price estimate
     /// @return pegPrice {target/ref} The actual price observed in the peg
-    function tryPrice() external view override returns (uint192 low, uint192 high, uint192 pegPrice) {
+    function tryPrice()
+        external
+        view
+        override
+        returns (
+            uint192 low,
+            uint192 high,
+            uint192 pegPrice
+        )
+    {
         // {tar/tok}
         // {ETH/wsuperOETHb}
         uint192 targetPerTok = _safeWrap(targetPerTokChainlinkFeed.price()) / 1e18;
