05bf7541559045d4b1346c11c0562b199e9e2848
Akshat Mittal <itsreallyakshat@gmail.com>
2024-11-05 22:00:40 +0530

    Add Yearn Curve Oracle

--- contracts/facade/oracles/curve-oracle/CurveOracle.sol
+++ contracts/facade/oracles/curve-oracle/CurveOracle.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: BlueOak-1.0.0
 pragma solidity 0.8.19;
 
-import { FIX_ONE, divuu } from "../../../libraries/Fixed.sol";
-
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import { IExchangeRateOracle } from "../exchange-rate/IExchangeRateOracle.sol";
-
-import "hardhat/console.sol";
 
 interface ICurveStableSwapNG {
     function coins(uint256 i) external view returns (address);
@@ -20,10 +16,16 @@ interface ICurveStableSwapNG {
  * @title CurveOracle
  * @notice An immutable Exchange Rate Oracle for a StableSwapNG Curve LP Token,
  *         with one or more appreciating assets. Only for 2-asset Curve LP Tokens.
+ * @dev Does not account for native asset appreciation, only accounts for the
+ *      appreciation in the Curve LP via trading volume.
+ *
+ *      The oracles specified for the pool MUST be for the base unit, for example
+ *      if the paired token is sDAI, you'd specify the oracle for DAI/USD.
  */
 contract CurveOracle {
     enum OracleType {
         STORED,
+        STATIC,
         RTOKEN,
         CHAINLINK
     }
@@ -31,6 +33,7 @@ contract CurveOracle {
     struct OracleConfig {
         OracleType oracleType;
         address rateProvider;
+        uint256 staticValue;
         uint256 timeout;
     }
 
@@ -57,12 +60,14 @@ contract CurveOracle {
 
         if (oracleType == OracleType.STORED) {
             return curvePool.stored_rates()[tokenId];
+        } else if (oracleType == OracleType.STATIC) {
+            return oracleConfig.staticValue;
         } else if (oracleType == OracleType.RTOKEN) {
             return IExchangeRateOracle(oracleConfig.rateProvider).exchangeRate();
         } else if (oracleType == OracleType.CHAINLINK) {
-            (, int256 price, , uint256 updateTime, ) = AggregatorV3Interface(
-                oracleConfig.rateProvider
-            ).latestRoundData();
+            AggregatorV3Interface oracle = AggregatorV3Interface(oracleConfig.rateProvider);
+            uint8 decimals = oracle.decimals();
+            (, int256 price, , uint256 updateTime, ) = oracle.latestRoundData();
 
             if (price < 0) {
                 revert BadOracleValue();
@@ -72,7 +77,13 @@ contract CurveOracle {
                 revert BadOracleValue();
             }
 
-            return uint256(price);
+            if (decimals == 18) {
+                return uint256(price);
+            } else if (decimals < 18) {
+                return uint256(price) * (10**(18 - decimals));
+            } else {
+                return uint256(price) / (10**(decimals - 18));
+            }
         }
 
         revert InvalidOracleType();
@@ -82,13 +93,8 @@ contract CurveOracle {
         uint256 token0Price = _getTokenPrice(0);
         uint256 token1Price = _getTokenPrice(1);
 
-        console.log("token0Price: %d", token0Price);
-        console.log("token1Price: %d", token1Price);
-
         uint256 minPrice = token0Price < token1Price ? token0Price : token1Price;
         uint256 virtualPrice = curvePool.get_virtual_price();
-
-        console.log("virtualPrice: %d", virtualPrice);
 
         return (virtualPrice * minPrice) / 1e18;
     }
