fbe9d62510a86b754422f7add9077465a8c330f3
Taylor Brent <taylor.w.brent@gmail.com>
2025-12-19 16:58:11 -0500

    new AssetPluginRegistry address

--- contracts/spells/4_2_0.sol
+++ contracts/spells/4_2_0.sol
@@ -179,7 +179,7 @@ contract Upgrade4_2_0 is Versioned {
             // DAO registries (mainnet)
             registries = IDeployer.Registries(
                 VersionRegistry(0x1895b15B3d0a70962be86Af0E337018aD63464e0),
-                AssetPluginRegistry(0x4a818c41131CB9FE65BadF2Bb8671dDE4D117135),
+                AssetPluginRegistry(0xA9145A22537B39b04fe91AA479c1b8e7a3569c98),
                 DAOFeeRegistry(0xec716deD4eABa060937D1a915F166E237039342B),
                 ITrustedFillerRegistry(0x279ccF56441fC74f1aAC39E7faC165Dec5A88B3A)
             );
@@ -208,7 +208,7 @@ contract Upgrade4_2_0 is Versioned {
             // DAO registries (base)
             registries = IDeployer.Registries(
                 VersionRegistry(0xBbC532A80DD141449330c1232C953Da6801Aed01),
-                AssetPluginRegistry(0x7Ac954307356301A10adDb0dB4f61b4a475d3551),
+                AssetPluginRegistry(0x3312507BC3F22430B34D5841A472c767DC5C36e4),
                 DAOFeeRegistry(0x3513D2c7D2F51c678889CeC083E7D7Ae27b219aD),
                 ITrustedFillerRegistry(0x72DB5f49D0599C314E2f2FEDf6Fe33E1bA6C7A18)
             );
