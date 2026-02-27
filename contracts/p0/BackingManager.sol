79fe8744049bcf04aca593dc4c681e52acde21f0
Taylor Brent <taylor.w.brent@gmail.com>
2026-02-26 20:13:06 -0500

    lint clean

--- contracts/p0/BackingManager.sol
+++ contracts/p0/BackingManager.sol
@@ -176,7 +176,8 @@ contract BackingManagerP0 is TradingP0, IBackingManager {
             }
         }
 
-        uint192 needed = main.rToken().basketsNeeded().mul(FIX_ONE.plus(backingBuffer), CEIL); // {BU}
+        // {BU}
+        uint192 needed = main.rToken().basketsNeeded().mul(FIX_ONE.plus(backingBuffer), CEIL);
 
         // Handout excess assets above what is needed, including any newly minted RToken
         RevenueTotals memory totals = main.distributor().totals();
