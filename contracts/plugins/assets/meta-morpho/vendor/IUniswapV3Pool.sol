417ebd186598d605a5629f1ab58664914bdae1c8
Julian R <julianmrodri@gmail.com>
2026-08-28 11:34:21 -0300

    add slot0 to interface

--- contracts/plugins/assets/meta-morpho/vendor/IUniswapV3Pool.sol
+++ contracts/plugins/assets/meta-morpho/vendor/IUniswapV3Pool.sol
@@ -19,4 +19,20 @@ interface IUniswapV3Pool {
     function token0() external view returns (address);
 
     function token1() external view returns (address);
+
+    /// @dev Uniswap V3 exposes no standalone getter for observationCardinality; it is packed
+    ///      into slot0. Not used by this plugin at runtime -- only by the deployment script's
+    ///      pre-flight check on the pool's observation buffer.
+    function slot0()
+        external
+        view
+        returns (
+            uint160 sqrtPriceX96,
+            int24 tick,
+            uint16 observationIndex,
+            uint16 observationCardinality,
+            uint16 observationCardinalityNext,
+            uint8 feeProtocol,
+            bool unlocked
+        );
 }
